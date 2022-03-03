cffe1f77d79a3b7b20f18202a090c76235baa424
lazyperson0710 <lazyperson0710@gmail.com>
2022-03-03 12:34:23 +0900

    e

---
+++ EconomyCShop-master/README.md
@@ -0,0 +1,17 @@
+# EconomyCShop
+
+ChestShop for EconomyAPI.
+
+## Usage
+
+1. Place chest and add items you want to sell.
+2. Place sign with format below.
+    1. Line 1: (none)
+    2. Line 2: price:int
+    3. Line 3: amount:int
+    4. Line 4: item:string(item name) or (id:int):(meta:int)
+3. Complete!
+
+## Permission
+
+- `economy-c-shop.force-close-shop` allow to destroy others' shop.
---
+++ EconomyCShop-master/src/EconomyCShop.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+namespace famima65536\EconomyCShop;
+
+use famima65536\EconomyCShop\application\ShopApplicationService;
+use famima65536\EconomyCShop\repository\IShopRepository;
+use famima65536\EconomyCShop\repository\JsonShopRepository;
+use famima65536\EconomyCShop\repository\SqliteShopRepository;
+use famima65536\EconomyCShop\service\ShopService;
+use famima65536\EconomyCShop\utils\economy\EconomyAPIWrapper;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use SQLite3;
+use Webmozart\PathUtil\Path;
+
+class EconomyCShop extends PluginBase {
+
+    private IShopRepository $shopRepository;
+
+    public function onLoad(): void {
+        //$this->saveDefaultConfig();
+        //$this->saveResource("message.yml");
+        $this->setupRepository();
+    }
+
+    private function setupRepository() {
+        $selectedRepository = "";
+        switch ($this->getConfig()->get("repository")) {
+            default:
+                $this->getLogger()->error("Unknown repository type is selected!");
+            case "json":
+                $this->shopRepository = new JsonShopRepository(new Config(Path::join($this->getDataFolder(), "shops.json")));
+                $selectedRepository = "json";
+                break;
+            case "sqlite":
+                $this->shopRepository = new SqliteShopRepository(new SQLite3(Path::join($this->getDataFolder(), "shops.sqlite")));
+                $selectedRepository = "sqlite";
+                break;
+        }
+        $this->getLogger()->notice("${selectedRepository} is selected to save shop data");
+    }
+
+    public function onEnable(): void {
+        $shopService = new ShopService($this->shopRepository);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(
+            new ShopApplicationService($shopService, $this->shopRepository),
+            $shopService,
+            $this->shopRepository,
+            new EconomyAPIWrapper(),
+        //new Config(Path::join($this->getDataFolder(), "message.yml"), Config::YAML)
+        ), $this);
+    }
+
+    public function onDisable(): void {
+        $this->shopRepository->close();
+    }
+}
