d3df503e41ab5cd06f16370b84d854c3f19e7509
lazyperson0710 <lazyperson0710@gmail.com>
2022-03-03 12:37:19 +0900

    v

---
+++ VanillaSponge/src/bbo51dog/vanillasponge/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace bbo51dog\vanillasponge;
+
+use bbo51dog\vanillasponge\block\VanillaSponge;
+use pocketmine\block\BlockFactory;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+    protected function onEnable(): void {
+        BlockFactory::getInstance()->register(new VanillaSponge(), true);
+    }
+}
