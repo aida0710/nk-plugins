30643b98974bb80e39d980e8ab10c0201133b0e7
lazyperson0710 <lazyperson0710@gmail.com>
2022-03-12 02:38:41 +0900

    Eventを追加。BeakEventの代替

---
+++ miningtools/src/deceitya/miningtools/event/CountBlockEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace deceitya\miningtools\event;
+
+use pocketmine\block\Block;
+use pocketmine\event\block\BlockEvent;
+use pocketmine\event\Cancellable;
+use pocketmine\event\CancellableTrait;
+use pocketmine\player\Player;
+
+class CountBlockEvent extends BlockEvent implements Cancellable{
+    use CancellableTrait;
+
+    /** @var int[] intの配列 array(1, 1, 5, 6); */
+    public array $blockIds;
+    private Player $player;
+
+    public function __construct(Player $player, Block $block) {
+        parent::__construct($block);
+        $this->player = $player;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getPlayer(): Player {
+        return $this->player;
+    }
+
+}
