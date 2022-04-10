9cdd8a44c9ee4332b998b2a36276ae5c3c5cfe5b
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-11 00:44:22 +0900

    mtコマンドで表示されるformの内部処理をコマンドのほうに合わせて大きく変更

--- ServerFunctionForms/src/lazyperson710/sff/form/MiningToolsForm.php
+++ ServerFunctionForms/src/lazyperson710/sff/form/MiningToolsForm.php
@@ -11,22 +11,28 @@ use pocketmine\Server;
 class MiningToolsForm extends SimpleForm {
 
     public function __construct(Player $player) {
-        if (MiningLevelAPI::getInstance()->getLevel($player) >= 25) {//2
-            $level25 = "§a25レベルの為、開放済み";
+        if (MiningLevelAPI::getInstance()->getLevel($player) >= 15) {//2
+            $level15 = "§a15レベルの為、開放済み";
         } else {
-            $level25 = "§c25レベル以上で開放されます";
+            $level15 = "§c15レベル以上で開放されます";
         }
-        if (MiningLevelAPI::getInstance()->getLevel($player) >= 50) {//3
-            $level50 = "§a50レベルの為、開放済み";
+        if (MiningLevelAPI::getInstance()->getLevel($player) >= 30) {//3
+            $level30 = "§a30レベルの為、開放済み";
         } else {
-            $level50 = "§c50レベル以上で開放されます";
+            $level30 = "§c30レベル以上で開放されます";
+        }
+        if (MiningLevelAPI::getInstance()->getLevel($player) >= 120) {//4
+            $level120 = "§a120レベルの為、開放済み";
+        } else {
+            $level120 = "§c120レベル以上で開放されます";
         }
         $this
             ->setTitle("Mining Tools")
             ->setText("見たいコンテンツを選択してください")
             ->addElements(
-                new CommandDispatchButton("Diamond Tools - 範囲採掘ツール\n{$level25}", "mt1", null),
-                new CommandDispatchButton("Netherite Tools - 範囲採掘ツール\n{$level50}", "mt2", null),
+                new CommandDispatchButton("Diamond Tools - 範囲採掘ツール\n{$level15}", "mt1", null),
+                new CommandDispatchButton("Netherite Tools - 範囲採掘ツール\n{$level30}", "mt2", null),
+                new CommandDispatchButton("アップグレード - Ex範囲採掘ツール\n{$level120}", "mt3", null),
             );
         if (Server::getInstance()->isOp($player->getName())) {
             $this->addElement(new CommandDispatchButton("Debug_Tools", "mt4", null));
