7f442ef166894dd1477296da8df19a09eabc1615
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-10 23:41:58 +0900

    opの場合処理貫通するように

--- DelEnchant-master/src/deceitya/delenchant/command/ReductionCommand.php
+++ DelEnchant-master/src/deceitya/delenchant/command/ReductionCommand.php
@@ -9,6 +9,7 @@ use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\Item;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class ReductionCommand extends Command {
 
@@ -21,9 +22,11 @@ class ReductionCommand extends Command {
             return true;
         }
         $item = $sender->getInventory()->getItemInHand();
-        if ($item->getNamedTag()->getInt('MiningTools_3', -1) !== -1 || $item->getNamedTag()->getInt('4mining', -1) !== -1 || $item->getNamedTag()->getInt('MiningTools_Expansion', -1) !== -1 || $item->hasEnchantment(VanillaEnchantments::PUNCH())) {
-            $sender->sendMessage('§bReduceEnchant §7>> §cこのアイテムはエンチャントを削減できません');
-            return true;
+        if (!Server::getInstance()->isOp($sender->getName())){
+            if ($item->getNamedTag()->getInt('MiningTools_3', -1) !== -1 || $item->getNamedTag()->getInt('4mining', -1) !== -1 || $item->getNamedTag()->getInt('MiningTools_Expansion', -1) !== -1 || $item->hasEnchantment(VanillaEnchantments::PUNCH())) {
+                $sender->sendMessage('§bReduceEnchant §7>> §cこのアイテムはエンチャントを削減できません');
+                return true;
+            }
         }
         /** @var ?ListTag $enchListTag */
         $enchListTag = $item->getNamedTag()->getListTag(Item::TAG_ENCH);
