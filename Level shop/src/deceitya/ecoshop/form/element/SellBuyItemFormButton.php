81e76efec1e024df948507fcb05f68478dee5fcd
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-16 02:37:29 +0900

    form処理を変更。ボタンクラスを作成

---
+++ Level shop/src/deceitya/ecoshop/form/element/SellBuyItemFormButton.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace deceitya\ecoshop\form\element;
+
+use bbo51dog\bboform\element\Button;
+use bbo51dog\bboform\element\ButtonImage;
+use deceitya\ecoshop\database\LevelShopAPI;
+use deceitya\ecoshop\form\SellBuyForm;
+use pocketmine\form\Form;
+use pocketmine\player\Player;
+
+class SellBuyItemFormButton extends Button {
+
+    private int $itemId;
+
+    /**
+     * @param string $text
+     * @param ButtonImage|null $image
+     */
+
+    public function __construct(string $text,int $itemId, ?ButtonImage $image = null) {
+        parent::__construct($text, $image);
+        $this->itemId = $itemId;
+    }
+
+    public function handleSubmit(Player $player): void {
+        $player->sendForm(new SellBuyForm($this->itemId, LevelShopAPI::getInstance()->getBuy($this->itemId), LevelShopAPI::getInstance()->getBuy($this->itemId)));
+    }
+}
