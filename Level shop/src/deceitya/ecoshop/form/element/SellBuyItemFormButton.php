81739f354ee08004f1f345cadcdb8bde89603739
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-20 02:54:41 +0900

    売却値が0円のとき購入選択を飛ばすように変更

--- Level shop/src/deceitya/ecoshop/form/element/SellBuyItemFormButton.php
+++ Level shop/src/deceitya/ecoshop/form/element/SellBuyItemFormButton.php
@@ -5,8 +5,13 @@ namespace deceitya\ecoshop\form\element;
 use bbo51dog\bboform\element\Button;
 use bbo51dog\bboform\element\ButtonImage;
 use deceitya\ecoshop\database\LevelShopAPI;
+use deceitya\ecoshop\form\PurchaseForm;
 use deceitya\ecoshop\form\SellBuyForm;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
 use pocketmine\player\Player;
+use ree_jp\stackstorage\api\StackStorageAPI;
 
 class SellBuyItemFormButton extends Button {
 
@@ -22,6 +27,25 @@ class SellBuyItemFormButton extends Button {
     }
 
     public function handleSubmit(Player $player): void {
-        $player->sendForm(new SellBuyForm($this->itemId, LevelShopAPI::getInstance()->getBuy($this->itemId), LevelShopAPI::getInstance()->getBuy($this->itemId)));
+        if (LevelShopAPI::getInstance()->getSell($this->itemId) === 0) {//売却値が0だった時選択がそもそもスキップされるように
+            $item = ItemFactory::getInstance()->get($this->itemId);
+            StackStorageAPI::$instance->getCount($player->getXuid(), $item, function ($count) use ($player, $item) {
+                $this->callback($player, $item, $count);
+            }, function () use ($player, $item) {
+                $this->callback($player, $item, 0);
+            });
+        }
+        $player->sendForm(new SellBuyForm($this->itemId, LevelShopAPI::getInstance()->getBuy($this->itemId), LevelShopAPI::getInstance()->getSell($this->itemId)));
+    }
+
+    public function callback(Player $player, Item $item, int $strage): void {
+        $count = 0;
+        $mymoney = EconomyAPI::getInstance()->mymoney($player);
+        foreach ($player->getInventory()->getContents() as $v) {
+            if ($item->getId() === $v->getId() && $item->getMeta() === $v->getMeta()) {
+                $count += $v->getCount();
+            }
+        }
+        $player->sendForm(new PurchaseForm($item, LevelShopAPI::getInstance()->getBuy($this->itemId), $count, $mymoney, $strage));
     }
 }
