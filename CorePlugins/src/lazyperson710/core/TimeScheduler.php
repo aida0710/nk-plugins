1a61fa0bd74b7fc1015143af4d224a2381dbe175
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-20 12:08:52 +0900

    リファクタリング

--- CorePlugins/src/lazyperson710/core/TimeScheduler.php
+++ CorePlugins/src/lazyperson710/core/TimeScheduler.php
@@ -5,6 +5,7 @@ namespace lazyperson710\core;
 use onebone\economyapi\EconomyAPI;
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
 
@@ -24,108 +25,81 @@ class TimeScheduler extends Task {
                 break;
         }
         foreach (Server::getInstance()->getOnlinePlayers() as $player) {
-            if (!Server::getInstance()->isOp($player->getName())){
-                if ($player->getHungerManager()->getFood() <= 6){
+            if (!Server::getInstance()->isOp($player->getName())) {
+                if ($player->getHungerManager()->getFood() <= 6) {
                     $effect = new EffectInstance(VanillaEffects::SLOWNESS(), 40, 0, false);
                     $player->getEffects()->add($effect);
                 }
-                if ($player->getHungerManager()->getFood() <= 3){
+                if ($player->getHungerManager()->getFood() <= 3) {
                     $effect = new EffectInstance(VanillaEffects::SLOWNESS(), 40, 0, false);
                     $player->getEffects()->add($effect);
                     $effect = new EffectInstance(VanillaEffects::MINING_FATIGUE(), 40, 0, false);
                     $player->getEffects()->add($effect);
                 }
             }
-
-            $checkHASTE = $player->getEffects()->get(VanillaEffects::HASTE());
-            $checkNIGHT_VISION = $player->getEffects()->get(VanillaEffects::NIGHT_VISION());
-            $checkSATURATION = $player->getEffects()->get(VanillaEffects::SATURATION());
             switch ($count) {
                 case 8:
                     $bonus = "{$color}同時ログイン数8以上の為\n暗視エフェクト付与&毎秒1円贈与中！";
-                    if ($checkNIGHT_VISION === null) {
-                        $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    } elseif ($checkNIGHT_VISION->getDuration() < 299) {
-                        $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    }
+                    $this->is_NightVision($player);
                     EconomyAPI::getInstance()->addMoney($player, 1);
                     Server::getInstance()->broadcastPopup($bonus);
                     break;
                 case 9:
+                    $this->is_NightVision($player);
+                    $this->is_Saturation($player);
                     $bonus = "{$color}同時ログイン数9以上の為\n満腹&暗視エフェクト付与&毎秒1円贈与中！";
-                    if ($checkNIGHT_VISION === null) {
-                        $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    } elseif ($checkNIGHT_VISION->getDuration() < 299) {
-                        $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    }
-                    if ($checkSATURATION === null) {
-                        $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    } elseif ($checkSATURATION->getDuration() < 299) {
-                        $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    }
                     EconomyAPI::getInstance()->addMoney($player, 1);
                     Server::getInstance()->broadcastPopup($bonus);
                     break;
                 case 10:
                 case 11:
+                    $this->is_Haste($player);
+                    $this->is_NightVision($player);
+                    $this->is_Saturation($player);
                     $bonus = "{$color}同時ログイン数10以上の為\n採掘上昇&満腹&暗視エフェクト付与&毎秒3円贈与中！";
-                    if ($checkHASTE === null) {
-                        $effect = new EffectInstance(VanillaEffects::HASTE(), 300, 1, false);
-                        $player->getEffects()->add($effect);
-                    } elseif ($checkHASTE->getDuration() < 299) {
-                        $effect = new EffectInstance(VanillaEffects::HASTE(), 300, 1, false);
-                        $player->getEffects()->add($effect);
-                    }
-                    if ($checkNIGHT_VISION === null) {
-                        $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    } elseif ($checkNIGHT_VISION->getDuration() < 299) {
-                        $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    }
-                    if ($checkSATURATION === null) {
-                        $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    } elseif ($checkSATURATION->getDuration() < 299) {
-                        $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
-                        $player->getEffects()->add($effect);
-                    }
                     EconomyAPI::getInstance()->addMoney($player, 3);
                     Server::getInstance()->broadcastPopup($bonus);
                     break;
             }
             if ($count >= 12) {
+                $this->is_Haste($player);
+                $this->is_NightVision($player);
+                $this->is_Saturation($player);
                 $bonus = "{$color}同時ログイン数12以上の為\n採掘上昇&満腹&暗視エフェクト付与&毎秒5円贈与中！";
-                if ($checkHASTE === null) {
-                    $effect = new EffectInstance(VanillaEffects::HASTE(), 300, 1, false);
-                    $player->getEffects()->add($effect);
-                } elseif ($checkHASTE->getDuration() < 299) {
-                    $effect = new EffectInstance(VanillaEffects::HASTE(), 300, 1, false);
-                    $player->getEffects()->add($effect);
-                }
-                if ($checkNIGHT_VISION === null) {
-                    $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                    $player->getEffects()->add($effect);
-                } elseif ($checkNIGHT_VISION->getDuration() < 299) {
-                    $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                    $player->getEffects()->add($effect);
-                }
-                if ($checkSATURATION === null) {
-                    $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
-                    $player->getEffects()->add($effect);
-                } elseif ($checkSATURATION->getDuration() < 299) {
-                    $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
-                    $player->getEffects()->add($effect);
-                }
                 EconomyAPI::getInstance()->addMoney($player, 5);
                 Server::getInstance()->broadcastPopup($bonus);
             }
         }
     }
+
+    private function is_Haste(Player $player) {
+        if ($player->getEffects()->get(VanillaEffects::HASTE()) === null) {
+            $effect = new EffectInstance(VanillaEffects::HASTE(), 300, 1, false);
+            $player->getEffects()->add($effect);
+        } elseif ($player->getEffects()->get(VanillaEffects::HASTE())->getDuration() < 299) {
+            $effect = new EffectInstance(VanillaEffects::HASTE(), 300, 1, false);
+            $player->getEffects()->add($effect);
+        }
+    }
+
+    private function is_Saturation(Player $player) {
+        if ($player->getEffects()->get(VanillaEffects::SATURATION()) === null) {
+            $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
+            $player->getEffects()->add($effect);
+        } elseif ($player->getEffects()->get(VanillaEffects::SATURATION())->getDuration() < 299) {
+            $effect = new EffectInstance(VanillaEffects::SATURATION(), 300, 0, false);
+            $player->getEffects()->add($effect);
+        }
+    }
+
+    private function is_NightVision(Player $player) {
+        if ($player->getEffects()->get(VanillaEffects::NIGHT_VISION()) === null) {
+            $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
+            $player->getEffects()->add($effect);
+        } elseif ($player->getEffects()->get(VanillaEffects::NIGHT_VISION())->getDuration() < 299) {
+            $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 0, false);
+            $player->getEffects()->add($effect);
+        }
+    }
+
 }
