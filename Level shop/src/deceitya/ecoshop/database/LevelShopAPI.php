af8a440e76a4ddde0cd473a647ce769bb7ad0fe5
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-21 18:18:41 +0900

    databaseの整理

--- Level shop/src/deceitya/ecoshop/database/LevelShopAPI.php
+++ Level shop/src/deceitya/ecoshop/database/LevelShopAPI.php
@@ -29,8 +29,13 @@ class LevelShopAPI {
     }
 
     protected function init(): void {
-        #Others
-        $this->register(VanillaItems::WRITABLE_BOOK(), 50, 0, 1);##Book & Quill
+        /*Shop1*/
+        #Currency
+        $this->register(VanillaItems::NETHER_STAR(), 1000, 1000, 1);##Nether Star
+        #Foods
+        $this->register(VanillaItems::STEAK(), 50, 30, 1);##Steak
+        $this->register(VanillaItems::BREAD(), 250, 15, 1);##Bread
+        $this->register(VanillaBlocks::CAKE()->asItem(), 1500, 50, 1);##Cake
         #Logs
         $this->register(VanillaBlocks::OAK_LOG()->asItem(), 25, 15, 1);##Oak Log
         $this->register(VanillaBlocks::SPRUCE_LOG()->asItem(), 25, 15, 1);##Spruce Log
@@ -38,26 +43,6 @@ class LevelShopAPI {
         $this->register(VanillaBlocks::JUNGLE_LOG()->asItem(), 25, 15, 1);##Jungle Log
         $this->register(VanillaBlocks::ACACIA_LOG()->asItem(), 25, 15, 1);##Acacia Log
         $this->register(VanillaBlocks::DARK_OAK_LOG()->asItem(), 25, 15, 1);##Dark Oak Log
-        #Stones
-        $this->register(VanillaBlocks::DIRT()->asItem(), 25, 1, 1);##Dirt
-        $this->register(VanillaBlocks::STONE()->asItem(), 25, 5, 1);##Stone
-        $this->register(VanillaBlocks::COBBLESTONE()->asItem(), 25, 3, 1);##Cobblestone
-        $this->register(VanillaBlocks::GRANITE()->asItem(), 25, 3, 1);##Granite
-        $this->register(VanillaBlocks::DIORITE()->asItem(), 25, 3, 1);##Diorite
-        $this->register(VanillaBlocks::ANDESITE()->asItem(), 25, 3, 1);##Andesite
-        $this->register(VanillaBlocks::SAND()->asItem(), 15, 1, 1);##Sand
-        $this->register(VanillaBlocks::SANDSTONE()->asItem(), 15, 1, 1);##Sandstone
-        $this->register(VanillaBlocks::GRAVEL()->asItem(), 15, 1, 1);##Gravel
-        #Currency
-        $this->register(VanillaItems::NETHER_STAR(), 1000, 1000, 1);##Nether Star
-        #Tools
-        $this->register(VanillaItems::IRON_PICKAXE(), 150, 0, 1);##Iron Pickaxe
-        $this->register(VanillaItems::IRON_SHOVEL(), 150, 0, 1);##Iron Shovel
-        $this->register(VanillaItems::IRON_AXE(), 150, 0, 1);##Iron Axe
-        $this->register(VanillaItems::DIAMOND_PICKAXE(), 250, 0, 1);##Diamond Pickaxe
-        $this->register(VanillaItems::DIAMOND_SHOVEL(), 250, 0, 1);##Diamond Shovel
-        $this->register(VanillaItems::DIAMOND_AXE(), 250, 0, 1);##Diamond Axe
-        $this->register(VanillaItems::SHEARS(), 15000, 0, 1);##Shears
         #Ores
         $this->register(VanillaItems::COAL(), 75, 15, 1);##Coal
         $this->register(VanillaBlocks::IRON_ORE()->asItem(), 750, 150, 1);##Iron Ore
@@ -68,10 +53,27 @@ class LevelShopAPI {
         $this->register(VanillaItems::REDSTONE_DUST(), 75, 15, 1);##Redstone
         $this->register(VanillaItems::DIAMOND(), 4500, 800, 1);##Diamond
         $this->register(VanillaItems::EMERALD(), 15000, 3000, 1);##Emerald
-        #Foods
-        $this->register(VanillaItems::STEAK(), 50, 30, 1);##Steak
-        $this->register(VanillaItems::BREAD(), 250, 15, 1);##Bread
-        $this->register(VanillaBlocks::CAKE()->asItem(), 1500, 50, 1);##Cake
+        #Others
+        $this->register(VanillaItems::WRITABLE_BOOK(), 50, 0, 1);##Book & Quill
+        #Stones
+        $this->register(VanillaBlocks::DIRT()->asItem(), 25, 1, 1);##Dirt
+        $this->register(VanillaBlocks::STONE()->asItem(), 25, 5, 1);##Stone
+        $this->register(VanillaBlocks::COBBLESTONE()->asItem(), 25, 3, 1);##Cobblestone
+        $this->register(VanillaBlocks::GRANITE()->asItem(), 25, 3, 1);##Granite
+        $this->register(VanillaBlocks::DIORITE()->asItem(), 25, 3, 1);##Diorite
+        $this->register(VanillaBlocks::ANDESITE()->asItem(), 25, 3, 1);##Andesite
+        $this->register(VanillaBlocks::SAND()->asItem(), 15, 1, 1);##Sand
+        $this->register(VanillaBlocks::SANDSTONE()->asItem(), 15, 1, 1);##Sandstone
+        $this->register(VanillaBlocks::GRAVEL()->asItem(), 15, 1, 1);##Gravel
+        #Tools
+        $this->register(VanillaItems::IRON_PICKAXE(), 150, 0, 1);##Iron Pickaxe
+        $this->register(VanillaItems::IRON_SHOVEL(), 150, 0, 1);##Iron Shovel
+        $this->register(VanillaItems::IRON_AXE(), 150, 0, 1);##Iron Axe
+        $this->register(VanillaItems::DIAMOND_PICKAXE(), 250, 0, 1);##Diamond Pickaxe
+        $this->register(VanillaItems::DIAMOND_SHOVEL(), 250, 0, 1);##Diamond Shovel
+        $this->register(VanillaItems::DIAMOND_AXE(), 250, 0, 1);##Diamond Axe
+        $this->register(VanillaItems::SHEARS(), 15000, 0, 1);##Shears
+        /*Shop2*/
         #Crop
         $this->register(VanillaItems::WHEAT(), 250, 8, 1);##Wheat
         $this->register(VanillaItems::POTATO(), 250, 8, 1);##Potato
@@ -83,34 +85,16 @@ class LevelShopAPI {
         $this->register(VanillaItems::APPLE(), 250, 8, 1);##Apple
         $this->register(VanillaBlocks::MELON()->asItem(), 500, 6, 1);##Melon Block
         $this->register(VanillaBlocks::PUMPKIN()->asItem(), 500, 6, 1);##Pumpkin
+        #FarmingTools
+        $this->register(VanillaBlocks::WATER()->asItem(), 800, 150, 1);##Water
+        $this->register(VanillaBlocks::FARMLAND()->asItem(), 50, 0, 1);##Farmland
+        $this->register(VanillaItems::DIAMOND_HOE(), 15000, 0, 1);##Diamond Hoe
         #Seeds
         $this->register(VanillaItems::WHEAT_SEEDS(), 250, 3, 1);##Wheat Seeds
         $this->register(VanillaItems::BEETROOT_SEEDS(), 250, 3, 1);##Beetroot Seeds
         $this->register(VanillaItems::PUMPKIN_SEEDS(), 250, 3, 1);##Pumpkin Seeds
         $this->register(VanillaItems::MELON_SEEDS(), 250, 3, 1);##Melon Seeds
-        #FarmingTools
-        $this->register(VanillaBlocks::WATER()->asItem(), 800, 150, 1);##Water
-        $this->register(VanillaBlocks::FARMLAND()->asItem(), 50, 0, 1);##Farmland
-        $this->register(VanillaItems::DIAMOND_HOE(), 15000, 0, 1);##Diamond Hoe
-        #OtherBlocks3
-        $this->register(VanillaBlocks::PACKED_ICE()->asItem(), 50, 0, 1);##Packed Ice
-        $this->register(VanillaBlocks::OBSIDIAN()->asItem(), 50, 5, 1);##Obsidian
-        $this->register(VanillaBlocks::END_ROD()->asItem(), 50, 0, 1);##End Rod
-        $this->register(VanillaBlocks::ANVIL()->asItem(), 150, 0, 1);##Anvil
-        $this->register(VanillaBlocks::SHULKER_BOX()->asItem(), 3000, 0, 1);##Shulker Box
-        $this->register(VanillaBlocks::SLIME()->asItem(), 50, 0, 1);##Slime Block
-        $this->register(VanillaBlocks::BOOKSHELF()->asItem(), 50, 0, 1);##Bookshelf
-        $this->register(VanillaBlocks::COBWEB()->asItem(), 50, 0, 1);##Cobweb
-        $this->register(VanillaBlocks::BLAST_FURNACE()->asItem(), 250, 0, 1);##Blast Furnace
-        $this->register(VanillaBlocks::SMOKER()->asItem(), 250, 0, 1);##Smoker
-        $this->register(VanillaBlocks::LECTERN()->asItem(), 2500, 0, 1);##Lectern
-        #Ores
-        $this->register(VanillaBlocks::COAL_ORE()->asItem(), 50000, 15, 1);##Coal Ore
-        $this->register(VanillaBlocks::LAPIS_LAZULI_ORE()->asItem(), 50000, 40, 1);##Lapis Lazuli Ore
-        $this->register(VanillaBlocks::REDSTONE_ORE()->asItem(), 50000, 15, 1);##Redstone Ore
-        $this->register(VanillaBlocks::DIAMOND_ORE()->asItem(), 50000, 800, 1);##Diamond Ore
-        $this->register(VanillaBlocks::NETHER_QUARTZ_ORE()->asItem(), 50000, 30, 1);##Nether Quartz Ore
-        $this->register(VanillaBlocks::EMERALD_ORE()->asItem(), 50000, 3000, 1);##Emerald Ore
+        /*Shop3*/
         #BuildingMaterials
         $this->register(VanillaBlocks::STONE_BRICKS()->asItem(), 25, 5, 1);##Stone Bricks
         $this->register(VanillaBlocks::BRICKS()->asItem(), 25, 0, 1);##Bricks
@@ -146,6 +130,28 @@ class LevelShopAPI {
         $this->register(VanillaItems::PURPLE_DYE(), 5, 0, 1);##Purple Dye
         $this->register(VanillaItems::MAGENTA_DYE(), 5, 0, 1);##Magenta Dye
         $this->register(VanillaItems::PINK_DYE(), 5, 0, 1);##Pink Dye
+        #Ores
+        $this->register(VanillaBlocks::COAL_ORE()->asItem(), 50000, 15, 1);##Coal Ore
+        $this->register(VanillaBlocks::LAPIS_LAZULI_ORE()->asItem(), 50000, 40, 1);##Lapis Lazuli Ore
+        $this->register(VanillaBlocks::REDSTONE_ORE()->asItem(), 50000, 15, 1);##Redstone Ore
+        $this->register(VanillaBlocks::DIAMOND_ORE()->asItem(), 50000, 800, 1);##Diamond Ore
+        $this->register(VanillaBlocks::NETHER_QUARTZ_ORE()->asItem(), 50000, 30, 1);##Nether Quartz Ore
+        $this->register(VanillaBlocks::EMERALD_ORE()->asItem(), 50000, 3000, 1);##Emerald Ore
+        #OtherBlocks3
+        $this->register(VanillaBlocks::PACKED_ICE()->asItem(), 50, 0, 1);##Packed Ice
+        $this->register(VanillaBlocks::OBSIDIAN()->asItem(), 50, 5, 1);##Obsidian
+        $this->register(VanillaBlocks::END_ROD()->asItem(), 50, 0, 1);##End Rod
+        $this->register(VanillaBlocks::ANVIL()->asItem(), 150, 0, 1);##Anvil
+        $this->register(VanillaBlocks::SHULKER_BOX()->asItem(), 3000, 0, 1);##Shulker Box
+        $this->register(VanillaBlocks::SLIME()->asItem(), 50, 0, 1);##Slime Block
+        $this->register(VanillaBlocks::BOOKSHELF()->asItem(), 50, 0, 1);##Bookshelf
+        $this->register(VanillaBlocks::COBWEB()->asItem(), 50, 0, 1);##Cobweb
+        $this->register(VanillaBlocks::BLAST_FURNACE()->asItem(), 250, 0, 1);##Blast Furnace
+        $this->register(VanillaBlocks::SMOKER()->asItem(), 250, 0, 1);##Smoker
+        $this->register(VanillaBlocks::LECTERN()->asItem(), 2500, 0, 1);##Lectern
+        /*Shop4*/
+        #Elytra
+        $this->registerFromId(444, 0, 3500000, 0, 1);##Elytra
         #OtherBlocks4
         $this->register(VanillaBlocks::GRASS()->asItem(), 10, 1, 1);##Grass
         $this->register(VanillaBlocks::PODZOL()->asItem(), 10, 0, 1);##Podzol
@@ -155,8 +161,6 @@ class LevelShopAPI {
         $this->register(VanillaBlocks::SMOOTH_QUARTZ()->asItem(), 10, 3, 1);##Smooth Quartz Block
         $this->register(VanillaBlocks::SMOOTH_SANDSTONE()->asItem(), 10, 0, 1);##Smooth Sandstone
         $this->register(VanillaBlocks::SMOOTH_RED_SANDSTONE()->asItem(), 10, 0, 1);##Smooth Red Sandstone
-        #Elytra
-        $this->registerFromId(444, 0, 3500000, 0, 1);##Elytra
         #Weapon
         $this->register(VanillaItems::IRON_SWORD(), 300, 0, 1);##Iron Sword
         $this->register(VanillaItems::DIAMOND_SWORD(), 800, 0, 1);##Diamond Sword
@@ -166,13 +170,7 @@ class LevelShopAPI {
         $this->register(VanillaItems::EGG(), 10, 0, 1);##Egg
         $this->registerFromId(513, 0, 500, 0, 1);##Shield
         $this->registerFromId(772, 0, 300000, 0, 1);##Spyglass
-        #OtherItems
-        $this->registerFromId(-228, 0, 50, 0, 1);##Crimson Fungus
-        $this->registerFromId(-229, 0, 50, 0, 1);##Warped Fungus
-        $this->registerFromId(-231, 0, 50, 0, 1);##Weeping Vines
-        $this->registerFromId(-287, 0, 50, 0, 1);##Twisting Vines
-        $this->registerFromId(-223, 0, 50, 0, 1);##Crimson Roots
-        $this->registerFromId(-224, 0, 50, 0, 1);##Warped Roots
+        /*Shop5*/
         #NetherStones
         $this->registerFromId(-273, 0, 50, 0, 1);##Blackstone
         $this->registerFromId(-234, 0, 50, 0, 1);##Basalt
@@ -189,6 +187,31 @@ class LevelShopAPI {
         $this->registerFromId(-227, 0, 50, 0, 1);##Warped Wart Block
         $this->registerFromId(-289, 0, 50, 0, 1);##Crying Obsidian
         $this->registerFromId(-272, 0, 50, 0, 1);##Respawn Anchor
+        #OtherItems
+        $this->registerFromId(-228, 0, 50, 0, 1);##Crimson Fungus
+        $this->registerFromId(-229, 0, 50, 0, 1);##Warped Fungus
+        $this->registerFromId(-231, 0, 50, 0, 1);##Weeping Vines
+        $this->registerFromId(-287, 0, 50, 0, 1);##Twisting Vines
+        $this->registerFromId(-223, 0, 50, 0, 1);##Crimson Roots
+        $this->registerFromId(-224, 0, 50, 0, 1);##Warped Roots
+        /*Shop6*/
+        #DecorativeBlock
+        $this->registerFromId(720, 0, 80000, 1, 1);##Campfire
+        $this->registerFromId(801, 0, 80000, 0, 1);##Soul Campfire
+        $this->registerFromId(-268, 0, 50, 0, 1);##Soul Torch
+        $this->register(VanillaBlocks::LANTERN()->asItem(), 50, 0, 1);##Lantern
+        $this->registerFromId(-269, 0, 50, 0, 1);##Soul Lantern
+        $this->register(VanillaBlocks::SEA_PICKLE()->asItem(), 50, 0, 1);##Sea Pickle
+        $this->registerFromId(758, 0, 50, 0, 1);##Chain
+        $this->register(VanillaBlocks::BELL()->asItem(), 150000, 0, 1);##Bell
+        $this->register(VanillaBlocks::BEACON()->asItem(), 300000, 0, 1);##Beacon
+        #Heads
+        $this->register(VanillaItems::PLAYER_HEAD(), 800000, 0, 1);##Player Head
+        $this->register(VanillaItems::ZOMBIE_HEAD(), 800000, 0, 1);##Zombie Head
+        $this->register(VanillaItems::SKELETON_SKULL(), 800000, 0, 1);##Skeleton Skull
+        $this->register(VanillaItems::CREEPER_HEAD(), 800000, 0, 1);##Creeper Head
+        $this->register(VanillaItems::WITHER_SKELETON_SKULL(), 800000, 0, 1);##Creeper Head
+        $this->register(VanillaItems::DRAGON_HEAD(), 800000, 0, 1);##Dragon Head
         #Vegetation
         $this->register(VanillaBlocks::DANDELION()->asItem(), 30, 0, 1);##Dandelion
         $this->register(VanillaBlocks::POPPY()->asItem(), 30, 0, 1);##Poppy
@@ -214,23 +237,7 @@ class LevelShopAPI {
         $this->register(VanillaBlocks::DEAD_BUSH()->asItem(), 30, 0, 1);##Dead Bush
         $this->register(VanillaBlocks::LILY_PAD()->asItem(), 30, 0, 1);##Lily Pad
         $this->register(VanillaBlocks::VINES()->asItem(), 30, 0, 1);##Vines
-        #DecorativeBlock
-        $this->registerFromId(720, 0, 80000, 1, 1);##Campfire
-        $this->registerFromId(801, 0, 80000, 0, 1);##Soul Campfire
-        $this->registerFromId(-268, 0, 50, 0, 1);##Soul Torch
-        $this->register(VanillaBlocks::LANTERN()->asItem(), 50, 0, 1);##Lantern
-        $this->registerFromId(-269, 0, 50, 0, 1);##Soul Lantern
-        $this->register(VanillaBlocks::SEA_PICKLE()->asItem(), 50, 0, 1);##Sea Pickle
-        $this->registerFromId(758, 0, 50, 0, 1);##Chain
-        $this->register(VanillaBlocks::BELL()->asItem(), 150000, 0, 1);##Bell
-        $this->register(VanillaBlocks::BEACON()->asItem(), 300000, 0, 1);##Beacon
-        #Heads
-        $this->register(VanillaItems::PLAYER_HEAD(), 800000, 0, 1);##Player Head
-        $this->register(VanillaItems::ZOMBIE_HEAD(), 800000, 0, 1);##Zombie Head
-        $this->register(VanillaItems::SKELETON_SKULL(), 800000, 0, 1);##Skeleton Skull
-        $this->register(VanillaItems::CREEPER_HEAD(), 800000, 0, 1);##Creeper Head
-        $this->register(VanillaItems::WITHER_SKELETON_SKULL(), 800000, 0, 1);##Creeper Head
-        $this->register(VanillaItems::DRAGON_HEAD(), 800000, 0, 1);##Dragon Head
+        /*Shop7*/
         #OtherBlocks7
         $this->register(VanillaBlocks::ITEM_FRAME()->asItem(), 25000, 0, 1);##Item Frame
         $this->register(VanillaBlocks::FLETCHING_TABLE()->asItem(), 25000, 0, 1);##Fletching Table
