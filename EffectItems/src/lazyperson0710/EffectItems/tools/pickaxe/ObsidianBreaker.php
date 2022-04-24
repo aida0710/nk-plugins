ff3a8dbbe44ee7cfa799dc71f2fdd2608e92f71f
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-25 01:03:49 +0900

    クラスをツール単位で分割&判定本式を変更

---
+++ EffectItems/src/lazyperson0710/EffectItems/tools/pickaxe/ObsidianBreaker.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace lazyperson0710\EffectItems\tools\pickaxe;
+
+use pocketmine\block\BlockLegacyIds;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class ObsidianBreaker implements Listener {
+
+    public function onInteract(PlayerInteractEvent $event): void {
+        if ($event->isCancelled()) return;
+        $player = $event->getPlayer();
+        $inHand = $player->getInventory()->getItemInHand();
+        $block = $event->getBlock();
+        $position = $player->getPosition();
+        if ($inHand->getNamedTag()->getTag('ObsidianBreaker') !== null) {//ObsidianBreaker
+            $pos = $block->getPosition()->add($position->getFloorX(), $position->getFloorY(), $position->getFloorZ());
+            if ($event->getBlock()->getId() === BlockLegacyIds::OBSIDIAN) {
+                $block->getPosition()->getWorld()->useBreakOn($pos, $inHand, $player);
+            }
+        }
+    }
+
+    public function onBreak(BlockBreakEvent $event){
+        if ($event->isCancelled()) return;
+        $player = $event->getPlayer();
+        $inHand = $player->getInventory()->getItemInHand();
+        if ($inHand->getNamedTag()->getTag('ObsidianBreaker') !== null) {//ObsidianBreaker
+            $event->cancel();
+        }
+    }
+}
---
+++ EffectItems/src/lazyperson0710/EffectItems/tools/pickaxe/OldPickaxe.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lazyperson0710\EffectItems\tools\pickaxe;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+
+class OldPickaxe implements Listener {
+
+    public function onBreak(BlockBreakEvent $event): void {
+        if ($event->isCancelled()) return;
+        $player = $event->getPlayer();
+        $inHand = $player->getInventory()->getItemInHand();
+        $namedTag = $player->getInventory()->getItemInHand()->getNamedTag();
+        if ($namedTag->getTag('OldPickaxe') !== null) {//OldPickaxe
+            if (rand(1, 50) === 50) {
+                $player->getInventory()->removeItem($inHand);
+                $sound = new PlaySoundPacket();
+                $sound->soundName = "random.break";
+                $sound->x = $player->getPosition()->getX();
+                $sound->y = $player->getPosition()->getY();
+                $sound->z = $player->getPosition()->getZ();
+                $sound->volume = 1;
+                $sound->pitch = 1;
+                $player->getNetworkSession()->sendDataPacket($sound);
+            }
+        }
+    }
+}
