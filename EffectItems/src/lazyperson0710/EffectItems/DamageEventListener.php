4fa405d00a2d6b3963a7fe28eaf0eab7901c73d0
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-27 15:41:35 +0900

    ダメージ無効系のアイテムを作成

--- EffectItems/src/lazyperson0710/EffectItems/DamageEventListener.php
+++ EffectItems/src/lazyperson0710/EffectItems/DamageEventListener.php
@@ -6,17 +6,30 @@ use pocketmine\entity\object\ItemEntity;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class DamageEventListener implements Listener {
 
     public function onDamage(EntityDamageEvent $event) {
-        $player = $event->getEntity();
+        $item = $event->getEntity();
         //todo 落下ダメージ無効化ブーツ
         //todo 守りの石的な感じで何度かダメージを完全に無効化
-        if (!$event->isCancelled() && $event->getEntity() instanceof ItemEntity && ($event->getCause() === EntityDamageEvent::CAUSE_FIRE || $event->getCause() === EntityDamageEvent::CAUSE_LAVA)) {
+        if (!$event->isCancelled()) return;
+        if (!$event->getEntity() instanceof ItemEntity) return;
+        if (($event->getCause() === EntityDamageEvent::CAUSE_FIRE || $event->getCause() === EntityDamageEvent::CAUSE_LAVA)) {
             /** @var ItemEntity $item */
-            $item = $event->getEntity();
-            if ($item->getItem()->getId() === 384) {
+            if ($item->getItem()->getNamedTag()->getTag('FireResistancePickaxe') !== null) {//FireResistancePickaxe
+                $event->cancel();
+            }
+        }
+        if (($event->getCause() === EntityDamageEvent::CAUSE_FALL)){
+            /** @var ItemEntity $item */
+            if ($item->getItem()->getNamedTag()->getTag('DefensiveStone') !== null) {//DefensiveStone
+                var_dump(0);
+                if (!Server::getInstance()->getPlayerExact($event->getEntity()->getTargetEntity()->getNameTag()) == Player::class){
+                    var_dump(1);
+                    return;
+                }
                 $event->cancel();
             }
         }
