2751451ed66072def43f0d45240038f5208a6f2b
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-27 10:24:02 +0900

    いらん文を削除

--- EffectItems/src/lazyperson0710/EffectItems/PlayerBlockBreakEvent.php
+++ EffectItems/src/lazyperson0710/EffectItems/PlayerBlockBreakEvent.php
@@ -39,87 +39,9 @@ class PlayerBlockBreakEvent implements Listener {
 
         }
         if ($inHand->getId() === 1511) {//アルケミピッケル
-            foreach ($event->getDrops() as $item) {
-                switch ($item->getId()) {
-                    case BlockLegacyIds::COBBLESTONE:
-                    case BlockLegacyIds::STONE:
-                        if (rand(1, 500) === 5) {
-                            if ($inHand->hasEnchantment(EnchantmentIdMap::getInstance()->fromId(EnchantmentIds::FORTUNE))) {
-                                $fortune = rand(1, 3);
-                            } else {
-                                $fortune = 1;
-                            }
-                            $drop = [VanillaBlocks::STONE()->asItem()];
-                            switch (rand(1, 8)) {
-                                case 1:
-                                    $drop = [VanillaItems::COAL()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> 石炭");
-                                    break;
-                                case 2:
-                                    $drop = [VanillaItems::IRON_INGOT()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> 鉄インゴット");
-                                    break;
-                                case 3:
-                                    $drop = [VanillaItems::GOLD_INGOT()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> 金インゴット");
-                                    break;
-                                case 4:
-                                    $drop = [VanillaItems::REDSTONE_DUST()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> レッドストーン");
-                                    break;
-                                case 5:
-                                    $drop = [VanillaItems::LAPIS_LAZULI()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> ラピスラズリ");
-                                    break;
-                                case 6:
-                                    $drop = [VanillaItems::DIAMOND()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> ダイヤモンド");
-                                    break;
-                                case 7:
-                                    $drop = [VanillaItems::EMERALD()->setCount($fortune)];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石を発見したみたい！発見した鉱石 -> エメラルド");
-                                    break;
-                                case 8:
-                                    $drop = [VanillaBlocks::OBSIDIAN()->asItem()];
-                                    $event->getPlayer()->sendMessage("§bMining §7>> §a鉱石?を発見したみたい！発見した鉱石? -> 黒曜石");
-                                    break;
-                            }
-                            $event->setDrops($drop);
-                        }
-                        break;
-                }
-            }
-            return;
+
         }
         if ($inHand->getId() === 1512) {//鉱石より愛を込めて
-            if ($event->getPlayer()->getInventory()->getItemInHand()->hasEnchantment(EnchantmentIdMap::getInstance()->fromId(EnchantmentIds::UNBREAKING))) {
-                $event->cancel();
-                $event->getPlayer()->sendMessage("§bEnchant §7>> §cこのアイテムにはシルクタッチは使用できません");
-            }
-            switch ($event->getBlock()->getId()) {
-                case BlockLegacyIds::COAL_ORE:
-                case BlockLegacyIds::NETHER_QUARTZ_ORE:
-                    //何かやりたい
-                    break;
-                case BlockLegacyIds::IRON_ORE:
-                case BlockLegacyIds::GOLD_ORE:
-                    //何かやりたい
-                    break;
-                case BlockLegacyIds::LAPIS_ORE:
-                case BlockLegacyIds::REDSTONE_ORE:
-                case BlockLegacyIds::LIT_REDSTONE_ORE:
-                    //何かやりたい
-                    break;
-                case BlockLegacyIds::DIAMOND_ORE:
-                    //何かやりたい
-                    break;
-                case BlockLegacyIds::EMERALD_ORE:
-                    if (rand(1, 15) === 3) {
-                        //何かやりたい
-                    }
-                    break;
-            }
-            return;
         }
         if ($inHand->getId() === 1513) {//ハイドロゲル
             if ($inHand->getId() === ItemIds::WOODEN_PICKAXE) {
