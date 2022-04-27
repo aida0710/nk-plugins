5cb2b22307f593d9a1cd3e6b8284b7743832ef3b
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-04-27 21:53:38 +0900

    落下ダメージ無効化アイテム機能を作った

--- EffectItems/src/lazyperson0710/EffectItems/DamageEventListener.php
+++ EffectItems/src/lazyperson0710/EffectItems/DamageEventListener.php
@@ -5,6 +5,7 @@ namespace lazyperson0710\EffectItems;
 use pocketmine\entity\object\ItemEntity;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
+use pocketmine\item\Durable;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
@@ -23,16 +24,29 @@ class DamageEventListener implements Listener {
             }
         }
         if (($event->getCause() === EntityDamageEvent::CAUSE_FALL)) {
-            $player = Server::getInstance()->getPlayerExact($event->getEntity()->getTargetEntity()->getNameTag());
             if (!$event->getEntity() instanceof Player) return;
-            if (!Server::getInstance()->getPlayerExact($event->getEntity()->getTargetEntity()->getNameTag()) === null) {
-                var_dump(1);
+            $playerName = $event->getEntity()->getName();
+            try {
+                if ($player = Server::getInstance()->getPlayerExact($playerName)) {
+                    $armorInventory = $player->getArmorInventory();
+                    for ($i = 0, $size = $armorInventory->getSize(); $i < $size; ++$i) {
+                        $item = clone $armorInventory->getItem($i);
+                        if ($item->getNamedTag()->getTag('DefensiveStone') !== null) {//DefensiveStone
+                            if ($item instanceof Durable) {
+                                $damage = $event->getBaseDamage() * 2;
+                                $item->applyDamage($damage);
+                                $armorInventory->setItem($i, $item);
+                                $player->sendTip("§bDurable §7>> §a落下ダメージが無効化されました！耐久 -{$damage}");
+                                $event->cancel();
+                            }
+                        }
+                    }
+                }
+            } catch (\Exception $exception) {
+                var_dump($exception);
                 return;
             }
-            if ($player->getInventory()->getItemInHand()->getNamedTag()->getTag('DefensiveStone') !== null) {//DefensiveStone
-                var_dump(0);
-                $event->cancel();
-            }
+            return;
         }
     }
 }
