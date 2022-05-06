a88f07c0926ebecef529c11a7c3c9464517a32c7
lazyperson0710 <51500566+lazyperson710@users.noreply.github.com>
2022-05-06 11:19:02 +0900

    ログイン時に自動変換するように

--- Ticket/src/lazyperson0710/ticket/EventListener/JoinEventListener.php
+++ Ticket/src/lazyperson0710/ticket/EventListener/JoinEventListener.php
@@ -16,9 +16,14 @@ class JoinEventListener implements Listener {
      * @param PlayerJoinEvent $event
      * @priority HIGHEST
      */
-    public function onBreak(PlayerJoinEvent $event) {
-        $result = TicketAPI::getInstance()->replaceTicket($event->getPlayer());
-        if ($result === false) return;
-        $event->getPlayer()->sendMessage("§bTicket §7>> §a{$result}個のTicketを交換しました");
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        if (TicketAPI::getInstance()->exists($player) !== true) {
+            TicketAPI::getInstance()->createData($event->getPlayer());
+        }
+        if ((TicketAPI::getInstance()->replaceInventoryTicket($player) || TicketAPI::getInstance()->replaceStackStorageTicket($player)) === true) {
+            $player->sendMessage("§bTicket §7>> §aチケットの変換処理を実行しました");
+        }
+        
     }
 }
