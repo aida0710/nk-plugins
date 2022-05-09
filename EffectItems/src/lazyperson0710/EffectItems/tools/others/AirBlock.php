e23506ed0d4bc861eb2453404dca26b6a28e55af
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-05-10 07:31:01 +0900

    airblockの基本実装。要検証

--- EffectItems/src/lazyperson0710/EffectItems/tools/others/AirBlock.php
+++ EffectItems/src/lazyperson0710/EffectItems/tools/others/AirBlock.php
@@ -3,9 +3,10 @@
 namespace lazyperson0710\EffectItems\tools\others;
 
 use onebone\economyland\EconomyLand;
+use pocketmine\block\Block;
+use pocketmine\block\BlockLegacyIds;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemUseEvent;
-use pocketmine\item\ItemFactory;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 
 class AirBlock implements Listener {
@@ -16,11 +17,22 @@ class AirBlock implements Listener {
         $inHand = $player->getInventory()->getItemInHand();
         if ($inHand->getNamedTag()->getTag('AirBlock') !== null) {//AirBlock
             $pos = $player->getPosition()->add(0, -1, 0);
+            $world_name = $event->getPlayer()->getWorld()->getDisplayName();
+            $world_search = mb_substr($world_name, 0, null, 'utf-8');
+            var_dump(1);
+            if (!(str_contains($world_search, "-c") || str_contains($world_search, "-f") || str_contains($world_search, "nature") || str_contains($world_search, "nether") || str_contains($world_search, "end") || str_contains($world_search, "MiningWorld") || str_contains($world_search, "debug"))) {
+                $player->sendMessage("§bAirBlock §7>> §c使用可能なワールドは資源系ワールドと生活ワールドのみとなります");
+            }
+            var_dump(2);
+            if ($inHand->getBlock() != Block::class) return;
+            var_dump(3);
+            if ($player->getPosition()->getWorld()->getBlock($pos) != BlockLegacyIds::AIR) return;
+            var_dump(4);
             if (EconomyLand::getInstance()->posCheck($pos, $player) === true) {
-                $item = ItemFactory::getInstance()->get($inHand->getId(), $inHand->getMeta(), 1);//アイテムが全滅するバグあり
-                $player->getInventory()->remove($item);
+                $item = $inHand;
+                $item->setCount(1);
+                $player->getInventory()->removeItem($item);
                 $player->getPosition()->getWorld()->setBlock($pos, $inHand->getBlock());
-                $player->sendMessage("ブロックを設置しました");
                 $sound = new PlaySoundPacket();
                 $sound->soundName = "dig.stone";
                 $sound->x = $player->getPosition()->getX();
@@ -30,7 +42,7 @@ class AirBlock implements Listener {
                 $sound->pitch = 1;
                 $player->getNetworkSession()->sendDataPacket($sound);
             } else {
-                $player->sendMessage("他人の土地には設置できません");
+                $player->sendMessage("§bAirBlock §7>> §c他人の土地には設置できません");
             }
         }
     }
