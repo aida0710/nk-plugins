0197a2a11fe8e64b143f749bdfcd095c366c7470
sloth <lazyperson0710@gmail.com>
2022-05-10 16:08:25 +0900

    ブロックの判定処理を追加

--- EffectItems/src/lazyperson0710/EffectItems/tools/others/AirBlock.php
+++ EffectItems/src/lazyperson0710/EffectItems/tools/others/AirBlock.php
@@ -19,15 +19,12 @@ class AirBlock implements Listener {
             $pos = $player->getPosition()->add(0, -1, 0);
             $world_name = $event->getPlayer()->getWorld()->getDisplayName();
             $world_search = mb_substr($world_name, 0, null, 'utf-8');
-            var_dump(1);
             if (!(str_contains($world_search, "-c") || str_contains($world_search, "-f") || str_contains($world_search, "nature") || str_contains($world_search, "nether") || str_contains($world_search, "end") || str_contains($world_search, "MiningWorld") || str_contains($world_search, "debug"))) {
                 $player->sendMessage("§bAirBlock §7>> §c使用可能なワールドは資源系ワールドと生活ワールドのみとなります");
             }
-            var_dump(2);
-            if ($inHand->getBlock() != Block::class) return;
-            var_dump(3);
-            if ($player->getPosition()->getWorld()->getBlock($pos) != BlockLegacyIds::AIR) return;
-            var_dump(4);
+            //持っているものがブロックかアイテムかを判定したい
+            if ($inHand->getBlock() === null) return;
+                if ($player->getPosition()->getWorld()->getBlock($pos) != BlockLegacyIds::AIR) return;
             if (EconomyLand::getInstance()->posCheck($pos, $player) === true) {
                 $item = $inHand;
                 $item->setCount(1);
