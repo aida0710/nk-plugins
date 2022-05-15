6f0361a2a39ed55c5156eed57b1e3e8344383f8d
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-05-16 04:55:32 +0900

    更新

---
+++ CSkull/src/ColinHDev/CSkull/CSkull.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ColinHDev\CSkull;
+
+use ColinHDev\CSkull\blocks\Skull as SkullBlock;
+use ColinHDev\CSkull\commands\ShowSkullsCommand;
+use ColinHDev\CSkull\commands\SkullCommand;
+use ColinHDev\CSkull\items\Skull as SkullItem;
+use ColinHDev\CSkull\listeners\BlockPlaceListener;
+use ColinHDev\CSkull\listeners\ChunkLoadListener;
+use ColinHDev\CSkull\listeners\EntityEffectAddListener;
+use ColinHDev\CSkull\listeners\PlayerLoginListener;
+use pocketmine\block\BlockFactory;
+use pocketmine\block\utils\SkullType;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\event\Listener;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemIds;
+use pocketmine\plugin\PluginBase;
+
+class CSkull extends PluginBase implements Listener {
+
+    private static CSkull $instance;
+
+    public static function getInstance(): CSkull {
+        return self::$instance;
+    }
+
+    public function onEnable(): void {
+        self::$instance = $this;
+        $oldSkullBlock = VanillaBlocks::MOB_HEAD();
+        BlockFactory::getInstance()->register(
+            new SkullBlock(
+                $oldSkullBlock->getIdInfo(),
+                $oldSkullBlock->getName(),
+                $oldSkullBlock->getBreakInfo()
+            ),
+            true
+        );
+        foreach (SkullType::getAll() as $skullType) {
+            ItemFactory::getInstance()->register(
+                new SkullItem(
+                    new ItemIdentifier(ItemIds::SKULL, $skullType->getMagicNumber()),
+                    $skullType->getDisplayName(),
+                    $skullType
+                ),
+                true
+            );
+        }
+        ResourceManager::getInstance();
+        DataProvider::getInstance();
+        $this->getServer()->getPluginManager()->registerEvents(new BlockPlaceListener(), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new ChunkLoadListener(), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EntityEffectAddListener(), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new PlayerLoginListener(), $this);
+        $this->getServer()->getCommandMap()->register("CSkull", new ShowSkullsCommand());
+        $this->getServer()->getCommandMap()->register("CSkull", new SkullCommand());
+    }
+
+    public function onDisable(): void {
+        DataProvider::getInstance()->close();
+    }
+}
---
+++ Spyglass/src/ItsRealNise/Spyglass/Main.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ItsRealNise\Spyglass;
+
+use ItsRealNise\Spyglass\item\Spyglass;
+use pocketmine\inventory\CreativeInventory;
+use pocketmine\item\ItemFactory;
+use pocketmine\network\mcpe\convert\ItemTranslator;
+use pocketmine\plugin\PluginBase;
+use ReflectionClass;
+use const pocketmine\BEDROCK_DATA_PATH;
+
+class Main extends PluginBase {
+
+    public function onLoad(): void {
+        $this->initItems();
+    }
+
+    public function initItems(): void {
+        $reflectionClass = new ReflectionClass(ItemTranslator::getInstance());
+        $property = $reflectionClass->getProperty("simpleCoreToNetMapping");
+        $property->setAccessible(true);
+        $value = $property->getValue(ItemTranslator::getInstance());
+        $property2 = $reflectionClass->getProperty("simpleNetToCoreMapping");
+        $property2->setAccessible(true);
+        $value2 = $property2->getValue(ItemTranslator::getInstance());
+        $runtimeIds = json_decode(file_get_contents(BEDROCK_DATA_PATH . 'required_item_list.json'), true);
+        $itemIds = json_decode(file_get_contents(BEDROCK_DATA_PATH . 'item_id_map.json'), true);
+        foreach ([
+                     "minecraft:spyglass" => CustomIds::SPYGLASS
+                 ] as $name => $id) {
+            $itemIds[$name] = $id;
+            $itemId = $itemIds[$name];
+            $runtimeId = $runtimeIds[$name]["runtime_id"];
+            $value[$itemId] = $runtimeId;
+            $value2[$runtimeId] = $itemId;
+            $property->setValue(ItemTranslator::getInstance(), $value);
+            $property2->setValue(ItemTranslator::getInstance(), $value2);
+        }
+        $property->setValue(ItemTranslator::getInstance(), $value);
+        $property2->setValue(ItemTranslator::getInstance(), $value2);
+        $item = new Spyglass();
+        ItemFactory::getInstance()->register($item, true);
+        CreativeInventory::getInstance()->add($item);
+    }
+}
