5d12888538237f83696f7e98984d27bf3ce5fd8d
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-05-16 05:02:23 +0900

    コマンドを実行した際に大文字などが含まれていても正常に実行できるように

---
+++ IgnoreCase/src/kim/present/ignorecase/Main.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection SpellCheckingInspection
+ */
+
+declare(strict_types=1);
+namespace kim\present\ignorecase;
+
+use kim\present\ignorecase\libs\kim\present\traits\removeplugindatadir\RemovePluginDataDirTrait;
+use pocketmine\event\Listener;
+use pocketmine\event\server\CommandEvent;
+use pocketmine\plugin\PluginBase;
+use function array_shift;
+use function explode;
+use function implode;
+use function rtrim;
+use function strcasecmp;
+
+final class Main extends PluginBase implements Listener {
+
+    use RemovePluginDataDirTrait;
+
+    /** @var array<string, string> */
+    private array $caches = ["" => ""];
+
+    protected function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    /**
+     * Replace command to exact command with ignore case
+     *
+     * @priority LOWEST
+     */
+    public function onCommandEvent(CommandEvent $event): void {
+        $commandLines = explode(" ", rtrim($event->getCommand(), "\r\n"));
+        $label = array_shift($commandLines);
+        /** Check caches and change command name if cache is not empty */
+        if (($cache = $this->caches[$label] ?? null) !== null) {
+            if ($cache !== "") {
+                $event->setCommand(implode(" ", [$cache, ...$commandLines]));
+            }
+            return;
+        }
+        $commands = $this->getServer()->getCommandMap()->getCommands();
+        if (isset($commands[$label])) {
+            /** Register "" to cache to avoid retrying navigations */
+            $this->caches[$label] = "";
+            return;
+        }
+        /** Find command by case-insensitive and register caches */
+        foreach ($commands as $key => $value) {
+            if (
+                strcasecmp($label, $find = $key) === 0 ||
+                strcasecmp($label, $find = $value->getLabel()) === 0
+            ) {
+                $this->caches[$label] = $find;
+                $event->setCommand(implode(" ", [$find, ...$commandLines]));
+                return;
+            }
+        }
+        /** Register "" to cache to avoid retrying navigations */
+        $this->caches[$label] = "";
+    }
+}
