feae974f1c42661053306dc43d15dbda26a18d02
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-05-16 04:58:47 +0900

    airBlock完成

--- EffectItems/src/lazyperson0710/EffectItems/tools/others/AirBlock.php
+++ EffectItems/src/lazyperson0710/EffectItems/tools/others/AirBlock.php
@@ -22,25 +22,22 @@ class AirBlock implements Listener {
             if (!(str_contains($world_search, "-c") || str_contains($world_search, "-f") || str_contains($world_search, "nature") || str_contains($world_search, "nether") || str_contains($world_search, "end") || str_contains($world_search, "MiningWorld") || str_contains($world_search, "debug"))) {
                 $player->sendMessage("§bAirBlock §7>> §c使用可能なワールドは資源系ワールドと生活ワールドのみとなります");
             }
-            //持っているものがブロックかアイテムかを判定したい
-            if ($inHand->getBlock() === null) return;
-                if ($player->getPosition()->getWorld()->getBlock($pos) != BlockLegacyIds::AIR) return;
-            if (EconomyLand::getInstance()->posCheck($pos, $player) === true) {
-                $item = $inHand;
-                $item->setCount(1);
-                $player->getInventory()->removeItem($item);
-                $player->getPosition()->getWorld()->setBlock($pos, $inHand->getBlock());
-                $sound = new PlaySoundPacket();
-                $sound->soundName = "dig.stone";
-                $sound->x = $player->getPosition()->getX();
-                $sound->y = $player->getPosition()->getY();
-                $sound->z = $player->getPosition()->getZ();
-                $sound->volume = 1;
-                $sound->pitch = 1;
-                $player->getNetworkSession()->sendDataPacket($sound);
-            } else {
+            if ($player->getPosition()->getWorld()->getBlock($pos)->getId() != BlockLegacyIds::AIR) return;
+            if (EconomyLand::getInstance()->posCheck($pos, $player) === false) {
                 $player->sendMessage("§bAirBlock §7>> §c他人の土地には設置できません");
             }
+            $item = $inHand;
+            $item->setCount(1);
+            $player->getInventory()->removeItem($item);
+            $player->getPosition()->getWorld()->setBlock($pos, $inHand->getBlock());
+            $sound = new PlaySoundPacket();
+            $sound->soundName = "dig.stone";
+            $sound->x = $player->getPosition()->getX();
+            $sound->y = $player->getPosition()->getY();
+            $sound->z = $player->getPosition()->getZ();
+            $sound->volume = 1;
+            $sound->pitch = 1;
+            $player->getNetworkSession()->sendDataPacket($sound);
         }
     }
 }
