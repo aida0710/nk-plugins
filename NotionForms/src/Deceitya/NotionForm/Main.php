bbab2b66ad05595cb6cf018437d765ad69d9febe
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-05-21 00:55:58 +0900

    コマンドの解説を追加、他解説もﾁｮｯﾄﾀﾞｹ。。。

--- NotionForms/src/Deceitya/NotionForm/Main.php
+++ NotionForms/src/Deceitya/NotionForm/Main.php
@@ -2,6 +2,7 @@
 
 namespace Deceitya\NotionForm;
 
+use Deceitya\NotionForm\command\CommandListCommand;
 use Deceitya\NotionForm\command\FunctionCommand;
 use Deceitya\NotionForm\command\SpecificationCommand;
 use pocketmine\plugin\PluginBase;
@@ -11,15 +12,19 @@ class Main extends PluginBase {
 
     static array $function;
     static array $specification;
+    static array $command;
 
     public function onEnable(): void {
         $this->saveResource('function.json');
         $this->saveResource('specification.json');
+        $this->saveResource('command.json');
         self::$function = (new Config($this->getDataFolder() . 'function.json', Config::JSON))->getAll();
         self::$specification = (new Config($this->getDataFolder() . 'specification.json', Config::JSON))->getAll();
+        self::$specification = (new Config($this->getDataFolder() . 'command.json', Config::JSON))->getAll();
         $this->getServer()->getCommandMap()->registerAll("notionForm", [
             new FunctionCommand(),
             new SpecificationCommand(),
+            new CommandListCommand()
         ]);
     }
 }
---
+++ NotionForms/src/Deceitya/NotionForm/command/CommandListnCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Deceitya\NotionForm\command;
+
+use Deceitya\NotionForm\Form\StartForm;
+use Deceitya\NotionForm\Main;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class CommandListCommand extends Command {
+
+    public function __construct() {
+        parent::__construct("command", "鯖内で使用できるコマンドの機能や仕様が書かれています");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if (!($sender instanceof Player)) {
+            $sender->sendMessage("Please use in server");
+            return;
+        }
+        $sender->sendForm(new StartForm(Main::$command));
+    }
+
+}
