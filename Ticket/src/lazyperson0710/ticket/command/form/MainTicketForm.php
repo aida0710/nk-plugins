dbdc5f1c6b3b857df38146a3e7d37f1042ce0c6c
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-05-24 06:05:26 +0900

    MainFormでop分岐するように

--- Ticket/src/lazyperson0710/ticket/command/form/MainTicketForm.php
+++ Ticket/src/lazyperson0710/ticket/command/form/MainTicketForm.php
@@ -5,19 +5,29 @@ namespace lazyperson0710\ticket\command\form;
 use bbo51dog\bboform\form\SimpleForm;
 use lazyperson0710\ticket\command\form\element\SendFormButton;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class MainTicketForm extends SimpleForm {
 
     public function __construct(Player $player) {
-        $this
-            ->setTitle("Ticket")
-            ->addElements(
-                new SendFormButton(new CheckTicketForm($player), "Ticket枚数を調べる"),
-                new SendFormButton(new SetTicketForm($player), "Ticket枚数を設定する"),
-                new SendFormButton(new AddTicketForm($player), "Ticket枚数を増やす"),
-                new SendFormButton(new ReduceTicketForm($player), "Ticket枚数を減らす"),
-                new SendFormButton(new ReplaceTicketForm($player), "Ticketを変換する"),
-                new SendFormButton(new SaveTicketForm($player), "Ticketデータをセーブする"),
-            );
+        if (Server::getInstance()->isOp($player->getName())) {
+            $this
+                ->setTitle("Ticket")
+                ->addElements(
+                    new SendFormButton(new CheckTicketForm($player), "Ticket枚数を調べる"),
+                    new SendFormButton(new SetTicketForm($player), "Ticket枚数を設定する"),
+                    new SendFormButton(new AddTicketForm($player), "Ticket枚数を増やす"),
+                    new SendFormButton(new ReduceTicketForm($player), "Ticket枚数を減らす"),
+                    new SendFormButton(new ReplaceTicketForm($player), "Ticketを変換する"),
+                    new SendFormButton(new SaveTicketForm($player), "Ticketデータをセーブする"),
+                );
+        } else {
+            $this
+                ->setTitle("Ticket")
+                ->addElements(
+                    new SendFormButton(new TransferTicketForm($player), "Ticketを譲渡する"),
+                    new SendFormButton(new ReplaceTicketForm($player), "Ticketを変換する"),
+                );
+        }
     }
 }
