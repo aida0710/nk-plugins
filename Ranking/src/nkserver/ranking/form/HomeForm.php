1b49ca4f4c967cb0d181da09878871c59359f312
lazyperson0710 <lazyperson0710@gmail.com>
2022-05-25 14:15:11 +0900

    訂正

--- Ranking/src/nkserver/ranking/form/HomeForm.php
+++ Ranking/src/nkserver/ranking/form/HomeForm.php
@@ -13,15 +13,10 @@ class HomeForm extends SimpleForm {
 
     public function __construct() {
         $this->title = TextFormat::BOLD . 'ホーム';
-        $this->addButton('統計情報を見る'); //send StatisticsForm
-        //$this->addButton('所持金ランキング');
+        $this->addButton('統計情報を見る');
         $this->addButton('銀行預金ランキング');
         $this->addButton('マイニングレベルランキング');
         $this->submit = function (Player $player, int $data): void {
-            /*if ($data === 1) {
-                Server::getInstance()->dispatchCommand($player, "topmoney");
-                return;
-            }*/
             $player->sendForm(
                 match ($data) {
                     0 => new StatisticsForm($this, $player->getName()),
