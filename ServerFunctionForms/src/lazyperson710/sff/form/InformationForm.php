f01c730c36b218402796764a5c8e443eeacaeabc
lazyperson0710 <lazyperson0710@gmail.com>
2022-05-31 17:19:50 +0900

    古い情報系formを削除

--- ServerFunctionForms/src/lazyperson710/sff/form/InformationForm.php
+++ ServerFunctionForms/src/lazyperson710/sff/form/InformationForm.php
@@ -15,10 +15,10 @@ class InformationForm extends SimpleForm {
         $this->setText("見たいコンテンツを選択してください");
         $this->addElements(
             new CommandDispatchButton("お知らせ", "announce", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/invite_base.png")),
-            new SendFormButton(new CommandListForm(), "コマンドリスト\nコマンドリストを表示する", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/mute_off.png")),
-            new SendFormButton(new FunctionServerListForm(), "機能一覧", new ButtonImage(ButtonImage::TYPE_PATH, "textures/items/book_portfolio.png")),
-            new SendFormButton(new SpecificationListForm(), "仕様一覧", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/debug_glyph_color.png")),
-            new SendFormButton(new TosForm(), "利用規約\nweb版:www.nkserver.net/tos", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/Feedback.png")),
+            new CommandDispatchButton("コマンドリスト\nコマンドリストを表示する", "command", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/mute_off.png")),
+            new CommandDispatchButton("機能一覧", "function", new ButtonImage(ButtonImage::TYPE_PATH, "textures/items/book_portfolio.png")),
+            new CommandDispatchButton("仕様一覧", "specification", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/debug_glyph_color.png")),
+            new SendFormButton(new TosForm(), "利用規約\nweb版:www.nkserver.net/tos.html", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/Feedback.png")),
             new Button("閉じる", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/redX1.png")),
         );
     }
