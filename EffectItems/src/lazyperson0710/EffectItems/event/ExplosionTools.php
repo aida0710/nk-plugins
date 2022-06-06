760e85fe9cebdecb6e0786b6fd30252ab8129210
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-07 00:24:44 +0900

    爆発効果アイテムの挙動を変更

--- EffectItems/src/lazyperson0710/EffectItems/event/ExplosionTools.php
+++ EffectItems/src/lazyperson0710/EffectItems/event/ExplosionTools.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace lazyperson0710\EffectItems\tools\all;
+namespace lazyperson0710\EffectItems\event;
 
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
@@ -11,12 +12,14 @@ use pocketmine\world\Explosion;
 
 class ExplosionTools implements Listener {
 
-    public function onBreak(BlockBreakEvent $event): void {
-        if ($event->isCancelled()) return;
+    public function onBreak(BlockPlaceEvent $event): void {
         $player = $event->getPlayer();
         $inHand = $player->getInventory()->getItemInHand();
         $namedTag = $player->getInventory()->getItemInHand()->getNamedTag();
-        if ($namedTag->getTag('ExplosionTools') !== null) {//ExplosionTools
+        if ($namedTag->getTag('ExplosionBlock') !== null) {//ExplosionBlock
+            $event->cancel();
+            $inHand->setCount(1);
+            $player->getInventory()->removeItem($inHand);
             $explosion = (new Explosion($event->getBlock()->getPosition()->asPosition(), mt_rand(3, 15)));
             //$explosion->explodeA();//破壊するブロックを計算(オプション、実行しない場合、「explodeB」関数にてエフェクトとダメージの処理のみ実施)
             //$explosion->explodeB();//エフェクト表示、エンティティに対するダメージ、explodeAを実行した場合ブロックの破壊を実施
@@ -31,14 +34,8 @@ class ExplosionTools implements Listener {
             $getEntity = $event->getEntity();
             $playerName = $getEntity->getName();
             if ($player = Server::getInstance()->getPlayerExact($playerName)) {
-                $namedTag = $player->getInventory()->getItemInHand()->getNamedTag();
-                if ($namedTag->getTag('ExplosionTools') !== null) {//ExplosionTools
-                    if (mt_rand(1, 10) != 10) {
-                        $event->cancel();
-                    } else {
-                        $player->sendTip("運が悪くてダメージを食らった！");
-                    }
-                }
+                $player->sendTip("§bExplosion §7>> §a爆発ダメージが無効化されました");
+                $event->cancel();
             }
         }
     }
