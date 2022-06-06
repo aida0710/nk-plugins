25cdbbddfce2a4045007c2a297947711fd65fb7e
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-07 00:26:10 +0900

    マイナス効果を削除

--- EffectItems/src/lazyperson0710/EffectItems/event/BlastFurnacePickaxe.php
+++ EffectItems/src/lazyperson0710/EffectItems/event/BlastFurnacePickaxe.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace lazyperson0710\EffectItems\tools\pickaxe;
+namespace lazyperson0710\EffectItems\event;
 
 use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\VanillaBlocks;
@@ -19,46 +19,29 @@ class BlastFurnacePickaxe implements Listener {
             foreach ($event->getDrops() as $item) {
                 switch ($item->getId()) {
                     case BlockLegacyIds::COBBLESTONE:
-                        $this->addFire($player);
                         $event->setDrops([VanillaBlocks::STONE()->asItem()]);
                         break;
                     case BlockLegacyIds::IRON_ORE:
-                        $this->addFire($player);
                         $event->setDrops([VanillaItems::IRON_INGOT()]);
                         break;
                     case BlockLegacyIds::GOLD_ORE:
-                        $this->addFire($player);
                         $event->setDrops([VanillaItems::GOLD_INGOT()]);
                         break;
                     case BlockLegacyIds::NETHERRACK:
-                        $this->addFire($player);
                         $event->setDrops([VanillaItems::NETHER_BRICK()]);
                         break;
                     case BlockLegacyIds::SAND:
-                        $this->addFire($player);
                         $event->setDrops([VanillaBlocks::GLASS()->asItem()]);
                         break;
                     case BlockLegacyIds::SPONGE:
-                        $this->addFire($player);
                         $event->setDrops([VanillaBlocks::SPONGE()->asItem()]);
                         break;
                     case BlockLegacyIds::LOG:
                     case BlockLegacyIds::LOG2:
-                        $this->addFire($player);
                         $event->setDrops([VanillaItems::COAL()]);
                         break;
                 }
             }
-        }
-    }
-
-    public function addFire(Player $player) {
-        if ($player->isOnFire()) {
-            return;
-        }
-        if (mt_rand(1, 350) === 350) {
-            $player->setOnFire(5);
-            $player->sendMessage("自分も燃えてしまった");
         }
     }
 }
