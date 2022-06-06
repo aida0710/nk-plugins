470b323834579ef1d03aa18fcc8e347be84338ce
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-07 00:59:20 +0900

    グロウストーン判定を削除。野菜系に対応

--- LuckyEnchant/src/deceitya/luckyenchant/Main.php
+++ LuckyEnchant/src/deceitya/luckyenchant/Main.php
@@ -2,6 +2,7 @@
 
 namespace deceitya\luckyenchant;
 
+use pocketmine\block\BlockLegacyIds;
 use pocketmine\data\bedrock\EnchantmentIdMap;
 use pocketmine\data\bedrock\EnchantmentIds;
 use pocketmine\event\block\BlockBreakEvent;
@@ -10,6 +11,7 @@ use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\enchantment\ItemFlags;
 use pocketmine\item\enchantment\Rarity;
 use pocketmine\item\enchantment\StringToEnchantmentParser;
+use pocketmine\item\VanillaItems;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
@@ -38,7 +40,17 @@ class Main extends PluginBase implements Listener {
         if ($enchant === null) {
             return;
         }
-        if (!in_array($event->getBlock()->getId(), [56, 21, 73, 16, 129, 153, -288, 89])) {
+        if (empty($event->getDrops())) return;
+        if (!in_array($event->getBlock()->getId(), [
+            BlockLegacyIds::DIAMOND_ORE,
+            BlockLegacyIds::LAPIS_ORE,
+            BlockLegacyIds::REDSTONE_ORE,
+            BlockLegacyIds::LIT_REDSTONE_ORE,
+            BlockLegacyIds::COAL_ORE,
+            BlockLegacyIds::EMERALD_ORE,
+            BlockLegacyIds::NETHER_QUARTZ_ORE,
+            BlockLegacyIds::CARROT_BLOCK,
+            BlockLegacyIds::POTATO_BLOCK,])) {
             return;
         }
         $rand = mt_rand(0, 999);
