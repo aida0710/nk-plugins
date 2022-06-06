4f0f379e2298148eaf7493ebff99d461825fbe40
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-07 00:51:07 +0900

    指定した範囲をparticleの線で結ばれるように

--- CorePlugins/src/lazyperson710/core/listener/Major.php
+++ CorePlugins/src/lazyperson710/core/listener/Major.php
@@ -2,9 +2,12 @@
 
 namespace lazyperson710\core\listener;
 
+use InvalidArgumentException;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\math\Vector3;
+use pocketmine\world\particle\RedstoneParticle;
 
 class Major implements Listener {
 
@@ -19,29 +22,37 @@ class Major implements Listener {
                 if (!$player->isSneaking()) {
                     $block = $event->getblock();
                     if (isset($this->data[$name])) {
-                        //for ($i = $this->data[$name]->distance($block->getPosition()); $i <= 0; $i--)
-                        //$block->getPosition()->getWorld()->addParticle($, new DustParticle(new Color(255, 255, 255)));
-                        //todo 点と点を線で繋ぎたい
+                        $particle = new RedstoneParticle(15);
+                        for ($i = 0; $i <= 10; $i += 0.5) {
+                            $pos = $this->lerp($this->data[$name]->add(0, 1, 0), $block->getPosition()->add(0, 1, 0), $i * 0.1);
+                            $player->getWorld()->addParticle($pos, $particle);
+                        }
                         $distance = $this->data[$name]->distance($block->getPosition());
                         $player->sendActionBarMessage("§bMajor §7>> §a" . (round($distance, 2) + 1) . " mです");
                     } else {
                         $this->data[$name] = $block->getPosition();
-                        $player->sendMessage("§bMajor §7>> §a始点のブロック座標を記録しました。 {$block->getPosition()->getX()}, {$block->getPosition()->getY()}, {$block->getPosition()->getZ()} \n§bMajor §7>> §aスニークしながらタップすることで座標を再設定出来ます");
+                        $player->sendMessage("§bMajor §7>> §a始点のブロック座標を記録しました。 {$block->getPosition()->getX()}, {$block->getPosition()->getY()}, {$block->getPosition()->getZ()}");
+                        $player->sendTip("§bMajor §7>> §aスニークしながらタップすることで座標を再設定出来ます");
                     }
                 } else {
                     unset($this->data[$name]);
-                    $player->sendMessage("§bMajor §7>> §a記録した座標のデータを削除しました");
+                    $player->sendTip("§bMajor §7>> §a記録した座標のデータを削除しました");
                 }
             }
         }
     }
 
-    public function onBreak(BlockBreakEvent $event) {
-        $player = $event->getPlayer();
-        if ($player->getInventory()->getItemInHand()->getId() == 318) {
-            if ($player->getInventory()->getItemInHand()->getName() === "Major") {
-                $event->cancel();
-            }
+    /**
+     * @param Vector3 $start
+     * @param Vector3 $end
+     * @param float $percent
+     * @return Vector3
+     */
+    function lerp(Vector3 $start, Vector3 $end, float $percent): Vector3 {
+        if (0 > $percent or $percent > 1) {
+            throw new InvalidArgumentException("percentage $percent should have a value of 0 to 1");
         }
+        return $end->subtract($start->getFloorX(), $start->getFloorY(), $start->getFloorZ())->multiply($percent)->add($start->getFloorX(), $start->getFloorY(), $start->getFloorZ());
     }
+
 }
