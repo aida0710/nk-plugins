e4a43750502866f9d52cbf8ae7becb890057e5ec
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-17 04:30:42 +0900

    Effect用のformButtonを追加

---
+++ miningtools/src/deceitya/miningtools/element/SendEffectFormButton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace deceitya\miningtools\element;
+
+use bbo51dog\bboform\element\Button;
+use bbo51dog\bboform\element\ButtonImage;
+use pocketmine\entity\effect\Effect;
+use pocketmine\player\Player;
+
+class SendEffectFormButton extends Button {
+
+    private Effect $effect;
+
+    /**
+     * @param string $text
+     * @param Effect $effect
+     * @param ButtonImage|null $image
+     */
+    public function __construct(string $text, Effect $effect, ?ButtonImage $image = null) {
+        parent::__construct($text, $image);
+        $this->effect = $effect;
+    }
+
+    public function handleSubmit(Player $player): void {
+        $player->sendForm(new EffectConfirmForm($player, $this->effect));
+    }
+}
