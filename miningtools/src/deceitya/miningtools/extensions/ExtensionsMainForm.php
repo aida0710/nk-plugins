dcf06d2189b4818f8f57ca16a676f76a5bb96948
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-17 14:59:15 +0900

    Menuで選択できるformを変更

--- miningtools/src/deceitya/miningtools/extensions/ExtensionsMainForm.php
+++ miningtools/src/deceitya/miningtools/extensions/ExtensionsMainForm.php
@@ -5,7 +5,6 @@ namespace deceitya\miningtools\extensions\range;
 use bbo51dog\bboform\form\SimpleForm;
 use deceitya\miningtools\element\SendFormButton;
 use deceitya\miningtools\extensions\effect\EffectSelectForm;
-use deceitya\miningtools\extensions\enchant\EffectConfirmForm;
 use pocketmine\player\Player;
 
 class ExtensionsMainForm extends SimpleForm {
@@ -23,7 +22,7 @@ class ExtensionsMainForm extends SimpleForm {
             ->setText("強化したい機能を選択してください")
             ->addElements(
                 new SendFormButton(new RangeConfirmForm($player), "採掘範囲拡張\n最大9x9まで拡張できます"),
-                new SendFormButton(new EffectConfirmForm($player), "拡張エンチャント\nオーバーエンチャントが可能"),
+                new SendFormButton(new EffectSelectForm($player), "拡張エンチャント\nオーバーエンチャントが可能"),
                 new SendFormButton(new EffectSelectForm($player), "拡張エフェクト効果\n手に持った時にエフェクトが付与されるように")
             );
     }
