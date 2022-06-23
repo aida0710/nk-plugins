8e16345e796dfce9d04b294d0f5a46edc7789348
lazyperson0710 <lazyperson0710@gmail.com>
2022-06-24 07:36:30 +0900

    エンチャント拡張機能。表示Messageを除き完成

--- miningtools/src/deceitya/miningtools/extensions/enchant/EnchantBuyForm.php
+++ miningtools/src/deceitya/miningtools/extensions/enchant/EnchantBuyForm.php
@@ -4,8 +4,11 @@ namespace deceitya\miningtools\extensions\enchant;
 
 use bbo51dog\bboform\element\Button;
 use bbo51dog\bboform\form\SimpleForm;
+use deceitya\miningtools\extensions\CheckPlayerData;
 use deceitya\miningtools\extensions\SetLoreJudgment;
 use onebone\economyapi\EconomyAPI;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
@@ -17,12 +20,12 @@ class EnchantBuyForm extends SimpleForm {
         $this->nbt = $nbt;
         $upgrade = "未定義のエラー";
         $namedTag = $player->getInventory()->getItemInHand()->getNamedTag();
-        if ($namedTag->getTag('MiningTools_Expansion_Range') !== null) {
+        if ($namedTag->getTag('MiningTools_Expansion_Enchant') !== null) {
             $upgrade = match ($namedTag->getInt("MiningTools_Expansion")) {
                 1 => "上位ツールにアップグレードしますか？\n\n費用は600万円\n範囲は7x7になります",
                 2 => "最上位ツールにアップグレードしますか？\n\n費用は1500万円\n範囲は9x9になります",
             };
-        } elseif ($namedTag->getTag('MiningTools_3') !== null) {
+        } else {
             $upgrade = "上位ツールにアップグレードしますか？\n\n費用は350万円\n範囲は5x5になります";
         }
         $this
@@ -33,63 +36,53 @@ class EnchantBuyForm extends SimpleForm {
 
     public function handleSubmit(Player $player): void {
         $namedTag = $player->getInventory()->getItemInHand()->getNamedTag();
-        $radius = 0;
+        $rank = 0;
         $item = $player->getInventory()->getItemInHand();
-        if (array_key_exists("MiningTools_Expansion_Range", $this->nbt)) {
-            if ($this->nbt["MiningTools_Expansion_Range"] !== $namedTag->getInt("MiningTools_Expansion_Range")) {
+        if (empty($this->nbt)) {
+            if ($item->getNamedTag()->getTag('MiningTools_Expansion_Enchant') !== null) {
                 $player->sendMessage("現在所持しているアイテムは最初に持っているアイテムでは無いため不正防止の為処理が中断されました");
                 return;
             }
-            if ($item->getNamedTag()->getTag('MiningTools_Expansion_Range') !== null) {
-                $nbt = $item->getNamedTag();
-                switch ($namedTag->getInt("MiningTools_Expansion_Range")) {
+            if ($item->getNamedTag()->getTag('MiningTools_3') !== null) {
+                $rank = 1;
+                if ((new CheckPlayerData())->ReduceMoney($player, EnchantConfirmForm::Rank2_MoneyCost) === false) return;
+                if ((new CheckPlayerData())->ReduceCostItem($player, $rank, EnchantConfirmForm::CostItemId, EnchantConfirmForm::CostItemNBT) === false) return;
+                $item->addEnchantment(new EnchantmentInstance(VanillaEnchantments::UNBREAKING(), 25));
+            }
+        }
+        if (array_key_exists("MiningTools_Expansion_Enchant", $this->nbt)) {
+            if ($this->nbt["MiningTools_Expansion_Enchant"] !== $namedTag->getInt("MiningTools_Expansion_Enchant")) {
+                $player->sendMessage("現在所持しているアイテムは最初に持っているアイテムでは無いため不正防止の為処理が中断されました");
+                return;
+            }
+            if ($item->getNamedTag()->getTag('MiningTools_Expansion_Enchant') !== null) {
+                switch ($namedTag->getInt("MiningTools_Expansion_Enchant")) {
                     case 1:
-                        $price = 6000000;
-                        $radius = 2;
-                        $this->onReduceMoney($player, $price);
+                        $rank = 2;
+                        if ((new CheckPlayerData())->ReduceMoney($player, EnchantConfirmForm::Rank2_MoneyCost) === false) return;
+                        if ((new CheckPlayerData())->ReduceCostItem($player, $rank, EnchantConfirmForm::CostItemId, EnchantConfirmForm::CostItemNBT) === false) return;
+                        $item->addEnchantment(new EnchantmentInstance(VanillaEnchantments::UNBREAKING(), 35));
                         break;
                     case 2:
-                        $price = 15000000;
-                        $radius = 3;
-                        $this->onReduceMoney($player, $price);
+                        $rank = 3;
+                        if ((new CheckPlayerData())->ReduceMoney($player, EnchantConfirmForm::Rank3_MoneyCost) === false) return;
+                        if ((new CheckPlayerData())->ReduceCostItem($player, $rank, EnchantConfirmForm::CostItemId, EnchantConfirmForm::CostItemNBT) === false) return;
+                        $item->addEnchantment(new EnchantmentInstance(VanillaEnchantments::MENDING(), 1));
+                        $item->addEnchantment(new EnchantmentInstance(VanillaEnchantments::UNBREAKING(), 50));
                         break;
                     default:
                         Server::getInstance()->broadcastMessage("[" . $player->getName() . "]" . __DIR__ . "の" . __LINE__ . "行目でエラーが発生しました");
                         return;
                 }
-                $tag = "MiningTools_Expansion_Range";
-                $nbt->removeTag($tag);
-                $nbt->setInt('MiningTools_Expansion_Range', $radius);
-                $item->setNamedTag($nbt);
-                $itemName = match ($namedTag->getInt("MiningTools_Expansion_Range")) {
-                    1 => "§aNetheriteMiningPickaxe Ex.Secondary",
-                    2 => "§aNetheriteMiningPickaxe Ex.Tertiary",
-                };
-                $item->setCustomName($itemName);
-                $player->getInventory()->setItemInHand($item);
             }
         }
-        if (array_key_exists("MiningTools_3", $this->nbt)) {
-            if ($this->nbt["MiningTools_3"] !== $namedTag->getInt("MiningTools_3")) {
-                $player->sendMessage("現在所持しているアイテムは最初に持っているアイテムでは無いため不正防止の為処理が中断されました");
-                return;
-            }
-            if ($item->getNamedTag()->getTag('MiningTools_3') !== null) {
-                $radius = 1;
-                $price = 3500000;
-                if (!$this->onReduceMoney($player, $price)) return;
-                $nbt = $item->getNamedTag();
-                $tag = "MiningTools_3";
-                $nbt->removeTag($tag);
-                $nbt->setInt('MiningTools_Expansion_Range', $radius);
-                $item->setNamedTag($nbt);
-                $item->setCustomName("§aNetheriteMiningPickaxe Ex.Primary");
-                $player->getInventory()->setItemInHand($item);
-            }
-        }
+        $nbt = $item->getNamedTag();
+        $nbt->removeTag('MiningTools_Expansion_Enchant');
+        $nbt->setInt('MiningTools_Expansion_Enchant', $rank);
+        $item->setNamedTag($nbt);
         $item->setLore((new SetLoreJudgment())->SetLoreJudgment($item));
         $player->getInventory()->setItemInHand($item);
-        Server::getInstance()->broadcastMessage("§bMiningTools §7>> §e{$player->getName()}がNetheriteMiningToolsをEx.Rank{$radius}にアップグレードしました");
+        Server::getInstance()->broadcastMessage("§bMiningTools §7>> §e{$player->getName()}がNetheriteMiningToolsをEx.Rank{$rank}にアップグレードしました");
     }
 
     public function onReduceMoney(Player $player, $price): bool {
--- miningtools/src/deceitya/miningtools/extensions/enchant/EnchantConfirmForm.php
+++ miningtools/src/deceitya/miningtools/extensions/enchant/EnchantConfirmForm.php
@@ -6,14 +6,13 @@ use bbo51dog\bboform\element\Button;
 use bbo51dog\bboform\form\SimpleForm;
 use pocketmine\block\BlockLegacyIds;
 use pocketmine\player\Player;
-use pocketmine\Server;
 
 class EnchantConfirmForm extends SimpleForm {
 
-    private array $nbt;
-    const Rank_1_MoneyCost = 1500;
-    const Rank_2_MoneyCost = 1500;
-    const Rank_3_MoneyCost = 1500;
+    private array $nbt = [];
+    const Rank1_MoneyCost = 1500;
+    const Rank2_MoneyCost = 1500;
+    const Rank3_MoneyCost = 1500;
 
     const CostItemId = BlockLegacyIds::PACKED_ICE;
     const CostItemNBT = "MiningToolsEnchantCostItem";
@@ -23,7 +22,7 @@ class EnchantConfirmForm extends SimpleForm {
         $namedTag = $player->getInventory()->getItemInHand()->getNamedTag();
         if ($namedTag->getTag('MiningTools_Expansion_Enchant') !== null) {
             $this->nbt = ["MiningTools_Expansion_Enchant" => $namedTag->getInt("MiningTools_Expansion_Enchant")];
-            $upgrade = match ($namedTag->getInt('MiningTools_Expansion_Range')) {
+            $upgrade = match ($namedTag->getInt('MiningTools_Expansion_Enchant')) {
                 1 => "現在、耐久力エンチャントはRank.2です\n以下のコストを支払ってMiningToolを強化しますか？",
                 2 => "現在、耐久力エンチャントは最大レベルです\nエンドコンテンツとして修繕を付与することが可能です",
                 3 => "最上位ツールの為アップグレードに対応していません",
@@ -35,14 +34,10 @@ class EnchantConfirmForm extends SimpleForm {
         $this
             ->setTitle("Expansion Mining Tools")
             ->setText($upgrade)
-            ->addElements(new Button("アップデートする", null));
+            ->addElements(new Button("アップデートする"));
     }
 
     public function handleSubmit(Player $player): void {
-        if (empty($this->nbt)) {
-            Server::getInstance()->getLogger()->error(__DIR__ . "の" . __LINE__ . "行目でエラーが発生しました");
-            return;
-        }
         if ($player->getInventory()->getItemInHand()->getNamedTag()->getInt('MiningTools_Expansion_Enchant') === 3) {
             return;
         }
