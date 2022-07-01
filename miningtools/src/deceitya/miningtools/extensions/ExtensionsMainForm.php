fa28e2a192795dd7fd238c9602b5a23dd274ee20
lazyperson0710 <lazyperson0710@gmail.com>
2022-07-02 02:33:01 +0900

    formTextを修正

--- miningtools/src/deceitya/miningtools/extensions/ExtensionsMainForm.php
+++ miningtools/src/deceitya/miningtools/extensions/ExtensionsMainForm.php
@@ -13,7 +13,7 @@ class ExtensionsMainForm extends SimpleForm {
     public function __construct(Player $player) {
         $this
             ->setTitle("Expansion Mining Tools")
-            ->setText("強化したい機能を選択してください")
+            ->setText("強化したい機能を選択してください\n全て三段階で強化が可能です")
             ->addElements(
                 new SendFormButton(new RangeConfirmForm($player), "採掘範囲拡張\n最大9x9まで拡張できます"),
                 new SendFormButton(new EnchantFunctionSelectForm($player), "拡張エンチャント\nオーバーエンチャントが可能"),
