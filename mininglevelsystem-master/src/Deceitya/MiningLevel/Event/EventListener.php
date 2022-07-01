6eaeaceb268b359d6d50f61fa6818e5f2227db80
lazyperson0710 <lazyperson0710@gmail.com>
2022-07-02 02:29:27 +0900

    レベルアップ時のTutorialメッセージを変更

--- mininglevelsystem-master/src/Deceitya/MiningLevel/Event/EventListener.php
+++ mininglevelsystem-master/src/Deceitya/MiningLevel/Event/EventListener.php
@@ -7,6 +7,7 @@ use bbo51dog\pmdiscord\element\Embed;
 use bbo51dog\pmdiscord\element\Embeds;
 use DateTime;
 use DateTimeInterface;
+use Deceitya\MiningLevel\Form\MiningLevelUPForm;
 use Deceitya\MiningLevel\MiningLevelAPI;
 use deceitya\miningtools\event\MiningToolsBreakEvent;
 use InfoSystem\InfoSystem;
@@ -15,7 +16,6 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\item\ItemFactory;
-use pocketmine\player\GameMode;
 use pocketmine\player\Player;
 use pocketmine\utils\Config;
 use ree_jp\stackStorage\api\StackStorageAPI;
@@ -46,18 +46,26 @@ class EventListener implements Listener {
      * @priority HIGHEST
      */
     public function onBlockBreak(BlockBreakEvent $event) {
+        if ($event->isCancelled()) {
+            return;
+        }
+        $player = $event->getPlayer();
+        $this->getBreakEvent($event, $player);
+    }
+
+    /**
+     * @priority HIGHEST
+     */
+    public function CountBlock(MiningToolsBreakEvent $event): void {
+        if ($event->isCancelled()) {
+            return;
+        }
         $player = $event->getPlayer();
         $this->getBreakEvent($event, $player);
     }
 
     public function getBreakEvent(BlockBreakEvent|MiningToolsBreakEvent $event, Player $player) {
         $player = $event->getPlayer();
-        if ($event->isCancelled()) {
-            return;
-        }
-        if ($player->getGamemode() !== GameMode::SURVIVAL()) {
-            return;
-        }
         if (!in_array($player->getPosition()->getWorld()->getFolderName(), $this->config['world'])) {
             return;
         }
@@ -80,12 +88,10 @@ class EventListener implements Listener {
                     StackStorageAPI::$instance->add($player->getXuid(), $item);
                     $player->sendActionBarMessage("§bStorage §7>> §aインベントリに空きが無いため" . $item->getName() . "が倉庫にしまわれました");
                 }
-                $giveitem = "§bLevel §7>> §aレベルアップボーナスとして{$item}が付与されました";
-                $player->sendMessage($giveitem);
+                $player->sendMessage("§bLevel §7>> §aレベルアップボーナスとして{$item->getName()}が付与されました");
             } else if ($level % 5 == 0) {
-                $givemoney = "§bLevel §7>> §aレベルアップボーナスとして8000円が付与されました";
                 EconomyAPI::getInstance()->addMoney($player, 8000);
-                $player->sendMessage($givemoney);
+                $player->sendMessage("§bLevel §7>> §aレベルアップボーナスとして8000円が付与されました");
             }
         }
         $api->setLevel($player, $level);
@@ -111,6 +117,7 @@ class EventListener implements Listener {
             } else {
                 $player->sendMessage("§bLevel §7>> §a{$name}がLv.{$originalLevel}からLv.{$level}にレベルアップしました");
                 $player->sendtitle("§bMining Level UP", "§aLv.{$originalLevel} -> Lv.{$level}");
+                $msg = null;
                 switch ($level) {
                     case 2:
                         $player->sendMessage("§bTutorial §7>> §g/shopでアイテムを売買できます！");
@@ -129,8 +136,9 @@ class EventListener implements Listener {
                         $player->sendMessage("§bTutorial §7>> §g死ぬと所持金の半分がなくなります。。。銀行を活用してみよう！銀行に預けると死んでもアイテムは消えません！/bank");
                         break;
                     case 15:
-                        $player->sendMessage("§bTutorial §7>> §gインベントリの物を一気にストレージに入れたいときは/stall !!");
-                        $player->sendMessage("§bTutorial §7>> §gオーバーワールド4&javaが解放されました！通常よりも鉱石の量が多いのが特徴です。ただし、javaのワールドはmininglevel経験値が取得できないため注意が必要です！");
+                        $msg .= "インベントリの物を一気にストレージに入れたいときは/stall !!\n";
+                        $msg .= "オーバーワールド4&javaが解放されました！通常よりも鉱石の量が多いのが特徴です。ただし、javaのワールドはmininglevel経験値が取得できないため注意が必要です！\n";
+                        $msg .= "DiamondMiningToolsが解放されました！範囲破壊が可能ですがDiamondグレードでは修繕ができないためご注意ください。。。\n";
                         break;
                     case 18:
                         $player->sendMessage("§bTutorial §7>> §gログインボーナスをアイテムと交換するには/bonusでできます！");
@@ -140,27 +148,33 @@ class EventListener implements Listener {
                         $player->sendMessage("§bTutorial §7>> §gネザーディメンション2,3が解放されました！");
                         break;
                     case 25:
-                        $player->sendMessage("§bTutorial §7>> §gLevelShop2が解放されました！浜松市に行って農業をやってみよう！/shop");
-                        $player->sendMessage("§bTutorial §7>> §gインベントリからアイテムを一括売却できる機能が解放されました！");
-                        $player->sendMessage("§bTutorial §7>> §gDiamondMiningToolsが解放されました！範囲破壊が可能ですがDiamondグレードでは修繕ができないためご注意ください。。。");
+                        $msg .= "LevelShop2が解放されました！浜松市に行って農業をやってみよう！\n";
+                        $msg .= "インベントリからアイテムを一括売却できる機能が解放されました！/shop\n\n";
+                        $msg .= "MyWarpでのワープ地点上限が5 -> 10になりました！/mw\n";
                         break;
                     case 30:
-                        $player->sendMessage("§bTutorial §7>> §gサーバーオリジナルレシピが存在します！/recipeから見れるよ！");
-                        $player->sendMessage("§bTutorial §7>> §gMiningWorldが解放されました！一週間に一度リセットされる特殊なワールドです！");
+                        $msg .= "サーバーオリジナルレシピが存在します！/recipeから見れるよ！\n";
+                        $msg .= "PVPワールドが解放されました！/pvp\n";
+                        $msg .= "連続で同じメッセージを発言できるようになりました\n";
+                        $msg .= "MiningWorldが解放されました！一週間に一度リセットされる特殊なワールドです！\n";
                         break;
                     case 45:
                         $player->sendMessage("§bTutorial §7>> §gショップ2が解放されました！/shopから確認してみよう！");
                         break;
                     case 50:
-                        $player->sendMessage("§bTutorial §7>> §gLevelShop3が解放されました！/shop");
-                        $player->sendMessage("§bTutorial §7>> §gNetheriteMiningToolsが解放されました！修繕可能なため範囲破壊をとことん楽しんでください！");
+                        $msg .= "LevelShop3が解放されました！/shop";
+                        $msg .= "MyWarpでのワープ地点上限が10 -> 15になりました！/mw";
                         break;
                     case 80:
-                        $player->sendMessage("§bTutorial §7>> §gShop4が解放されました！/shop");
-                        $player->sendMessage("§bTutorial §7>> §gコマンドから道具の修繕が可能になりました！/repair");
+                        $msg .= "Shop4が解放されました！/shop";
+                        $msg .= "コマンドから道具の修繕が可能になりました！/repair";
+                        break;
+                    case 100:
+                        $player->sendMessage("§bTutorial §7>> §gアイテムの修繕で失敗しないようになりました！/repair");
                         break;
                     case 120:
-                        $player->sendMessage("§bTutorial §7>> §gLevelShop5が解放されました！/shop");
+                        $msg .= "LevelShop5が解放されました！/shop";
+                        $msg .= "NetheriteMiningTools機能強化が解放されました！Gachaで手に入るアイテムを使って強化しよう！範囲拡張や耐久強化などが出来ます！/mt";
                         break;
                     case 180:
                         $player->sendMessage("§bTutorial §7>> §gLevelShop6が解放されました！/shop");
@@ -172,6 +186,9 @@ class EventListener implements Listener {
                         $player->sendMessage("§bTutorial §7>> §gLevelShop7が解放されました！/shop");
                         break;
                 }
+                if (!is_null($msg)) {
+                    $player->sendForm(new MiningLevelUPForm($msg));
+                }
             }
             (new MiningLevelUpEvent($player, $originalLevel, $level))->call();
             /** @var InfoSystem $plugin */
@@ -180,11 +197,4 @@ class EventListener implements Listener {
         }
     }
 
-    /**
-     * @priority HIGHEST
-     */
-    public function CountBlock(MiningToolsBreakEvent $event): void {
-        $player = $event->getPlayer();
-        $this->getBreakEvent($event, $player);
-    }
 }
