cd3bc95b0da7a9b64fdbf4c680e074fdd432e5c4
lazyperson0710 <lazyperson0710@gmail.com>
2022-07-02 02:30:00 +0900

    Tutorialメッセージが多い場合はformで送信するように変更

---
+++ mininglevelsystem-master/src/Deceitya/MiningLevel/Form/MiningLevelUPForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Deceitya\MiningLevel\Form;
+
+use bbo51dog\bboform\element\Label;
+use bbo51dog\bboform\form\CustomForm;
+
+class MiningLevelUPForm extends CustomForm {
+
+    public function __construct(string $msg) {
+        $this
+            ->setTitle("MiningLevel Form")
+            ->addElements(
+                new Label($msg),
+            );
+    }
+}
