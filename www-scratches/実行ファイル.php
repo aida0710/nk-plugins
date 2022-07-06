54338f8121c1399c82a8b7ad16f4f1203013555e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-06 20:49:26 +0900

    matchTest

--- www-scratches/実行ファイル.php
+++ www-scratches/実行ファイル.php
@@ -1,2 +1,44 @@
 <?php
 
+new Main();
+
+class Main {
+
+    public function __construct() {
+        $this->matchTest();
+    }
+
+    public function matchTest(): void {
+        $class = [
+            new Hoge_1(),
+            new Hoge_2(),
+            new Hoge_3(),
+        ];
+        foreach ($class as $value) {
+            $msg = match ($value::class) {
+                Hoge_1::class => "::class_1",
+                Hoge_2::class => "::class_2",
+                Hoge_3::class => "::class_3",
+            };
+            $instanceof = match (true) {
+                $value instanceof Hoge_1 => "instanceof_1",
+                $value instanceof Hoge_2 => "instanceof_2",
+                $value instanceof Hoge_3 => "instanceof_3",
+            };
+            echo $msg . "\n";
+            echo $instanceof . "\n";
+        }
+    }
+}
+
+class Hoge_1 {
+
+}
+
+class Hoge_2 {
+
+}
+
+class Hoge_3 {
+
+}
