4cf08e5c7244c647ce65330b0711d27600bc8bdd
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-14 07:46:05 +0900

    setting機能のクラス作成

---
+++ CorePlugins/src/lazyperson710/core/Setting.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace lazyperson710\core;
+
+class Setting {
+
+    private static array $coordinate;
+    private static array $joinItems;
+    private static array $directDropItemStorage;
+    private static array $levelUpMessage;
+    //private static array $chatMute;要らんかも
+
+}
