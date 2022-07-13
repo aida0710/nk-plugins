e60d9940691c268414e4bf4e244aaad4860683ec
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-14 08:18:16 +0900

    機能の策定

--- CorePlugins/src/lazyperson710/core/Setting.php
+++ CorePlugins/src/lazyperson710/core/Setting.php
@@ -4,10 +4,11 @@ namespace lazyperson710\core;
 
 class Setting {
 
-    private static array $coordinate;
-    private static array $joinItems;
-    private static array $directDropItemStorage;
-    private static array $levelUpMessage;
+    private static array $coordinate;           //座標の表示
+    private static array $joinItems;            //ログイン時のアイテム配布
+    private static array $directDropItemStorage;//Inventoryに空きがあってもストレージに
+    private static array $levelUpTitle;         //レベルアップ時のタイトル
+    private static array $enduranceWarning;     //耐久値の警告
+    private static array $destructionSound;     //ブロック破壊時のExp音
     //private static array $chatMute;要らんかも
-
 }
