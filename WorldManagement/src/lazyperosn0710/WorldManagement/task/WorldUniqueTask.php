166ba198f6f1cf3a05236287ec0df39a669e4890
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-19 11:07:37 +0900

    todoを追加

---
+++ WorldManagement/src/lazyperosn0710/WorldManagement/task/WorldUniqueTask.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lazyperson0710\WorldManagement\task;
+
+use pocketmine\scheduler\Task;
+
+class WorldUniqueTask extends Task {
+
+    public function onRun(): void {
+        /**
+         * todo: ワールドの固有機能など
+         * 常時スピードとか?
+         */
+    }
+
+}
