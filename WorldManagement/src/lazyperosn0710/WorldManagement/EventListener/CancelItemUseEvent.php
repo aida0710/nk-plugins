da280b72980d2b5277f62646a95364994f7ea009
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-19 11:04:54 +0900

    ワールド関係のCancel処理を移動

---
+++ WorldManagement/src/lazyperosn0710/WorldManagement/EventListener/CancelItemUseEvent.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace lazyperson0710\WorldManagement\EventListener;
+
+use pocketmine\block\BlockLegacyIds;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityTrampleFarmlandEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\item\ItemIds;
+use pocketmine\item\VanillaItems;
+use pocketmine\Server;
+
+class CancelItemUseEvent implements Listener {
+
+    public function onUes(PlayerItemUseEvent $event) {
+        $this->banItems($event);
+    }
+
+    public function onPlace(BlockPlaceEvent $event) {
+        $this->banItems($event);
+    }
+
+    public function onInteract(PlayerInteractEvent $event) {
+        $this->banItems($event);
+        $world_name = $event->getPlayer()->getWorld()->getDisplayName();
+        $farming = mb_substr($world_name, -2, 2, 'utf-8');
+        if ($farming !== "-f") {
+            switch ($event->getPlayer()->getInventory()->getItemInHand()->getId()) {
+                //クワ
+                case ItemIds::WOODEN_HOE:
+                case ItemIds::STONE_HOE:
+                case ItemIds::IRON_HOE:
+                case ItemIds::GOLD_HOE:
+                case ItemIds::DIAMOND_HOE:
+                    //作物関係
+                case ItemIds::WHEAT_SEEDS:
+                case ItemIds::PUMPKIN_SEEDS:
+                case ItemIds::MELON_SEEDS:
+                case ItemIds::NETHER_WART:
+                case ItemIds::BEETROOT_SEEDS:
+                case ItemIds::CARROT:
+                case ItemIds::POTATO:
+                case ItemIds::SUGARCANE:
+                case ItemIds::SUGARCANE_BLOCK:
+                case ItemIds::BAMBOO:
+                case ItemIds::BAMBOO_SAPLING:
+                    if (!Server::getInstance()->isOp($event->getPlayer()->getName())) {
+                        $event->cancel();
+                    }
+                    $event->getPlayer()->sendTip("§bFarming §7>> §c農業ワールドでのみ使用可能です");
+                    break;
+            }
+        }
+        if (!($farming === "-f" || $farming === "-c")) {
+            switch ($event->getPlayer()->getInventory()->getItemInHand()->getId()) {
+                //水関係
+                case BlockLegacyIds::WATER:
+                case BlockLegacyIds::WATER_LILY:
+                case BlockLegacyIds::WATERLILY:
+                case BlockLegacyIds::FLOWING_WATER:
+                case BlockLegacyIds::STILL_WATER:
+                    //その他
+                case ItemIds::HOPPER:
+                    if (!Server::getInstance()->isOp($event->getPlayer()->getName())) {
+                        $event->cancel();
+                    }
+                    $event->getPlayer()->sendTip("§bWater §7>> §c{$event->getPlayer()->getInventory()->getItemInHand()->getName()}は生活ワールドと農業ワールドでのみ使用可能です");
+                    break;
+            }
+        }
+    }
+
+    public function banItems(BlockPlaceEvent|PlayerItemUseEvent|PlayerInteractEvent $event) {
+        switch ($event->getPlayer()->getInventory()->getItemInHand()->getId()) {
+            case BlockLegacyIds::INFO_UPDATE;
+            case BlockLegacyIds::INFO_UPDATE2;
+            case BlockLegacyIds::RESERVED6;
+            case BlockLegacyIds::ICE:
+                //BanItems
+            case ItemIds::BANNER:
+            case ItemIds::BANNER_PATTERN:
+            case ItemIds::STANDING_BANNER:
+            case ItemIds::WALL_BANNER:
+            case ItemIds::FIRE_CHARGE:
+            case ItemIds::FIRE:
+            case ItemIds::FIREBALL:
+            case ItemIds::LAVA:
+            case ItemIds::FLOWING_LAVA:
+            case ItemIds::STILL_LAVA:
+            case ItemIds::MINECART:
+            case ItemIds::MINECART_WITH_CHEST:
+            case ItemIds::MINECART_WITH_COMMAND_BLOCK:
+            case ItemIds::MINECART_WITH_HOPPER:
+            case ItemIds::MINECART_WITH_TNT:
+            case ItemIds::ENCHANTED_BOOK:
+            case ItemIds::ENDER_CHEST:
+            case ItemIds::ENDER_EYE:
+            case ItemIds::ENDER_PEARL:
+            case ItemIds::LEAD:
+            case ItemIds::ARMOR_STAND:
+            case ItemIds::CHORUS_FLOWER:
+            case ItemIds::CHORUS_FRUIT:
+            case ItemIds::CHORUS_FRUIT_POPPED:
+            case ItemIds::CHORUS_PLANT:
+            case ItemIds::BOAT:
+            case ItemIds::FIREWORKS:
+            case ItemIds::FIREWORKS_CHARGE:
+            case ItemIds::CARROT_ON_A_STICK:
+            case ItemIds::LINGERING_POTION:
+            case ItemIds::SPLASH_POTION:
+            case ItemIds::PAINTING:
+            case ItemIds::FLINT_AND_STEEL:
+            case ItemIds::PISTON:
+            case ItemIds::STICKY_PISTON:
+            case ItemIds::REPEATER:
+            case ItemIds::COMPARATOR:
+                if (!Server::getInstance()->isOp($event->getPlayer()->getName())) {
+                    $event->cancel();
+                }
+                $event->getPlayer()->sendTip("§bCancel §7>> §cこのアイテムは使用できません");
+                break;
+        }
+        if ($event->getPlayer()->getInventory()->getItemInHand()->getId() === 325) {
+            if ($event->getPlayer()->getInventory()->getItemInHand()->getMeta() !== 1) {
+                if (!Server::getInstance()->isOp($event->getPlayer()->getName())) {
+                    $event->cancel();
+                }
+                $event->getPlayer()->sendTip("§bCancel §7>> §cこのアイテムは使用できません");
+            }
+        }
+        if ($event->getPlayer()->getInventory()->getItemInHand()->getNamedTag()->getTag('MiningToolsRangeCostItem') !== null) {
+            $event->cancel();
+            $event->getPlayer()->sendTip("§bCancel §7>> §cこのアイテムはMiningToolsの強化にのみ使用可能です/mt");
+        }
+        if ($event->getPlayer()->getInventory()->getItemInHand()->getNamedTag()->getTag('MiningToolsEnchantCostItem') !== null) {
+            $event->cancel();
+            $event->getPlayer()->sendTip("§bCancel §7>> §cこのアイテムはMiningToolsの強化にのみ使用可能です/mt");
+        }
+    }
+
+    public function onBreak(BlockBreakEvent $event) {
+        foreach ($event->getDrops() as $item) {
+            switch ($item->getId()) {
+                case BlockLegacyIds::INFO_UPDATE:
+                case BlockLegacyIds::INFO_UPDATE2:
+                case BlockLegacyIds::RESERVED6:
+                    $event->setDrops([
+                        VanillaItems::NETHER_STAR()->setCount(1),
+                    ]);
+            }
+        }
+    }
+
+    public function onEntityTrampleFarmland(EntityTrampleFarmlandEvent $event) {
+        $event->cancel();
+    }
+}
