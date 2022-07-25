18764f5ca0e9a863b6fa7a8eb2c14cf81d89e017
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-26 03:43:20 +0900

    天然資源ワールドの場合はカウントダウンが表示されるように変更

---
+++ WorldManagement/src/lazyperson0710/WorldManagement/WorldLimit/task/CheckPositionTask.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace lazyperson0710\WorldManagement\WorldLimit\task;
+
+use lazyperson0710\WorldManagement\database\WorldCategory;
+use lazyperson0710\WorldManagement\Main;
+use lazyperson0710\WorldManagement\WorldLimit\WorldProperty;
+use pocketmine\scheduler\Task;
+use pocketmine\scheduler\TaskScheduler;
+use pocketmine\Server;
+use pocketmine\world\World;
+
+class CheckPositionTask extends Task {
+
+    private TaskScheduler $scheduler;
+
+    /** @var WorldProperty[] */
+    private array $properties;
+
+    /**
+     * @param TaskScheduler $scheduler
+     * @param WorldProperty[] $properties
+     */
+    public function __construct(TaskScheduler $scheduler, array $properties) {
+        $this->scheduler = $scheduler;
+        $this->properties = $properties;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onRun(): void {
+        foreach ($this->properties as $property) {
+            $world = Server::getInstance()->getWorldManager()->getWorldByName($property->getWorldName());
+            if (!$world instanceof World) {
+                continue;
+            }
+            if (in_array($world->getFolderName(), WorldCategory::Nature) || in_array($world->getFolderName(), WorldCategory::NatureOthers) || in_array($world->getFolderName(), WorldCategory::Nether) || in_array($world->getFolderName(), WorldCategory::End)) {
+                foreach ($world->getPlayers() as $player) {
+                    if (!$property->inSafeArea($player->getPosition())) {
+                        $player->sendMessage("§bWorldBorder §7>> §cワールドの上限を越えています。" . Main::TELEPORT_INTERVAL . "秒以内にセーフエリアに戻ってください\n§7>> §c戻らなかった場合、強制的にテレポートされます");
+                        $this->scheduler->scheduleDelayedTask(new PlayerTeleportTask($player, $property), Main::TELEPORT_INTERVAL * 20);
+                    }
+                }
+            }
+        }
+    }
+}
