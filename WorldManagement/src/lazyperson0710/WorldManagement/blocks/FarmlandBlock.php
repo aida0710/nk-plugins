d1d4c2de73876cfb81d38f1689dcaf830a71af99
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-26 03:24:26 +0900

    指定ワールドでは農地が水なしでも最大まで湿るように

---
+++ WorldManagement/src/lazyperson0710/WorldManagement/blocks/FarmlandBlock.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace lazyperson0710\WorldManagement\blocks;
+
+use pocketmine\block\Farmland;
+use pocketmine\block\VanillaBlocks;
+
+class FarmlandBlock extends Farmland {
+
+    public function onRandomTick(): void {
+        if ($this->getPosition()->getWorld()->getFolderName() === "八街市-f") {
+            $this->wetness = 7;
+            $this->position->getWorld()->setBlock($this->position, $this, false);
+            return;
+        }
+        if (!$this->canHydrate()) {
+            if ($this->wetness > 0) {
+                $this->wetness--;
+                $this->position->getWorld()->setBlock($this->position, $this, false);
+            } else {
+                $this->position->getWorld()->setBlock($this->position, VanillaBlocks::DIRT());
+            }
+        } elseif ($this->wetness < 7) {
+            $this->wetness = 7;
+            $this->position->getWorld()->setBlock($this->position, $this, false);
+        }
+    }
+
+}
