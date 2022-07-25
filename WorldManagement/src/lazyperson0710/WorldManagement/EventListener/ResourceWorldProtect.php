6aa96b9ce3c04a06362a05f768ea5a4169f6155c
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-07-26 03:32:40 +0900

    人工資源で破壊、設置できるブロックの制限を行う機能の再構築

---
+++ WorldManagement/src/lazyperson0710/WorldManagement/EventListener/ResourceWorldProtect.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace lazyperson0710\WorldManagement\EventListener;
+
+use lazyperson0710\WorldManagement\database\WorldManagementAPI;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\Listener;
+
+class ResourceWorldProtect implements Listener {
+
+    /**
+     * @param BlockBreakEvent $event
+     * @return void
+     * @priority Low
+     */
+    public function onBreak(BlockBreakEvent $event) {
+        if ($event->getBlock()->getPosition()->getWorld()->getFolderName() === "resource") {
+            $blocks = [
+                VanillaBlocks::STONE()->getName(),
+                VanillaBlocks::GRANITE()->getName(),
+                VanillaBlocks::DIORITE()->getName(),
+                VanillaBlocks::ANDESITE()->getName(),
+                VanillaBlocks::OAK_LOG()->getName(),
+                VanillaBlocks::SPRUCE_LOG()->getName(),
+                VanillaBlocks::BIRCH_LOG()->getName(),
+                VanillaBlocks::JUNGLE_LOG()->getName(),
+                VanillaBlocks::ACACIA_LOG()->getName(),
+                VanillaBlocks::DARK_OAK_LOG()->getName(),
+            ];
+            $heightLimit = WorldManagementAPI::getInstance()->getHeightLimit("resource");
+            if ($event->getBlock()->getPosition()->getFloorY() >= $heightLimit) {
+                $event->getPlayer()->sendTip("§bProtect §7>> §c現在のワールドではY.{$heightLimit}以上のブロックを破壊することは許可されていません");
+                $event->cancel();
+                return;
+            }
+            if (!in_array($event->getBlock()->getName(), $blocks)) {
+                $event->getPlayer()->sendTip("§bProtect §7>> §c現在のワールドでは{$event->getBlock()->getName()}の破壊は許可されていません");
+                $event->cancel();
+            }
+        }
+    }
+
+    /**
+     * @param BlockPlaceEvent $event
+     * @return void
+     * @priority Low
+     */
+    public function onPlace(BlockPlaceEvent $event) {
+        $heightLimit = WorldManagementAPI::getInstance()->getHeightLimit("resource");
+        if ($event->getBlock()->getPosition()->getWorld()->getFolderName() === "resource") {
+            $blocks = [
+                VanillaBlocks::STONE()->getName(),
+                VanillaBlocks::GRANITE()->getName(),
+                VanillaBlocks::DIORITE()->getName(),
+                VanillaBlocks::ANDESITE()->getName(),
+                VanillaBlocks::OAK_LOG()->getName(),
+                VanillaBlocks::SPRUCE_LOG()->getName(),
+                VanillaBlocks::BIRCH_LOG()->getName(),
+                VanillaBlocks::JUNGLE_LOG()->getName(),
+                VanillaBlocks::ACACIA_LOG()->getName(),
+                VanillaBlocks::DARK_OAK_LOG()->getName(),
+            ];
+            if ($event->getBlock()->getPosition()->getFloorY() >= $heightLimit) {
+                $event->getPlayer()->sendTip("§bProtect §7>> §c現在のワールドではY.{$heightLimit}以上でブロックを設置することは許可されていません");
+                $event->cancel();
+                return;
+            }
+            if (!in_array($event->getBlock()->getName(), $blocks)) {
+                $event->getPlayer()->sendTip("§bProtect §7>> §c現在のワールドでは{$event->getBlock()->getName()}の設置は許可されていません");
+                $event->cancel();
+            }
+        }
+    }
+
+}
