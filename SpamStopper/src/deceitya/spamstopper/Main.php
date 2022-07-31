fdfd6bd2af8f13be8dfbeee52b35f1cd77cc414b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-01 03:28:42 +0900

    30レベル未満のプレイヤーはチャットを送信してから5秒以内に再度チャットを送信出来なくなるように

--- SpamStopper/src/deceitya/spamstopper/Main.php
+++ SpamStopper/src/deceitya/spamstopper/Main.php
@@ -6,14 +6,19 @@ use Deceitya\MiningLevel\MiningLevelAPI;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
 
 class Main extends PluginBase implements Listener {
 
     private array $spam = [];
 
+    private static array $interval;
+
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        self::$interval = [];
     }
 
     public function onChat(PlayerChatEvent $event) {
@@ -34,8 +39,19 @@ class Main extends PluginBase implements Listener {
         } else {
             if (isset($this->spam[$name]) && $this->spam[$name] == $msg) {
                 $event->cancel();
-                $event->getPlayer()->sendMessage("§bSpamBlock §7>> §c30レベル未満のプレイヤーは連続して同じメッセージは遅れません");
+                $event->getPlayer()->sendMessage("§bSpamBlock §7>> §c30レベル未満のプレイヤーは連続して同じメッセージは送信できません");
                 return;
+            }
+            if (isset(self::$interval[$event->getPlayer()->getName()])) {
+                $event->getPlayer()->sendMessage("§bSpamBlock §7>> §c30レベル未満のプレイヤーはチャット後5秒以内は連続してメッセージを送信できません");
+                $event->cancel();
+            } else {
+                self::$interval[$event->getPlayer()->getName()] = true;
+                $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                    function () use ($event): void {
+                        $this->unset($event->getPlayer());
+                    }
+                ), 20 * 5);
             }
         }
         $this->spam[$name] = $msg;
@@ -43,5 +59,9 @@ class Main extends PluginBase implements Listener {
 
     public function onJoin(PlayerJoinEvent $event) {
         $this->spam[$event->getPlayer()->getName()] = false;
+    }
+
+    public function unset(Player $player): void {
+        unset(self::$interval[$player->getName()]);
     }
 }
