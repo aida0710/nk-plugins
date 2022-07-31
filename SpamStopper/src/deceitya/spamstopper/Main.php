ac138f087d050a9508fdfbe7d6b78b14c26ea77d
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-01 03:26:40 +0900

    30レベル以上のプレイヤーでも1秒以内は連投できないように

--- SpamStopper/src/deceitya/spamstopper/Main.php
+++ SpamStopper/src/deceitya/spamstopper/Main.php
@@ -20,7 +20,17 @@ class Main extends PluginBase implements Listener {
         $name = $event->getPlayer()->getName();
         $msg = $event->getMessage();
         if (MiningLevelAPI::getInstance()->getLevel($event->getPlayer()) >= 30) {
-            return;
+            if (isset(self::$interval[$event->getPlayer()->getName()])) {
+                $event->getPlayer()->sendMessage("§bSpamBlock §7>> §cチャットは1秒以内に再度送信することは出来ません");
+                $event->cancel();
+            } else {
+                self::$interval[$event->getPlayer()->getName()] = true;
+                $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                    function () use ($event): void {
+                        $this->unset($event->getPlayer());
+                    }
+                ), 20);
+            }
         } else {
             if (isset($this->spam[$name]) && $this->spam[$name] == $msg) {
                 $event->cancel();
