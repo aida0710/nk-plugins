7c007ef82ef2ac7fa130e480e23c6add8d63d9e8
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-01 03:22:48 +0900

    ブロックとアイテムの判定箇所を変更

--- WorldManagement/src/lazyperson0710/WorldManagement/EventListener/YachimataCityWorldProtect.php
+++ WorldManagement/src/lazyperson0710/WorldManagement/EventListener/YachimataCityWorldProtect.php
@@ -18,15 +18,15 @@ class YachimataCityWorldProtect implements Listener {
      * @priority Low
      */
     public function onBreak(BlockBreakEvent $event) {
-        if (in_array($event->getBlock()->getPosition()->getWorld()->getFolderName(), WorldCategory::UniqueAgricultureWorld)) {
+        if (in_array($event->getPlayer()->getPosition()->getWorld()->getFolderName(), WorldCategory::UniqueAgricultureWorld)) {
             $blocks = [
                 VanillaBlocks::WHEAT()->getId(),
                 VanillaBlocks::POTATOES()->getId(),
                 VanillaBlocks::CARROTS()->getId(),
                 VanillaBlocks::BEETROOTS()->getId(),
             ];
-            if (!in_array($event->getPlayer()->getInventory()->getItemInHand()->getId(), $blocks)) {
-                $event->getPlayer()->sendTip("§bProtect §7>> §c現在のワールドでは{$event->getPlayer()->getInventory()->getItemInHand()->getName()}の破壊は許可されていません");
+            if (!in_array($event->getBlock()->getId(), $blocks)) {
+                $event->getPlayer()->sendTip("§bProtect §7>> §c現在のワールドでは{$event->getBlock()->getName()}の破壊は許可されていません");
                 $event->cancel();
             }
         }
@@ -38,7 +38,7 @@ class YachimataCityWorldProtect implements Listener {
      * @priority Low
      */
     public function onPlace(BlockPlaceEvent $event) {
-        if (in_array($event->getBlock()->getPosition()->getWorld()->getFolderName(), WorldCategory::UniqueAgricultureWorld)) {
+        if (in_array($event->getPlayer()->getPosition()->getWorld()->getFolderName(), WorldCategory::UniqueAgricultureWorld)) {
             $items = [
                 VanillaItems::WHEAT_SEEDS()->getId(),
                 VanillaItems::POTATO()->getId(),
