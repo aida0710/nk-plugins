12a6271936b1c54268def27d5b78e633902aa30c
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-09 04:14:17 +0900

    pコマンドの追加

---
+++ ServerFunctionForms/src/lazyperson710/sff/command/PoliceCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lazyperson710\sff\command;
+
+use lazyperson710\sff\form\police\PoliceMainForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class PoliceCommand extends Command {
+
+    public function __construct() {
+        parent::__construct("p", "debugコマンド");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if (!($sender instanceof Player)) {
+            $sender->sendMessage("Please use in server");
+            return;
+        }
+        $players = [
+            "lazyperson710",
+        ];
+        if (in_array($sender->getName(), $players)) {
+            $sender->sendForm(new PoliceMainForm($sender));
+        } else {
+            $sender->sendMessage("§bSystem §7 >> §aコマンドの使用権限がありません");
+        }
+    }
+}
