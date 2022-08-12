1657ea700e84835565cb6cc4ebd21ea031460db7
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-13 03:59:45 +0900

    設定項目の追加

---
+++ PlayerSetting/src/lazyperson0710/PlayerSetting/object/settings/DirectDropItemStorageSetting.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+namespace lazyperson0710\PlayerSetting\object\settings;
+
+use lazyperson0710\PlayerSetting\object\Setting;
+
+class DirectDropItemStorageSetting extends Setting {
+
+    protected bool $value;
+
+    public static function getName(): string {
+        return 'DirectDropItemStorage';
+    }
+
+    public function setValue(mixed $value): void {
+        $this->value = $value;
+    }
+
+    public function getValue(): bool {
+        return $this->value ?? $this->getFallbackValue();
+    }
+
+    public function getFallbackValue(): bool {
+        return false;
+    }
+}
