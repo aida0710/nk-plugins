54c1d01fdc139140ccb94ca7b75aa919c700ee12
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-16 01:50:40 +0900

    form処理に中間処理を追加

---
+++ CorePlugins/src/lazyperson710/core/packet/SendForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace lazyperson710\core\packet;
+
+use lazyperson710\core\Main;
+use pocketmine\form\Form;
+use pocketmine\player\Player;
+use pocketmine\scheduler\ClosureTask;
+
+class SendForm {
+
+    private static array $formInterval = [];
+
+    public static function Send(Player $player, Form $form): void {
+        if (isset(self::$formInterval[$player->getName()])) {
+            $player->sendTip("§bSendForm §7>> §c0.3秒以内連続でFormを送信することは出来ません");
+            return;
+        } else {
+            self::$formInterval[$player->getName()] = true;
+            Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                function () use ($player): void {
+                    (new SendForm())->unset($player);
+                }
+            ), 6);
+        }
+        $player->sendForm($form);
+    }
+
+    public function unset(Player $player): void {
+        unset(self::$formInterval[$player->getName()]);
+    }
+}
