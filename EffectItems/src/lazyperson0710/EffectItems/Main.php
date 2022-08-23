3684025730632221d2ab3850e51c2ad2f16dc4ab
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-23 10:36:57 +0900

    ncコマンドを追加

--- EffectItems/src/lazyperson0710/EffectItems/Main.php
+++ EffectItems/src/lazyperson0710/EffectItems/Main.php
@@ -7,6 +7,7 @@ use lazyperson0710\EffectItems\event\PlayerBlockPlaceEvent;
 use lazyperson0710\EffectItems\event\PlayerDamageEvent;
 use lazyperson0710\EffectItems\event\PlayerItemEvent;
 use lazyperson0710\EffectItems\task\RepetitionTask;
+use lazyperson0710\Gacha\command\ItemNameChangeCommand;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
@@ -18,5 +19,8 @@ class Main extends PluginBase {
         $manager->registerEvents(new PlayerDamageEvent(), $this);
         $manager->registerEvents(new PlayerItemEvent(), $this);
         $this->getScheduler()->scheduleRepeatingTask(new RepetitionTask(), 10);
+        $this->getServer()->getCommandMap()->registerAll("effectItems", [
+            new ItemNameChangeCommand(),
+        ]);
     }
 }
---
+++ EffectItems/src/lazyperson0710/EffectItems/command/ItemNameChangeCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace lazyperson0710\Gacha\command;
+
+use lazyperson0710\EffectItems\form\ItemNameChangeForm;
+use lazyperson710\core\packet\SendForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class ItemNameChangeCommand extends Command {
+
+    public function __construct() {
+        parent::__construct("nc", "アイテムを消費して道具の名前を変更する");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if (!($sender instanceof Player)) {
+            $sender->sendMessage("Please use in server");
+            return;
+        }
+        SendForm::Send($sender, (new ItemNameChangeForm()));
+    }
+
+}
