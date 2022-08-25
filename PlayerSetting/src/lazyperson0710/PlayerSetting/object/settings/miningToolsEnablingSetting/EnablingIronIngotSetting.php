63a070ddadccf803c6ef97f38a07697d2514efbf
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-26 03:52:27 +0900

    マイニングツール設定の解放されているか否かの保存項目を追加

---
+++ PlayerSetting/src/lazyperson0710/PlayerSetting/object/settings/miningToolsEnablingSetting/EnablingIronIngotSetting.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+namespace lazyperson0710\PlayerSetting\object\settings\miningToolsEnablingSetting;
+
+use lazyperson0710\PlayerSetting\object\Setting;
+
+class EnablingIronIngotSetting extends Setting {
+
+    protected bool $value;
+
+    public static function getName(): string {
+        return 'EnablingIronIngot';
+    }
+
+    public function setValue(mixed $value): void {
+        $this->value = $value;
+    }
+
+    public function getValue(): bool {
+        return $this->value ?? $this->getFallbackValue();
+    }
+
+    public function getFallbackValue(): bool {
+        return false;
+    }
+}
