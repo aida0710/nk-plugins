224e96892a17ab4ac0c032b4ebfbf57792b9024b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-26 03:46:50 +0900

    破壊音が鳴らないバグを修正

--- CorePlugins/src/lazyperson710/core/listener/BreakSoundPacket.php
+++ CorePlugins/src/lazyperson710/core/listener/BreakSoundPacket.php
@@ -3,7 +3,7 @@
 namespace lazyperson710\core\listener;
 
 use lazyperson0710\PlayerSetting\object\PlayerSettingPool;
-use lazyperson0710\PlayerSetting\object\settings\DestructionSoundSetting;
+use lazyperson0710\PlayerSetting\object\settings\normal\DestructionSoundSetting;
 use lazyperson710\core\packet\SoundPacket;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
@@ -16,9 +16,9 @@ class BreakSoundPacket implements Listener {
      */
     public function onBreak(BlockBreakEvent $event): void {
         if (PlayerSettingPool::getInstance()->getSettingNonNull($event->getPlayer())->getSetting(DestructionSoundSetting::getName())?->getValue() === true) {
-            $volume = mt_rand(1, 2) / 10;
-            $pitch = mt_rand(5, 10) / 10;
-            SoundPacket::init($event->getPlayer(), "random.orb", $volume, $pitch);
+            $volume = mt_rand(1, 2);
+            $pitch = mt_rand(5, 10);
+            SoundPacket::init($event->getPlayer(), 'random.orb', $volume, $pitch, true, 10);
         }
     }
 }
