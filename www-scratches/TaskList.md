2664de0da986a20065c3fcc11116b81fba8dbd65
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-31 21:27:38 +0900

    設定関係を更新

--- www-scratches/TaskList.md
+++ www-scratches/TaskList.md
@@ -1,30 +1,11 @@
 ## 作業中又は着手したいこと
+
+* miningtoolsの設定を解放する手段を追加
+* がちゃで大量のアイテムをついかする
+
 ## バグ
-
-* 生活ワールドでブロックを高度上限に設置すると壊せなくなる
-* effect,enchantのSelect画面の説明が不十分
-* cmdListを未更新
-* Majorを1つ以上持っているときの表示が違う(インベントリは空けている)
-* エンチャント削減がマイニングツールに使えてしまうのと
-  マイナスの数値が実行できてしまう
-
 ## 余裕ができたらやりたいこと
 
 * ExpをServerFunctionFormの方で使えるように
 * プロモーションコードを入力できるように
 * 相手と自分の位置が特定できるpvp用アイテムが欲しい
-
-## 公開時にやること
-
-* あさぽんワールド削除
-* invseeのconfigファイル編集
-* lobbyのParticleサイズ確認
-* config確認
-  * invsee
-  * secretcommands
-    * makeplugin
-  * redstone削除
-  * Auction
-  * miningtools
-  * gacha
-* stackStorageのid0を削除{data.sqlite}
--- www-scratches/実行ファイル.php
+++ www-scratches/実行ファイル.php
@@ -1,44 +1,135 @@
 <?php
 
-new Main();
+/*interface Human {
 
-class Main {
+    public function height(): int;
+
+    public function weight(): int;
+
+    public function iq(): int;
+
+    public function name(): string;
+
+    public function age(): int;
+
+    public function isAlive(): bool;
+
+}
+
+class Spiny implements Human {
+
+    public function height(): int {
+        return 120;
+    }
+
+    public function weight(): int {
+        return 70;
+    }
+
+    public function iq(): int {
+        return 100;
+    }
+
+    public function name(): string {
+        return "Spiny";
+    }
+
+    public function age(): int {
+        return 16;
+    }
+
+    public function isAlive(): bool {
+        return false;
+    }
+
+}
+
+class WeakeningSpiny extends Spiny {
+    public function name(): string {
+        $name = parent::name();
+        return "弱弱しい". $name;
+    }
+    public function iq(): int {
+        $iq = parent::iq();
+        return $iq - 10;
+    }
+
+    public function isAlive(): bool {
+        return true;
+    }
+
+}
+
+echo (new WeakeningSpiny())->name();
+echo (bool)(new WeakeningSpiny())->isAlive();*/
+
+//$test = bcadd(1.7, 0.3, 2);
+//var_dump($test);
+//$test = bcadd($test, 5.9,2);
+//var_dump($test);
+//
+//echo $test;
+class test {
+
+    /** @var array<string, float|int> */
+    private array $tmp_table;
+    private array $rank;
+    protected int $num;
 
     public function __construct() {
-        $this->matchTest();
+        $this->tmp_table = $this->getTmpTable();
+        $this->initTable();
     }
 
-    public function matchTest(): void {
-        $class = [
-            new Hoge_1(),
-            new Hoge_2(),
-            new Hoge_3(),
-        ];
-        foreach ($class as $value) {
-            $msg = match ($value::class) {
-                Hoge_1::class => "::class_1",
-                Hoge_2::class => "::class_2",
-                Hoge_3::class => "::class_3",
-            };
-            $instanceof = match (true) {
-                $value instanceof Hoge_1 => "instanceof_1",
-                $value instanceof Hoge_2 => "instanceof_2",
-                $value instanceof Hoge_3 => "instanceof_3",
-            };
-            echo $msg . "\n";
-            echo $instanceof . "\n";
+    public function initTable(): void {
+        $this->rank = array_map(static fn($value) => $value * 1000, $this->tmp_table);
+        $this->num = (int)array_sum($this->rank);
+    }
+
+    public function run($num = 1): array {
+        $result = [];
+        for ($i = 1; $i <= $num; $i++) {
+            $rand = mt_rand(1, $this->num);
+            $count = 0;
+            foreach ($this->rank as $table => $item) {
+                $count += $item;
+                if ($rand <= $count) {
+                    $result[$table] ??= 0;
+                    ++$result[$table];
+                    continue 2;
+                }
+            }
+            throw new \RuntimeException("test");
         }
+        return $result;
+    }
+
+    protected function getTmpTable(): array {
+        return [
+            "C" => 80,
+            "UC" => 13,
+            "R" => 5,
+            "m" => 0.001,
+            "SR" => 1.7,
+            "L" => 0.3,
+        ];
     }
 }
 
-class Hoge_1 {
-
-}
-
-class Hoge_2 {
-
-}
-
-class Hoge_3 {
-
+//$count = 1000010;
+$count = 1000010000;
+$class = new test();
+$result = $class->run($count);
+$result += [
+    "C" => 0,
+    "UC" => 0,
+    "R" => 0,
+    "SR" => 0,
+    "L" => 0,
+    "m" => 0,
+];
+const SCALE = 10;
+foreach ($result as $table => $item) {
+    //bcmul(bcdiv($item,$count,SCALE), 100,SCALE)
+    echo "\n{$table}の数{$item},	", $item / $count * 100, "%";
 }
