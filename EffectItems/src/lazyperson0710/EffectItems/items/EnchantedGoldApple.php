ffbb10f4374aabd25b8c2220fad4ce6e0eaaaf0b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-08-31 21:07:09 +0900

    テスト用アイテムを二種作成

---
+++ EffectItems/src/lazyperson0710/EffectItems/items/EnchantedGoldApple.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace lazyperson0710\EffectItems\items;
+
+use Deceitya\MiningLevel\Event\EventListener;
+use lazyperson710\core\packet\AddEffectPacket;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+
+class EnchantedGoldApple {
+
+    public static function init(PlayerItemUseEvent|PlayerInteractEvent $event): void {
+        $event->cancel();
+        $player = $event->getPlayer();
+        $item = $player->getInventory()->getItemInHand();
+        $player->getInventory()->removeItem($item->setCount(1));
+        $vanillaEffect = VanillaEffects::HASTE();
+        $effect = new EffectInstance($vanillaEffect, 20 * 60 * 5, 14, false);
+        AddEffectPacket::init($player, $effect, $vanillaEffect, true);
+        EventListener::getInstance()->LevelCalculation($player, 5000);
+    }
+}
