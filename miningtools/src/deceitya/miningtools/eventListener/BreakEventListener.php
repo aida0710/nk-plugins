d0f5d60eaac3469eaa59b15d8a34e08d600f3e28
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-05 07:23:43 +0900

    不要なコードをコメントアウト

--- miningtools/src/deceitya/miningtools/eventListener/BreakEventListener.php
+++ miningtools/src/deceitya/miningtools/eventListener/BreakEventListener.php
@@ -11,9 +11,7 @@ use lazyperson0710\PlayerSetting\object\settings\normal\MiningToolsEnduranceWarn
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\Durable;
-use pocketmine\item\Item;
 use pocketmine\item\ItemIds;
-use pocketmine\player\Player;
 use pocketmine\Server;
 
 class BreakEventListener implements Listener {
@@ -30,7 +28,7 @@ class BreakEventListener implements Listener {
         $player = $event->getPlayer();
         $item = $player->getInventory()->getItemInHand();
         //mining4を変換
-        $this->itemNbtConversion($player, $item);
+        //$this->itemNbtConversion($player, $item);不要だと思われるため削除
         //所持しているアイテムがMiningToolsかどうかを確認
         if (!($item->getNamedTag()->getTag('MiningTools_3') !== null || $item->getNamedTag()->getTag('MiningTools_Expansion_Range') !== null)) return;
         $id = $item->getId();
@@ -79,20 +77,20 @@ class BreakEventListener implements Listener {
         Main::$flag[$player->getName()] = false;
     }
 
-    /**
-     * @param Player $player
-     * @param Item   $item
-     * @return void
-     */
-    public function itemNbtConversion(Player $player, Item $item): void {
-        if ($item->getNamedTag()->getTag('4mining') !== null) {
-            $nbt = $item->getNamedTag();
-            $tag = "4mining";
-            $nbt->removeTag($tag);
-            $nbt->setInt('MiningTools_3', 1);
-            $item->setNamedTag($nbt);
-            $player->getInventory()->setItemInHand($item);
-            $player->sendMessage("§bMiningTools §7>> §a所持しているマイニングツールの変換に成功しました");
-        }
-    }
+    ///**
+    // * @param Player $player
+    // * @param Item   $item
+    // * @return void
+    // */
+    //public function itemNbtConversion(Player $player, Item $item): void {
+    //    if ($item->getNamedTag()->getTag('4mining') !== null) {
+    //        $nbt = $item->getNamedTag();
+    //        $tag = "4mining";
+    //        $nbt->removeTag($tag);
+    //        $nbt->setInt('MiningTools_3', 1);
+    //        $item->setNamedTag($nbt);
+    //        $player->getInventory()->setItemInHand($item);
+    //        $player->sendMessage("§bMiningTools §7>> §a所持しているマイニングツールの変換に成功しました");
+    //    }
+    //}
 }
