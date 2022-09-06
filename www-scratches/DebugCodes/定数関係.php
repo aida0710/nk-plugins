2f9ee96dbcafeff04ce080bdf2698439666fd26b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-06 16:45:58 +0900

    設定項目の更新

---
+++ www-scratches/DebugCodes/定数関係.php
@@ -0,0 +1,36 @@
+<?php
+
+const Test = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+];
+const Test2 = [
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+];
+const Test3 = [
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+];
+const All = [
+    ...Test,
+    ...Test2,
+    ...Test3,
+];
+var_dump(Test);
+var_dump(All);
