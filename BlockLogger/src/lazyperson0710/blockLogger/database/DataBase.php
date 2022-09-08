17e51745ce89846dcee25c5060437c809b629840
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-09 00:36:29 +0900

    sqliteの型を指定

--- BlockLogger/src/lazyperson0710/blockLogger/database/DataBase.php
+++ BlockLogger/src/lazyperson0710/blockLogger/database/DataBase.php
@@ -9,6 +9,6 @@ class DataBase extends SQLite3 {
 
     public function __construct(string $path) {
         parent::__construct("{$path}log.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
-        $this->query("CREATE TABLE IF NOT EXISTS log (Player TEXT, action TEXT, world TEXT, X TEXT, Y TEXT, Z TEXT, BlockName TEXT, BlockID TEXT, BlockMeta TEXT,Others TEXT, Date TEXT, Time TEXT)");
+        $this->query("CREATE TABLE IF NOT EXISTS log (Player TEXT, action TEXT, world TEXT, X INT, Y INT, Z INT, BlockName TEXT, BlockID INT, BlockMeta INT,Others TEXT, Date TEXT, Time TEXT)");
     }
 }
