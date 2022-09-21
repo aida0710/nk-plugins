23249f8425dc63b2870aa70159b55fdf6803a83d
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-22 08:52:23 +0900

    ログイン時にアイテムを取得してるかの処理を追加

---
+++ LoginBonus/src/lazyperson0710/LoginBonus/event/JoinPlayerEvent.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace lazyperson0710\LoginBonus\event;
+
+use lazyperson0710\LoginBonus\Main;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class JoinPlayerEvent implements Listener {
+
+    public function PlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        if (date("Y/m/d") !== date("Y/m/d", explode('.', $player->getLastPlayed() / 1000)[0])) {
+            //一日以上たってたら
+            self::check($player, true);
+            return;
+        }
+        //一日未満でアイテムがあった場合
+        if (Main::getInstance()->lastBonusDateConfig->exists($player->getName())) {
+            self::check($player);
+        }
+    }
+
+    public static function check(Player $player, ?bool $new = false): void {
+        //一日以上経過していてもデータがある場合が存在する
+        //データのみがある場合もある
+        $lastBonus = 0;
+        if (Main::getInstance()->lastBonusDateConfig->exists($player->getName())) {
+            $lastBonus = Main::getInstance()->lastBonusDateConfig->get($player->getName());
+            if ($new === true) {
+                $lastBonus += 1;
+            }
+        } elseif ($new === true) {
+            $lastBonus = 1;
+        }
+        if ($lastBonus === 0) {
+            Server::getInstance()->getLogger()->critical("ログインBonusのチェック処理の際に0が入力されました");
+            return;
+        }
+        $item = Main::getInstance()->loginBonusItem;
+        $item->setCount($lastBonus);
+        if ($player->getInventory()->canAddItem($item)) {
+            Main::getInstance()->lastBonusDateConfig->remove($player->getName());
+            $player->getInventory()->addItem($item);
+            $player->sendMessage('§bLogin §7>> §aログインボーナスを受け取りました');
+        } else {
+            Main::getInstance()->lastBonusDateConfig->set($player->getName(), $lastBonus);
+            $player->sendMessage("§bLogin §7>> §aインベントリに空きがないため、ログインボーナスを受け取れませんでした。/bonusから保留になっているログインボーナスアイテムを受け取ろう");
+        }
+        Main::getInstance()->lastBonusDateConfig->save();
+    }
+
+}
