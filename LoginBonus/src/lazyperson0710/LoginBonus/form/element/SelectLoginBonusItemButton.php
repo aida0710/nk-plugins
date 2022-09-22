b80c118fccd26c6839baf8729508af178a3d16f5
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-22 13:21:35 +0900

    ボタン送信時にログインボーナスアイテムを確認するように変更

--- LoginBonus/src/lazyperson0710/LoginBonus/form/element/SelectLoginBonusItemButton.php
+++ LoginBonus/src/lazyperson0710/LoginBonus/form/element/SelectLoginBonusItemButton.php
@@ -5,6 +5,7 @@ namespace lazyperson0710\LoginBonus\form\element;
 use bbo51dog\bboform\element\Button;
 use lazyperson0710\LoginBonus\calculation\checkInventoryItem;
 use lazyperson0710\LoginBonus\dataBase\LoginBonusItemInfo;
+use lazyperson0710\LoginBonus\form\BonusForm;
 use lazyperson0710\LoginBonus\form\convert\ItemConvertConfirmationForm;
 use lazyperson710\core\packet\SendForm;
 use pocketmine\player\Player;
@@ -22,7 +23,7 @@ class SelectLoginBonusItemButton extends Button {
         if (checkInventoryItem::init($player, $this->itemInfo->getCost())) {
             SendForm::Send($player, (new ItemConvertConfirmationForm($this->itemInfo)));
         } else {
-            $player->sendMessage("§cインベントリ内にあるログインボーナス数が足りません");
+            SendForm::Send($player, new BonusForm($player, "\n§cインベントリ内にあるログインボーナス数が足りません"));
         }
     }
 }
--- LoginBonus/src/lazyperson0710/LoginBonus/form/element/SelectLoginBonusTicketButton.php
+++ LoginBonus/src/lazyperson0710/LoginBonus/form/element/SelectLoginBonusTicketButton.php
@@ -4,6 +4,7 @@ namespace lazyperson0710\LoginBonus\form\element;
 
 use bbo51dog\bboform\element\Button;
 use lazyperson0710\LoginBonus\calculation\checkInventoryItem;
+use lazyperson0710\LoginBonus\form\BonusForm;
 use lazyperson0710\LoginBonus\form\convert\TicketConvertConfirmationForm;
 use lazyperson710\core\packet\SendForm;
 use pocketmine\player\Player;
@@ -23,9 +24,7 @@ class SelectLoginBonusTicketButton extends Button {
         if (checkInventoryItem::init($player, $this->cost)) {
             SendForm::Send($player, (new TicketConvertConfirmationForm($this->cost, $this->quantity)));
         } else {
-            //todo form内に表示させたい
-            //todo 一つ前に戻るImage
-            $player->sendMessage("§cインベントリ内にあるログインボーナス数が足りません");
+            SendForm::Send($player, new BonusForm($player, "\n§cインベントリ内にあるログインボーナス数が足りません"));
         }
     }
 }
