fb2716c70f203daf875fd1edf8fce8844da2f41e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-30 01:51:34 +0900

    不正なobjectが挿入された際に鯖を止めるように変更

--- LoginBonus/src/lazyperson0710/LoginBonus/form/convert/ItemSelectForm.php
+++ LoginBonus/src/lazyperson0710/LoginBonus/form/convert/ItemSelectForm.php
@@ -7,6 +7,7 @@ use lazyperson0710\LoginBonus\form\element\SelectLoginBonusItemButton;
 use lazyperson0710\LoginBonus\item\ItemRegister;
 use lazyperson0710\LoginBonus\item\LoginBonusItemInfo;
 use lazyperson710\sff\form\element\SendFormButton;
+use pocketmine\Server;
 
 class ItemSelectForm extends SimpleForm {
 
@@ -20,7 +21,8 @@ class ItemSelectForm extends SimpleForm {
             if ($item instanceof LoginBonusItemInfo) {
                 $this->addElement(new SelectLoginBonusItemButton($item->getCustomName() . "x" . $item->getQuantity() . " / Cost : " . $item->getCost() . "\n" . $item->getFormExplanation(), $item));
             } else {
-                //todo サーバー停止させる
+                Server::getInstance()->getLogger()->critical("ログインボーナスTicketリスト生成時に不正なobjectが挿入されました。危険と思われるため鯖を停止しました");
+                Server::getInstance()->shutdown();
                 return;
             }
         }
