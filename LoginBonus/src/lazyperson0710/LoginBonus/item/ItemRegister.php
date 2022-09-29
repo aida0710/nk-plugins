21b3ad64af0a15575ef198007c407a172d630700
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-09-30 01:52:06 +0900

    ログインボーナスアイテムを追加

--- LoginBonus/src/lazyperson0710/LoginBonus/item/ItemRegister.php
+++ LoginBonus/src/lazyperson0710/LoginBonus/item/ItemRegister.php
@@ -3,7 +3,9 @@
 namespace lazyperson0710\LoginBonus\item;
 
 use lazyperson0710\Gacha\Main;
+use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
 use pocketmine\item\VanillaItems;
 use pocketmine\utils\SingletonTrait;
 
@@ -18,10 +20,10 @@ class ItemRegister {
      */
     public function init(): void {
         ##Item交換
-        $this->itemRegister(VanillaItems::APPLE(), 30, 1, "赤いリンゴ", [], null, [], [], []);
-        $this->itemRegister(VanillaItems::APPLE(), 40, 1, "いリゴ", [], null, [], [], []);
-        $this->itemRegister(VanillaItems::APPLE(), 40, 3, "赤いンゴ", [], null, [], [], []);
-        $this->itemRegister(VanillaItems::APPLE(), 40, 3, "赤リンゴ", [], null, [], [], []);
+        $this->itemRegister(VanillaItems::APPLE(), 5, 1, "りんご", [], "ただのりんご", [], [], []);
+        $this->itemRegister(VanillaItems::BAKED_POTATO(), 32, 1, "ベイクドポテト", [], "主食にもってこいのじゃがいも！", [], [], []);
+        $this->itemRegister(ItemFactory::getInstance()->get(\lazyperson710\core\Main::ITEM_COMMAND_BLOCK), 1, 1, "コマンド記憶装置", ["使用方法\n\nスニークしないでタップすると指定されたコマンドを実行\nスニークしながらタップで実行するコマンドを指定"], "設定したコマンドを即座に実行可能に", [], [], []);
+        $this->itemRegister(VanillaItems::DIAMOND_PICKAXE(), 1, 8, "<神器>ダイヤのつるはし[効率lv.8]", ["過去実装された当時に強かったもの"], "当時は強かったもの", [VanillaEnchantments::EFFICIENCY()], [], []);
         $this->itemRegister(VanillaItems::APPLE(), 40, 3, "赤いゴ", [], null, [], [], []);
         $this->itemRegister(VanillaItems::APPLE(), 40, 3, "赤リンゴ", [], null, [], [], []);
         $this->itemRegister(VanillaItems::APPLE(), 40, 3, "いリンゴ", [], null, [], [], []);
