2f1b1760ed3944d620666c915d3e93662827f371
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-01 03:10:41 +0900

    ideaを追加5.26.0で実装予定

--- SaveProgress/src/lazyperson0710/SaveProgress/Main.php
+++ SaveProgress/src/lazyperson0710/SaveProgress/Main.php
@@ -7,6 +7,8 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
     protected function onEnable(): void {
+        //idea セーブデータの保存はプレイヤー名のファイルに各セーブデータを個別に突っ込む感じで
+        // 3分に一回保存するようにする
         //$setting_file = SettingsJson::getInstance();
         //$setting_file->init($this->getDataFolder());
         //$setting_file->output(PlayerSettingPool::getInstance());
