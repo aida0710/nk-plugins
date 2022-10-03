1fcf964ed37425c88a1bb4b29c1ef26895297971
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-03 22:38:04 +0900

    マイニングレベルの強制変更機能を追加

---
+++ ItemEdit/src/lazyperson710/edit/form/player/SetMiningLevelPlayer.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace lazyperson710\edit\form\player;
+
+use bbo51dog\bboform\element\Dropdown;
+use bbo51dog\bboform\element\Input;
+use bbo51dog\bboform\element\Label;
+use bbo51dog\bboform\element\Toggle;
+use bbo51dog\bboform\form\CustomForm;
+use Deceitya\MiningLevel\MiningLevelAPI;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class SetMiningLevelPlayer extends CustomForm {
+
+    private Dropdown $players;
+
+    private Toggle $enableLevel;
+    private Toggle $enableExp;
+    private Toggle $enableUpExp;
+
+    private Input $level;
+    private Input $exp;
+    private Input $upExp;
+
+    public function __construct() {
+        $names = null;
+        foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
+            $name = $onlinePlayer->getName();
+            $names[] .= $name;
+        }
+        $this->players = new Dropdown("プレイヤーを選択してください", $names);
+        $this->enableLevel = new Toggle("レベルの設定を有効化する", false);
+        $this->enableExp = new Toggle("経験値の設定を有効化する", false);
+        $this->enableUpExp = new Toggle("次のレベルまでの経験値の設定を有効化する", false);
+        $this->level = new Input("設定したいレベル値を入力してください", "1");
+        $this->exp = new Input("設定したい経験値を入力してください", "1");
+        $this->upExp = new Input("設定したい次のレベルまでの経験値を入力してください", "80");
+        $this
+            ->setTitle("Player Edit")
+            ->addElements(
+                $this->players,
+                new Label("設定したい項目を有効化してください\n有効化していない項目は数値を入力しても反映されません"),
+                $this->enableLevel,
+                $this->enableExp,
+                $this->enableUpExp,
+                new Label("設定した項目の数値を入力してください\n必ず1以上で入力してください"),
+                $this->level,
+                $this->exp,
+                $this->upExp,
+            );
+    }
+
+    public function handleSubmit(Player $player): void {
+        $targetName = $this->players->getSelectedOption();
+        if (!Server::getInstance()->getPlayerByPrefix($targetName)) {
+            $player->sendMessage("§bPlayerEdit §7>> §cプレイヤーが存在しない為、処理を中断しました");
+            return;
+        }
+        $target = Server::getInstance()->getPlayerByPrefix($targetName);
+        $level = $this->level->getValue();
+        $exp = $this->exp->getValue();
+        $upExp = $this->upExp->getValue();
+        $input = false;
+        $type = [
+            "level" => $level,
+            "exp" => $exp,
+            "upExp" => $upExp,
+        ];
+        foreach ($type as $key => $value) {
+            if (empty($value)) {
+                match ($key) {
+                    "level" => $level = false,
+                    "exp" => $exp = false,
+                    "upExp" => $upExp = false,
+                };
+                continue;
+            }
+            $input = true;
+            $output = $this->checkValue($key, $value);
+            if ($output === true) {
+                continue;
+            } else {
+                $player->sendMessage("§bPlayerEdit §7>> §c{$output}");
+                return;
+            }
+        }
+        if (!$input) {
+            $player->sendMessage("§bPlayerEdit §7>> §c一つでも値を入力してください");
+            return;
+        }
+        if ($this->enableLevel->getValue() === true) {
+            if ($level !== false) {
+                MiningLevelAPI::getInstance()->setLevel($target, $level);
+                if ($player->getName() === $target->getName()) {
+                    $player->sendMessage("§bPlayerEdit §7>> §aレベルを{$level}に設定しました");
+                } else {
+                    $player->sendMessage("§bPlayerEdit §7>> §aレベルを{$level}に設定しました");
+                    $target->sendMessage("§bPlayerEdit §7>> §aレベルを{$player->getName()}が{$level}に設定しました");
+                }
+            }
+        }
+        if ($this->enableExp->getValue() === true) {
+            if ($exp !== false) {
+                MiningLevelAPI::getInstance()->setExp($target, $exp);
+                if ($player->getName() === $target->getName()) {
+                    $player->sendMessage("§bPlayerEdit §7>> §a経験値を{$exp}に設定しました");
+                } else {
+                    $player->sendMessage("§bPlayerEdit §7>> §a経験値を{$exp}に設定しました");
+                    $target->sendMessage("§bPlayerEdit §7>> §a経験値を{$player->getName()}が{$exp}に設定しました");
+                }
+            }
+        }
+        if ($this->enableUpExp->getValue() === true) {
+            if ($upExp !== false) {
+                MiningLevelAPI::getInstance()->setLevelUpExp($target, $upExp);
+                if ($player->getName() === $target->getName()) {
+                    $player->sendMessage("§bPlayerEdit §7>> §a次のレベルまでの経験値を{$upExp}に設定しました");
+                } else {
+                    $player->sendMessage("§bPlayerEdit §7>> §a次のレベルまでの経験値を{$upExp}に設定しました");
+                    $target->sendMessage("§bPlayerEdit §7>> §a次のレベルまでの経験値を{$player->getName()}が{$upExp}に設定しました");
+                }
+            }
+        }
+    }
+
+    private function checkValue(string $key, string $value): bool|string {
+        $type = match ($key) {
+            "level" => "レベル",
+            "exp" => "現在の経験値",
+            "upExp" => "次のレベルまでの経験値",
+        };
+        if (!is_numeric($value)) {
+            return "{$type}入力では数字以外の文字列を入力しないでください";
+        }
+        $value = (int)$value;
+        if ($value < 0) {
+            return "{$type}入力では0以上の数値で入力してください";
+        }
+        if ($value > 15000) {
+            return "{$type}入力では15000以下の数値で入力してください";
+        }
+        return true;
+    }
+
+}
