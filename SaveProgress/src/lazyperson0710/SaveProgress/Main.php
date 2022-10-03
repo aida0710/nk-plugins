9e0864b4057a412fa6f059eae726e465e06ba664
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-03 22:54:52 +0900

    取り合えず基盤作成

--- SaveProgress/src/lazyperson0710/SaveProgress/Main.php
+++ SaveProgress/src/lazyperson0710/SaveProgress/Main.php
@@ -2,11 +2,21 @@
 
 namespace lazyperson0710\SaveProgress;
 
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
 
 class Main extends PluginBase {
 
-    protected function onEnable(): void {
+    use SingletonTrait;
+
+    public Config $config;
+    public array $data = [];
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents(new JoinEvent, $this);
+        $this->config = new Config($this->getDataFolder() . "s.yml", Config::YAML, SettingData::DefaultData);
         //idea セーブデータの保存はプレイヤー名のファイルに各セーブデータを個別に突っ込む感じで
         // 3分に一回保存するようにする
         //$setting_file = SettingsJson::getInstance();
@@ -15,6 +25,14 @@ class Main extends PluginBase {
         //$this->getServer()->getCommandMap()->registerAll("playerSetting", [
         //    new SettingCommand(),
         //]);
+    }
+
+    public function getFolder(Player $player): string {
+        $sub = substr($name, 0, 1);
+        $folder = $this->getDataFolder() . $upper . '/';
+        if (!file_exists($folder)) mkdir($folder);
+        $lower = strtolower($name);
+        return $folder .= $lower . '.json';
     }
 
 }
