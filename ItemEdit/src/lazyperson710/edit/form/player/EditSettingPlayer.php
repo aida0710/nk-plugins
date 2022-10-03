960f937b4ee2c5d96a7693e93f224e97868416e4
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-03 22:32:56 +0900

    他プレイヤーの設定を強制的に変更できるように

--- ItemEdit/src/lazyperson710/edit/form/player/EditSettingPlayer.php
+++ ItemEdit/src/lazyperson710/edit/form/player/EditSettingPlayer.php
@@ -2,58 +2,38 @@
 namespace lazyperson710\edit\form\player;
 
 use bbo51dog\bboform\element\Dropdown;
-use bbo51dog\bboform\element\Input;
 use bbo51dog\bboform\form\CustomForm;
+use lazyperson0710\PlayerSetting\form\SelectSettingForm;
+use lazyperson710\core\packet\SendForm;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
 class EditSettingPlayer extends CustomForm {
 
     private Dropdown $players;
-    private Input $expLevel;
 
-    public function __construct(Player $player) {
+    public function __construct() {
         $names = null;
         foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
             $name = $onlinePlayer->getName();
             $names[] .= $name;
         }
-        $this->players = new Dropdown("プレイヤーを選択してください", $names);
-        $this->expLevel = new Input("増加させたい経験値量を入力してください", "1");
+        $this->players = new Dropdown("設定を変更したいプレイヤーを選択してください", $names);
         $this
             ->setTitle("Player Edit")
             ->addElements(
                 $this->players,
-                $this->expLevel,
             );
     }
 
     public function handleSubmit(Player $player): void {
-        $players = $this->players->getSelectedOption();
-        $expLevel = $this->expLevel->getValue();
-        if (!Server::getInstance()->getPlayerByPrefix($players)) {
+        $targetName = $this->players->getSelectedOption();
+        if (!Server::getInstance()->getPlayerByPrefix($targetName)) {
             $player->sendMessage("§bPlayerEdit §7>> §cプレイヤーが存在しない為、処理を中断しました");
             return;
         }
-        $target = Server::getInstance()->getPlayerByPrefix($players);
-        if (!is_numeric($expLevel)) {
-            $player->sendMessage("§bPlayerEdit §7>> §c経験値量は数値で入力してください");
-            return;
-        }
-        if ($expLevel < 0) {
-            $player->sendMessage("§bPlayerEdit §7>> §c経験値レベルは0以上の数値で入力してください");
-            return;
-        }
-        if ($expLevel > 1241258) {
-            $player->sendMessage("§bPlayerEdit §7>> §c経験値レベルは1241258以下の数値で入力してください");
-            return;
-        }
-        $target->getXpManager()->setXpLevel($expLevel);
-        if ($player->getName() === $target->getName()) {
-            $player->sendMessage("§bPlayerEdit §7>> §a経験値レベルを{$expLevel}に設定しました");
-        } else {
-            $player->sendMessage("§bPlayerEdit §7>> §a経験値レベルを{$player->getName()}が{$expLevel}に設定しました");
-        }
+        $target = Server::getInstance()->getPlayerByPrefix($targetName);
+        SendForm::Send($player, new SelectSettingForm($target));
     }
 
 }
