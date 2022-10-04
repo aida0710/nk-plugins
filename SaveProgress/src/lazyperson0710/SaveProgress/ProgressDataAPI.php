28a012ab804e22c876effd9c6e1aac1abe3478ec
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-05 07:42:32 +0900

    アイデアを追加、取り合えず別の方法をとりたい

--- SaveProgress/src/lazyperson0710/SaveProgress/ProgressDataAPI.php
+++ SaveProgress/src/lazyperson0710/SaveProgress/ProgressDataAPI.php
@@ -3,7 +3,6 @@
 namespace lazyperson0710\SaveProgress;
 
 use JetBrains\PhpStorm\Pure;
-use JsonException;
 use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
@@ -17,27 +16,30 @@ class ProgressDataAPI {
      * @var array|string[]
      */
     private array $cache;
-    private Config $allConfigData;
+    private Config $config;
 
     public function setCache($dataFile): void {
-        $this->allConfigData = new Config($dataFile, Config::YAML);
-        $this->cache = $this->allConfigData->getAll();
+        $this->config = new Config($dataFile, Config::YAML);
+        $this->cache = $this->config->getAll();
+        //note loginしたときにデータを読み込む形で
+        // 退出時にデータ保存かな、たぶんそれが一番楽、下のループの必要ないしね
     }
 
     //hack この書き込み方だとかなりのラグが生まれる可能性があります
     // 別の方法を模索する必要がありますが思いつかんのでとりあえずこれで
     // プレイヤーの名前で個別に保存したい
+    // 非同期が現実的かな。。。
     public function dataSave(): bool {
         try {
-            //$this->config->setAll($this->cache);
-            foreach ($this->cache as $name => $data) {
-                $tempConfig = new Config(Main::getInstance()->getDataFolder() . $name . ".yml", Config::YAML);
-                $tempConfig->setAll($name[$data]);
-                $tempConfig->save();
-            }
-            $this->allConfigData->save();
+            //foreach ($this->cache as $name => $data) {
+            //    $tempConfig = new Config(Main::getInstance()->getDataFolder() . $name . ".yml", Config::YAML);
+            //    $tempConfig->setAll($name[$data]);
+            //    $tempConfig->save();
+            //}
+            $this->config->setAll($this->cache);
+            $this->config->save();
             return true;
-        } catch (JsonException $e) {
+        } catch (\JsonException $e) {
             Server::getInstance()->getLogger()->warning($e->getMessage());
             return false;
         }
