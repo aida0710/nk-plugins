6980f2d2b45e37e61424ded11d88cd85a7e5900d
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-05 13:08:55 +0900

    データの保存の処理を記述

--- SaveProgress/src/lazyperson0710/SaveProgress/ProgressDataAPI.php
+++ SaveProgress/src/lazyperson0710/SaveProgress/ProgressDataAPI.php
@@ -2,7 +2,6 @@
 
 namespace lazyperson0710\SaveProgress;
 
-use JetBrains\PhpStorm\Pure;
 use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
@@ -12,15 +11,13 @@ class ProgressDataAPI {
 
     use SingletonTrait;
 
-    /**
-     * @var array|string[]
-     */
     private array $cache;
     private Config $config;
 
-    public function setCache($dataFile): void {
-        $this->config = new Config($dataFile, Config::YAML);
-        $this->cache = $this->config->getAll();
+    public function setCache($datafile, Player $player): void {
+        $this->config[$player->getName()] = new Config($datafile, Config::YAML);
+        $this->cache[] = $this->config->getAll();
+        //おそらくこれで$this->cache[$player->getName()]にプレイヤーのデータが入るはず
         //note loginしたときにデータを読み込む形で
         // 退出時にデータ保存かな、たぶんそれが一番楽、下のループの必要ないしね
     }
@@ -28,16 +25,18 @@ class ProgressDataAPI {
     //hack この書き込み方だとかなりのラグが生まれる可能性があります
     // 別の方法を模索する必要がありますが思いつかんのでとりあえずこれで
     // プレイヤーの名前で個別に保存したい
-    // 非同期が現実的かな。。。
-    public function dataSave(): bool {
+    // 非同期が現実的かな。。
+    //todo 退出時にデータ保存、後でチェックして
+    public function dataSave(Player $player, ?bool $allData = false): bool {
         try {
             //foreach ($this->cache as $name => $data) {
             //    $tempConfig = new Config(Main::getInstance()->getDataFolder() . $name . ".yml", Config::YAML);
             //    $tempConfig->setAll($name[$data]);
             //    $tempConfig->save();
             //}
-            $this->config->setAll($this->cache);
+            $this->config->setAll($this->cache[$player->getName()]);
             $this->config->save();
+            //warning これだとプレイヤー一人が保存されたら全員なくなりそう
             return true;
         } catch (\JsonException $e) {
             Server::getInstance()->getLogger()->warning($e->getMessage());
@@ -47,12 +46,13 @@ class ProgressDataAPI {
 
     public function createData(Player $player): bool {
         if ($this->dataExists($player) === false) {
-            $this->cache += [$player->getName() => 0];
+            $this->cache[$player->getName()] = SettingData::DefaultData;
+            //データを書き換えるときに整合性のチェックも行いたい
             return true;
         } else return false;
     }
 
-    #[Pure] public function dataExists(Player $player): bool {
+    public function dataExists(Player $player): bool {
         return array_key_exists($player->getName(), $this->cache);
     }
 }
