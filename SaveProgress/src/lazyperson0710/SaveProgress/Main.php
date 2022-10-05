602c3463a680ab4168f13ae47f52c0483dd20837
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-05 13:09:37 +0900

    サーバーを閉じたときの処理を記述、起動時に行う処理を変更

--- SaveProgress/src/lazyperson0710/SaveProgress/Main.php
+++ SaveProgress/src/lazyperson0710/SaveProgress/Main.php
@@ -2,9 +2,6 @@
 
 namespace lazyperson0710\SaveProgress;
 
-use lazyperson0710\ticket\command\TicketCommand;
-use lazyperson0710\ticket\EventListener\BreakEventListener;
-use lazyperson0710\ticket\EventListener\JoinEventListener;
 use lazyperson0710\ticket\SaveTask;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
@@ -14,20 +11,16 @@ class Main extends PluginBase {
     use SingletonTrait;
 
     protected function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents(new BreakEventListener(), $this);
-        $this->getServer()->getPluginManager()->registerEvents(new JoinEventListener(), $this);
-        $this->getServer()->getCommandMap()->registerAll("ticketApi", [
-            new TicketCommand(),
+        $this->getServer()->getPluginManager()->registerEvents(new QuitEventListener(), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new JoinEvent(), $this);
+        $this->getServer()->getCommandMap()->registerAll("SaveProgress", [
+            //コマンドを追加
         ]);
-        if (!file_exists($this->getDataFolder())) {
-            mkdir($this->getDataFolder() . "TicketData.yml");
-        }
         $this->getScheduler()->scheduleRepeatingTask(new SaveTask(), 20 * 60);
-        TicketAPI::getInstance()->setCache($this->getDataFolder() . "TicketData.yml");
     }
 
     protected function onDisable(): void {
-        TicketAPI::getInstance()->dataSave();
+        //todo all data save
     }
 
     /* public Config $config;
