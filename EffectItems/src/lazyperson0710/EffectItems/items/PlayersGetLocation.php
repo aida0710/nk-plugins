ea947030f85883ae42402e26cafdb5396040a384
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-08 12:22:02 +0900

    他プレイヤーの位置を特定できるアイテムを追加

---
+++ EffectItems/src/lazyperson0710/EffectItems/items/PlayersGetLocation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace lazyperson0710\EffectItems\items;
+
+use lazyperson710\core\listener\Major;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\Server;
+use pocketmine\world\particle\RedstoneParticle;
+
+class PlayersGetLocation {
+
+    //debug デバックしてください
+    public static function execution(PlayerItemUseEvent|PlayerInteractEvent $event): void {
+        $player = $event->getPlayer();
+        $inHand = $player->getInventory()->getItemInHand();
+        $particle = new RedstoneParticle(15);
+        $count = 0;
+        foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
+            if ($player->getWorld() !== $onlinePlayer->getWorld()) continue;
+            for ($i = 0; $i <= 10; $i += 0.5) {
+                $pos = Major::coordinateCalculation($player->getPosition()->add(0, 1, 0), $onlinePlayer->getPosition()->add(0, 1, 0), $i * 0.1);
+                $player->getWorld()->addParticle($pos, $particle);
+            }
+            $count++;
+            $onlinePlayer->sendTip("§bLocation §7>> §c{$player->getName()}さんがあなたのいる場所を特定しました！");
+        }
+        if ($count === 0) {
+            $player->sendTip("§bLocation §7>> §c現在同じワールドにいるプレイヤーが一人もいない為処理が中断されました");
+            return;
+        }
+        $player->sendMessage("§bLocation §7>> §c{$player->getName()}さんがあなたのいる場所を特定しました！");
+        $event->getPlayer()->getInventory()->removeItem($inHand->setCount(1));
+    }
+
+}
