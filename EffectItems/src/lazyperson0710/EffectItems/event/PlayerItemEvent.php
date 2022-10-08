3bb3e750d978fe1ec355ff03d893d1fa66e6508e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-08 12:17:31 +0900

    処理の序列を変更

--- EffectItems/src/lazyperson0710/EffectItems/event/PlayerItemEvent.php
+++ EffectItems/src/lazyperson0710/EffectItems/event/PlayerItemEvent.php
@@ -7,7 +7,7 @@ use lazyperson0710\EffectItems\items\CommandStorage;
 use lazyperson0710\EffectItems\items\EffectCleaner;
 use lazyperson0710\EffectItems\items\HasteItem;
 use lazyperson0710\EffectItems\items\HeavenGrass;
-use lazyperson0710\EffectItems\items\PlayerTeleportTicket;
+use lazyperson0710\EffectItems\items\PlayersGetLocation;
 use lazyperson0710\EffectItems\items\RedBull;
 use lazyperson0710\EffectItems\items\Unknown;
 use lazyperson0710\EffectItems\items\ZONe;
@@ -31,18 +31,18 @@ class PlayerItemEvent implements Listener {
         $player = $event->getPlayer();
         $inHand = $player->getInventory()->getItemInHand();
         //todo 使用時のメッセージ表示
-        if ($inHand->getNamedTag()->getTag('AirBlock') !== null) AirBlock::init($event);
-        if ($inHand->getNamedTag()->getTag('HeavenGrass') !== null) HeavenGrass::init($event);
-        if ($inHand->getNamedTag()->getTag('RedBull') !== null) RedBull::init($event);
-        if ($inHand->getNamedTag()->getTag('ZONe') !== null) ZONe::init($event);
-        if ($inHand->getNamedTag()->getTag('UnknownItem') !== null) Unknown::init($event);
-        if ($inHand->getNamedTag()->getTag('PlayerTeleportTicket') !== null) PlayerTeleportTicket::init($event);
-        if ($inHand->getNamedTag()->getTag('EffectCleaner') !== null) EffectCleaner::init($event);
-        if ($inHand->getNamedTag()->getTag('CommandStorage') !== null) CommandStorage::init($event);
+        if ($inHand->getNamedTag()->getTag('AirBlock') !== null) AirBlock::execution($event);
+        if ($inHand->getNamedTag()->getTag('HeavenGrass') !== null) HeavenGrass::execution($event);
+        if ($inHand->getNamedTag()->getTag('RedBull') !== null) RedBull::execution($event);
+        if ($inHand->getNamedTag()->getTag('ZONe') !== null) ZONe::execution($event);
+        if ($inHand->getNamedTag()->getTag('UnknownItem') !== null) Unknown::execution($event);
+        if ($inHand->getNamedTag()->getTag('EffectCleaner') !== null) EffectCleaner::execution($event);
+        if ($inHand->getNamedTag()->getTag('CommandStorage') !== null) CommandStorage::execution($event);
+        if ($inHand->getNamedTag()->getTag('PlayersGetLocation') !== null) PlayersGetLocation::execution($event);
         //if ($inHand->getNamedTag()->getTag('ega') !== null) EnchantedGoldApple::init($event);
         //if ($inHand->getNamedTag()->getTag('ga') !== null) GoldApple::init($event);
-        if ($inHand->getId() === 383 && $inHand->getMeta() === 110) HasteItem::init($event);
-        if ($inHand->getId() === 383 && $inHand->getMeta() === 35) CommandStorage::init($event);
+        if ($inHand->getId() === 383 && $inHand->getMeta() === 110) HasteItem::execution($event);
+        if ($inHand->getId() === 383 && $inHand->getMeta() === 35) CommandStorage::execution($event);
     }
 
 }
