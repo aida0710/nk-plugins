0edfc5e284873bac07f510048729af21b986de12
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-10 10:01:51 +0900

    デバック、修正終わり

--- EffectItems/src/lazyperson0710/EffectItems/items/PlayersGetLocation.php
+++ EffectItems/src/lazyperson0710/EffectItems/items/PlayersGetLocation.php
@@ -10,13 +10,13 @@ use pocketmine\world\particle\RedstoneParticle;
 
 class PlayersGetLocation {
 
-    //debug デバックしてください
     public static function execution(PlayerItemUseEvent|PlayerInteractEvent $event): void {
         $player = $event->getPlayer();
         $inHand = $player->getInventory()->getItemInHand();
-        $particle = new RedstoneParticle(15);
+        $particle = new RedstoneParticle(50);
         $count = 0;
         foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
+            if ($onlinePlayer === $player) continue;
             if ($player->getWorld() !== $onlinePlayer->getWorld()) continue;
             for ($i = 0; $i <= 10; $i += 0.5) {
                 $pos = Major::coordinateCalculation($player->getPosition()->add(0, 1, 0), $onlinePlayer->getPosition()->add(0, 1, 0), $i * 0.1);
@@ -29,7 +29,7 @@ class PlayersGetLocation {
             $player->sendTip("§bLocation §7>> §c現在同じワールドにいるプレイヤーが一人もいない為処理が中断されました");
             return;
         }
-        $player->sendMessage("§bLocation §7>> §c{$player->getName()}さんがあなたのいる場所を特定しました！");
+        $player->sendTip("§bLocation §7>> §cプレイヤーの場所を特定しました");
         $event->getPlayer()->getInventory()->removeItem($inHand->setCount(1));
     }
 
