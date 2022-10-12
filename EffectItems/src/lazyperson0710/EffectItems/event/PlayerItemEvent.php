4ae6bac9d48f4562f1279327dc7206f3d2fcb48a
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-13 00:54:22 +0900

    todoを削除

--- EffectItems/src/lazyperson0710/EffectItems/event/PlayerItemEvent.php
+++ EffectItems/src/lazyperson0710/EffectItems/event/PlayerItemEvent.php
@@ -31,7 +31,6 @@ class PlayerItemEvent implements Listener {
     public function onItemEvents(PlayerItemUseEvent|PlayerInteractEvent $event) {
         $player = $event->getPlayer();
         $inHand = $player->getInventory()->getItemInHand();
-        //todo 使用時のSoundを付けたい
         if ($inHand->getNamedTag()->getTag('AirBlock') !== null) AirBlock::execution($event);
         if ($inHand->getNamedTag()->getTag('HeavenGrass') !== null) HeavenGrass::execution($event);
         if ($inHand->getNamedTag()->getTag('RedBull') !== null) RedBull::execution($event);
