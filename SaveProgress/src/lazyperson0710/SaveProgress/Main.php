9c9a8f363df080b480f116c1d4d43e4e8fbcf90e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-17 06:32:29 +0900

    作りなおしの為commit

--- SaveProgress/src/lazyperson0710/SaveProgress/Main.php
+++ SaveProgress/src/lazyperson0710/SaveProgress/Main.php
@@ -2,7 +2,6 @@
 
 namespace lazyperson0710\SaveProgress;
 
-use lazyperson0710\ticket\SaveTask;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
@@ -11,15 +10,11 @@ class Main extends PluginBase {
 
     protected function onEnable(): void {
         self::$DataFolder = $this->getDataFolder();
-        $this->getServer()->getPluginManager()->registerEvents(new QuitEventListener(), $this);
-        $this->getServer()->getPluginManager()->registerEvents(new JoinEvent(), $this);
         $this->getServer()->getCommandMap()->registerAll("SaveProgress", [
             //コマンドを追加
         ]);
-        $this->getScheduler()->scheduleRepeatingTask(new SaveTask(), 20 * 60);
     }
 
     protected function onDisable(): void {
-        //todo all data save
     }
 }
