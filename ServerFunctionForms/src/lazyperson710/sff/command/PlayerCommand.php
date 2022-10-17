bf80b0026e48e2c11a5b6b2c30a3e932b1b760bf
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-18 04:48:55 +0900

    ディレクトリの変更

--- ServerFunctionForms/src/lazyperson710/sff/command/PlayerCommand.php
+++ ServerFunctionForms/src/lazyperson710/sff/command/PlayerCommand.php
@@ -3,7 +3,7 @@
 namespace lazyperson710\sff\command;
 
 use lazyperson710\core\packet\SendForm;
-use lazyperson710\sff\form\PlayerInfoForm;
+use lazyperson710\sff\form\playerInfo\PlayerInfoForm;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
