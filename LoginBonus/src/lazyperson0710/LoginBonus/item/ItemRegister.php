5c50dd63662420b3b38e60a039ea7ce99b6008a5
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-19 13:06:07 +0900

    アイテム追加

--- LoginBonus/src/lazyperson0710/LoginBonus/item/ItemRegister.php
+++ LoginBonus/src/lazyperson0710/LoginBonus/item/ItemRegister.php
@@ -19,22 +19,21 @@ class ItemRegister {
      * @return void
      */
     public function init(): void {
-        ##Item交換
         //todo 交換するItemを追加してください
-        ##Cost - 1
+        #cost - 1
         $this->itemRegister(VanillaItems::APPLE(), 5, 1, "りんご", [], "ただのりんご", [], [], []);
         $this->itemRegister(VanillaItems::BAKED_POTATO(), 32, 1, "ベイクドポテト", [], "主食にもってこいのじゃがいも！", [], [], []);
-        //todo コマンド記憶装置の判定方法を変える必要があります
-        // 現在はid判定だとは思いますがそれに付随してnbt判定をメインで行いたい
-        // EffectItemsの方に動作クラスが存在すると思いますのでLoginBonusのeventListenerにも移動させたい
-        $this->itemRegister(ItemFactory::getInstance()->get(Main::ITEM_COMMAND_BLOCK), 1, 1, "コマンド記憶装置", ["使用方法\n\nスニークしないでタップすると指定されたコマンドを実行\nスニークしながらタップで実行するコマンドを指定"], "設定したコマンドを即座に実行可能に", [], [], []);
-        ##Cost - 3
-        //食べ物とか
-        $this->itemRegister(VanillaItems::DIAMOND_PICKAXE(), 1, 8, "<神器>ダイヤのつるはし[効率lv.8]", ["過去実装された当時に強かったもの"], "当時は強かったもの", [VanillaEnchantments::EFFICIENCY()], [8], []);
-        $this->itemRegister(VanillaItems::APPLE(), 40, 3, "赤いゴ", [], null, [], [], []);
-        $this->itemRegister(VanillaItems::APPLE(), 40, 3, "赤リンゴ", [], null, [], [], []);
-        $this->itemRegister(VanillaItems::APPLE(), 40, 3, "いリンゴ", [], null, [], [], []);
-        $this->itemRegister(VanillaItems::APPLE(), 30, 1, "ンゴ", [], "赤いリンゴじゃないです", [], [], []);
+        $this->itemRegister(ItemFactory::getInstance()->get(Main::ITEM_COMMAND_BLOCK), 1, 1, "コマンド記憶装置", ["使用方法\n\nスニークしないでタップすると指定されたコマンドを実行\nスニークしながらタップで実行するコマンドを指定"], "設定したコマンドを即座に実行可能に", [], [], ["CommandStorage"]);
+        #cost - 3
+        $this->itemRegister(VanillaItems::BREAD(), 32, 3, "パン", ["パンはパンでも食べたいパン"], "パンはパンでも食べたいパン", [], [], []);
+        $this->itemRegister(VanillaItems::STEAK(), 32, 3, "焼き牛肉", ["牛肉は焼いたほうが美味しい"], "牛肉は焼いたほうが美味しい", [], [], []);
+        $this->itemRegister(VanillaItems::EXPERIENCE_BOTTLE(), 64, 3, "魔剤", [], "ただの経験値瓶", [], [], []);
+        #cost - 5
+        $this->itemRegister(VanillaItems::DIAMOND_PICKAXE(), 1, 5, "<神器>ダイヤのつるはし[効率lv.8]", ["過去実装された当時は強かったもの"], "当時は強かったもの", [VanillaEnchantments::EFFICIENCY()], [8], []);
+        //マイニングツール
+        #cost - 8
+        //修繕無料のアイテム
+        //道具の名前変更
     }
 
     private function itemRegister(Item $item, int $quantity, int $cost, string $customName, array $lore, ?string $formExplanation, array $enchants, array $level, array $nbt): void {
