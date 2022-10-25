50bab8093451387ede96626b1a33dfa75274c568
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-10-25 10:30:07 +0900

    コメントアウトした文を削除

--- CustomRecipe-master/src/deceitya/CustomRecipe/CraftEvnetListener.php
+++ CustomRecipe-master/src/deceitya/CustomRecipe/CraftEvnetListener.php
@@ -27,24 +27,6 @@ class CraftEvnetListener implements Listener {
                     ]);
                     $player->getInventory()->addItem($output);
                     break;
-
-                //case "採掘速度上昇バフアイテム":
-                //    //case ItemFactory::getInstance()->get(383, 110, 1)://採掘速度上昇バフアイテム
-                //    $window = $player->getCurrentWindow() ?? $player->getCraftingGrid();
-                //    $table = $window->getContents();
-                //    foreach ($table as $id => $craftitem) {
-                //        $craftitem->setCount($craftitem->getCount() - 1);
-                //        $window->setItem($id, $craftitem);
-                //    }
-                //    $event->cancel();
-                //    $output = ItemFactory::getInstance()->get(383, 110, 1);
-                //    $output->setCustomName("採掘速度上昇バフアイテム");
-                //    $output->setLore([
-                //        "lore1" => "ネザライトインゴットを作成するための材料",
-                //        "lore2" => "タップすると3分間採掘速度上昇エフェクトが付与されます",
-                //    ]);
-                //    $player->getInventory()->addItem($output);
-                //    break;
             }
         }
     }
