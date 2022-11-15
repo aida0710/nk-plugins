d2879bc395871fd7e01ea57853b39554c34dbe98
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-11-15 17:09:18 +0900

    CoralButterflyを追加

---
+++ EffectItems/src/lazyperson0710/EffectItems/items/interactListener/effect/CoralButterfly.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace lazyperson0710\EffectItems\items\interactListener\effect;
+
+use lazyperson710\core\packet\AddEffectPacket;
+use lazyperson710\core\packet\SendMessage\SendTip;
+use lazyperson710\core\packet\SoundPacket;
+use lazyperson710\core\task\IntervalTask;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\item\Item;
+use pocketmine\player\GameMode;
+
+class CoralButterfly {
+
+    public static function execution(PlayerItemUseEvent|PlayerInteractEvent $event, Item $item): void {
+        $event->cancel();
+        $player = $event->getPlayer();
+        if (IntervalTask::check($player, 'EffectItems')) {
+            SendTip::Send($player, '現在エフェクトアイテムのインターバル中です', 'EffectItems', false);
+            return;
+        } else {
+            IntervalTask::onRun($player, 'EffectItems', 20 * 3);
+        }
+        if ($player->getGamemode() !== GameMode::CREATIVE()) {
+            $player->getInventory()->removeItem($item->setCount(1));
+        }
+        if (mt_rand(1, 2) === 1) {
+            $effect = new EffectInstance(VanillaEffects::HASTE(), 20 * 60 * 15, 50, false);
+            AddEffectPacket::Add($player, $effect, VanillaEffects::HASTE(), true);
+        } else {
+            $effect = new EffectInstance(VanillaEffects::INSTANT_DAMAGE(), 50, 255, false);
+            AddEffectPacket::Add($player, $effect, VanillaEffects::INSTANT_DAMAGE(), true);
+        }
+        SoundPacket::Send($player, 'item.trident.return');
+    }
+
+}
