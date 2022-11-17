eff9263d52de3b8781cbbaf8673e48fb24e9c02c
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-11-18 01:45:22 +0900

    環境ファイルの更新

--- www-scratches/実行ファイル.php
+++ www-scratches/実行ファイル.php
@@ -1,16 +1,32 @@
 <?php
 
-class a{
-    public function __construct(){
-        $class = "aaa";
-        var_dump($class);
-    }
+class Main {
 
-    public function b(){
-        $class = "bbb";
-        var_dump($class);
+    private const A = 100;
+    private int $PI = 0;
+
+    public function __construct() {
+        for ($i = 0; $i < self::A; $i++) {
+            if ($i % 2 == 0) {
+                $b = -1;
+            } else {
+                $b = 1;
+            }
+            $this->PI += (4 / (2 * $i - 1)) * $b;
+            sleep(1);
+            echo($this->PI);
+        }
     }
 }
 
-$class = new a();
+/*A=100
+pi=0
+forL in range(1,A):
+ifL%2==0:
+B=-1
+else:
+B=1
+pi+=(4/(2*L-1))*B
+print(pi)*/
+$class = new Main();
 var_dump($class);
