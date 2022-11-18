fd1edef1ed1a8c7f2dcb29bc12a2f2a0a7986c3b
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-11-18 23:53:08 +0900

    各種アイテムを登録

--- Gacha/src/lazyperson0710/Gacha/database/GachaItemAPI.php
+++ Gacha/src/lazyperson0710/Gacha/database/GachaItemAPI.php
@@ -54,73 +54,36 @@ class GachaItemAPI {
         $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaItems::EXPERIENCE_BOTTLE(), 16, "魔剤セット", "ちょっとずつ飲もう。。。", [], [], []);
         $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaBlocks::TNT()->asItem(), 1, "爆発物", "設置すると爆発します", [], [], ["ExplosionBlock"]);
         $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaItems::EGG(), 1, "温泉卵", "温まってるので中身はない", [], [], []);
+        $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaItems::GLASS_BOTTLE(), 1, "エフェクトクリーナー", "現在付与されているエフェクトを全て削除します", [VanillaEnchantments::PUNCH()], [1], ["EffectCleaner"]);
+        $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaItems::COOKED_FISH(), 1, "ラムネ", "美味しいよね", [], [], []);
+        $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaItems::PAPER(), 1, "バスタオル", "常時濡れてるので叩かれると若干痛い", [VanillaEnchantments::SHARPNESS()], [1], []);
         ## R Rank
-        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::COOKED_FISH(), 1, "ラムネ", "美味しいよね", [], [], []);
+        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::MAGMA_CREAM(), 1, "ラッキー苗木キューブ", "何かの苗木がもらえるよ！", [VanillaEnchantments::PUNCH()], [1], []);
         $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::COOKED_RABBIT(), 1, "モンスター", "カフェイン摂取キモチエェェェ！", [], [], []);
         $this->dischargeItemRegister("常駐ガチャ", "R", VanillaBlocks::COBBLESTONE_STAIRS()->asItem(), 1, "鈍器", "殴られたら痛いじゃ済まないだろう", [VanillaEnchantments::SHARPNESS()], [50], []);
         $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::STONE_SHOVEL(), 1, "鋭利なシャベル", "ゾンビを倒すために使うものではない", [VanillaEnchantments::SHARPNESS()], [5], []);
-        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::PAPER(), 1, "バスタオル", "常時濡れてるので叩かれると若干痛い", [VanillaEnchantments::SHARPNESS()], [1], []);
         $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::IRON_PICKAXE(), 1, "溶鉱炉付きのつるはし", "一部のブロックを採掘時に焼いちゃうぞ！うおうお！", [VanillaEnchantments::SILK_TOUCH(), VanillaEnchantments::PUNCH()], [1, 1], ["BlastFurnacePickaxe"]);
         $this->dischargeItemRegister("常駐ガチャ", "R", VanillaBlocks::POPPY()->asItem(), 1, "§cﾌｧｲﾔｰﾌﾗﾜｰ", "ふぁいやー！！！", [VanillaEnchantments::FIRE_ASPECT()], [1], []);
         $this->dischargeItemRegister("常駐ガチャ", "R", ItemFactory::getInstance()->get(751), 1, "落下ダメージ完全防御ブーツ", "着用していると落下ダメージから自分を守ってくれる", [], [], ["DefensiveStone"]);
+        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaBlocks::CORAL_FAN()->setCoralType(CoralType::BRAIN())->asItem(), 1, "ライトムーシュルーム", "脳を覚醒させてくれる\n\n効果\n\n暗視 lv.1 60分\n1/5の確率で毒 lv.3 15秒", [VanillaEnchantments::PUNCH()], [1], ["LightMushroom"]);
+        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::RAW_FISH(), 1, "ZONe", "効果\n\nコンジットパワー lv.3 5分", [VanillaEnchantments::PUNCH()], [1], ["ZONe"]);
+        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::COMPASS(), 1, "プレイヤー追跡機", "同じワールドにいる他プレイヤーの居場所から自身の座標まで直線状のパーティクルを出現させる", [VanillaEnchantments::PUNCH()], [1], ["PlayersGetLocation"]);
         ## SR Rank
-        //ちゅーる sr
-        //体力増強30s 1lv
-        //スピード30s 2lv
-        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::COOKED_MUTTON(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::COOKED_CHICKEN(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        //
-        //コーラルバタフライ ssr
-        //採掘速度上昇 15m 50lv
-        //2分の1の確率でインスタントダメージ 50s 255lv
-        $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::SWEET_BERRIES(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        //
-        //クリーミーポテト ssr
-        //採掘速度上昇 60s 3lv
-        //5秒後に毒 25s 5lv
-        $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::POISONOUS_POTATO(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        //
-        //ヘイストアイテム sr
-        //採掘速度上昇 30s 50lv
-        //移動速度低下 90s 15lv
-        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaBlocks::Lantern()->asItem(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        //
-        //天国草 sr
-        //移動速度低下 5s 5lv
-        //耐性 120s 3lv
-        //25秒後に吐き気 15s 15lv
-        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaBlocks::CORAL_FAN()->setCoralType(CoralType::FIRE())->asItem(), 1, "天国草", "天国で群生してる草", [], [], ["HeavenGrass"]);
-        //
-        //ライトムーシュルーム r
-        //暗視 60m 1lv
-        //5分の1の確率で毒 15s 3lv
-        $this->dischargeItemRegister("常駐ガチャ", "R",  VanillaBlocks::CORAL_FAN()->setCoralType(CoralType::BRAIN())->asItem(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        //
-        //レッドブル sr
-        //浮遊 3s 30lv
-        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::WATER_POTION(), 1, "RedBull", "翼を授ける", [], [], ["RedBull"]);
-        //
-        //アンノウンアイテム ssr
-        //全てのエフェクトを3m 50lv
-        $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::DRAGON_BREATH(), 1, "繝｢繝ｳ繧ｹ繧ｿ繝ｼ", "驛｢譎｢?ｽ?｢驛｢譎｢?ｽ?ｳ驛｢?ｧ??ｹ驛｢?ｧ??ｿ驛｢譎｢?ｽ?ｼ", [], [], ["UnknownItem"]);
-        //
-        //ゾーン r
-        //コンジットパワー 5m 3lv
-        $this->dischargeItemRegister("常駐ガチャ", "R", VanillaItems::RAW_FISH(), 1, "ZONe", "現在付与されているエフェクトを全て削除します", [VanillaEnchantments::PUNCH()], [1], ["EffectCleaner"]);
-        //
-        //エフェクトクリーナー uc
-        $this->dischargeItemRegister("常駐ガチャ", "UC", VanillaItems::GLASS_BOTTLE(), 1, "エフェクトクリーナー", "現在付与されているエフェクトを全て削除します", [VanillaEnchantments::PUNCH()], [1], ["EffectCleaner"]);
-        //
-        //ラッキーexpCoin r
-        //ラッキーfoodCoin r
-        //ラッキーmoneyCoin sr
-        //プレイヤーロケーション r
-        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::COMPASS(), 1, "プレイヤー追跡機", "同じワールドにいる他プレイヤーの居場所から自身の座標まで直線状のパーティクルを出現させる", [VanillaEnchantments::PUNCH()], [1], ["PlayersGetLocation"]);
-        //
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::COOKED_MUTTON(), 1, "ちゅーる", "効果\n\n体力増強 lv.1 30s\nスピード lv.2 30秒", [], [], ["Churu"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::COOKED_CHICKEN(), 1, "ちゅーる", "効果\n\n体力増強 lv.1 30s\nスピード lv.2 30秒", [], [], ["Churu"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaBlocks::Lantern()->asItem(), 1, "ヘイストアイテム", "妖精の入ったランタンを照らして。。。\n\n効果\n\n採掘速度上昇 lv.50 30秒\n移動速度低下 lv.15 90秒", [VanillaEnchantments::PUNCH()], [1], ["HasteItem"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaBlocks::CORAL_FAN()->setCoralType(CoralType::FIRE())->asItem(), 1, "天国草", "天国で群生してる草\n\n効果\n\n移動速度低下 lv.5 5秒\n耐性 lv.3 2分\n25秒後に吐き気 lv.15 15秒", [VanillaEnchantments::PUNCH()], [1], ["HeavenGrass"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::WATER_POTION(), 1, "RedBull", "翼を授ける\n\n効果\n\n浮遊 lv.30 3秒", [VanillaEnchantments::PUNCH()], [1], ["RedBull"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::WOODEN_PICKAXE(), 1, "すぴにー(実写)", "効果\n\nなし", [VanillaEnchantments::PUNCH()], [1], []);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::ENDER_PEARL(), 1, "ラッキーEXPオーブ", "expがもらえるよ！", [VanillaEnchantments::PUNCH()], [1], []);
+        $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::GOLD_NUGGET(), 1, "ラッキーマネーコイン", "お金がもらえるよ！", [VanillaEnchantments::PUNCH()], [1], []);
         $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaBlocks::MONSTER_SPAWNER()->asItem(), 1, "レベルブロック", "れぇべぇるぅ\n破壊するとバニラ経験値がもらえます", [], [], []);
         $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::DIAMOND_PICKAXE(), 1, "黒曜石ブレイカー", "黒曜石だけを徹底的に破壊する", [VanillaEnchantments::PUNCH()], [1], ["ObsidianBreaker"]);
         $this->dischargeItemRegister("常駐ガチャ", "SR", VanillaItems::DIAMOND_HOE(), 1, "グロウストーンブレイカー", "グロウストーンだけを徹底的に破壊する", [VanillaEnchantments::PUNCH()], [1], ["GlowstoneBreaker"]);
         ## SSR Rank
+        $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::DRAGON_BREATH(), 1, "繝｢繝ｳ繧ｹ繧ｿ繝ｼ", "驛｢譎｢?ｽ?｢驛｢譎｢?ｽ?ｳ驛｢?ｧ??ｹ驛｢?ｧ??ｿ驛｢譎｢?ｽ?ｼ", [VanillaEnchantments::PUNCH()], [1], ["UnknownItem"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::SWEET_BERRIES(), 1, "コーラル バタフライ", "効果\n\n採掘速度上昇 lv.50 15分\n二分の一の確率でデメリットが発生します", [VanillaEnchantments::PUNCH()], [1], ["CoralButterfly"]);
+        $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::POISONOUS_POTATO(), 1, "クリーミーポテト", "本当にクリームみたいな柔らかさのじゃかいもだね\n\n効果\n\n採掘速度上昇 lv.3 60秒\n5秒経過後に毒 lv.5 25秒", [VanillaEnchantments::PUNCH()], [1], ["CreamyPotato"]);
         $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::FISHING_ROD(), 1, "SuperRod", "どこまでも走れる気がする", [VanillaEnchantments::PUNCH()], [1], []);
         $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::GOLD_INGOT(), 1, "道具名チェンジャー", "道具、装備の名前を変更することが可能です\n名前を変更したいアイテムを持った状態で/itemsコマンドを実行してください", [VanillaEnchantments::PROJECTILE_PROTECTION()], [5], ["ItemNameChangeIngot"]);
         $this->dischargeItemRegister("常駐ガチャ", "SSR", VanillaItems::IRON_INGOT(), 1, "修繕費用代替インゴット", "アイテムの修繕費用(ExpLevel)を代替します", [VanillaEnchantments::MENDING()], [1], ["repairIngot"]);
