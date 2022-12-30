afffbfa1a109beee582a85f19cd67a1d0e034e73
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-12-30 22:21:55 +0900

    実行コマンドを修正

--- ServerFunctionForms/src/lazyperson710/sff/form/InformationForm.php
+++ ServerFunctionForms/src/lazyperson710/sff/form/InformationForm.php
@@ -15,7 +15,7 @@ class InformationForm extends SimpleForm {
         $this->setText("見たいコンテンツを選択してください\nこのFormは/infoで使用可能です");
         $this->addElements(
             new CommandDispatchButton("お知らせ", "announce", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/invite_base.png")),
-            new CommandDispatchButton("コマンドリスト\nコマンドリストを表示する", "command", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/mute_off.png")),
+            new CommandDispatchButton("コマンドリスト\nコマンドリストを表示する", "cmdls", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/mute_off.png")),
             new CommandDispatchButton("機能一覧", "function", new ButtonImage(ButtonImage::TYPE_PATH, "textures/items/book_portfolio.png")),
             new CommandDispatchButton("仕様一覧", "specification", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/debug_glyph_color.png")),
             new SendFormButton(new TosForm(), "利用規約\nweb版:www.nkserver.net/tos.html", new ButtonImage(ButtonImage::TYPE_PATH, "textures/ui/Feedback.png")),
