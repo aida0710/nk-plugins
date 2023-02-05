d75b9fa399129f61571181ba4b515b22d5cd00b8
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-02-05 11:45:32 +0900

    レアリティークラスを作成

---
+++ Gacha/src/lazyperson0710/Gacha/database/RarityMap.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+namespace lazyperson0710\Gacha\database;
+
+/**
+ * @generate-registry-docblock
+ * @method static RarityMap C()
+ * @method static RarityMap UC()
+ * @method static RarityMap R()
+ * @method static RarityMap SR()
+ * @method static RarityMap SSR()
+ * @method static RarityMap L()
+ */
+class RarityMap {
+
+    //null
+}
