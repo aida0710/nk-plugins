28575fb4b1a2c74c1cb4448afaa7f9ddcb0b16f4
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-02-10 22:14:06 +0900

    基盤を作成

---
+++ MoneySystem/src/lazyperson0710/MoneySystem/MoneySystem.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace lazyperson0710\MoneySystem;
+
+use pocketmine\plugin\PluginBase;
+
+class MoneySystem extends PluginBase {
+
+	/**
+	 * @return void
+	 */
+	protected function onEnable() : void {
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function onDisable() : void {
+	}
+
+}
