ca2e86c9fdb820c3c5b02dcc65b6392c88a75d23
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-02-11 11:51:35 +0900

    donationFormにて見にくい文字を修正

--- ServerFunctionForms/src/lazyperson710/sff/form/donation/DonationForm.php
+++ ServerFunctionForms/src/lazyperson710/sff/form/donation/DonationForm.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types = 1);
+
 namespace lazyperson710\sff\form\donation;
 
 use bbo51dog\bboform\form\SimpleForm;
@@ -43,25 +44,20 @@ class DonationForm extends SimpleForm {
 	];
 
 	public function __construct(?string $message = null) {
-		//$msg1 = "なま鯖では寄付を募っています。\n寄付方法はamazonギフトカードでDiscordにてなまけもののDMでのみ受け付けさせて頂いています。";
-		//$msg2 = "また、寄付して頂いた額に応じて全員が特典を得ることができます\n特典は寄付額に応じて以下のボタンから寄付を受け取り可能です。";
-		//$msg3 = '特典に関しては寄付していただいた際に寄付してくださった方の要望をある程度反映したいと考えております。';
-		//$msg4 = '以下、今期が始まってからの総寄付金額';
+		$msg1 = "なま鯖では寄付を募っています。\n寄付方法はamazonギフトカードでDiscordにてなまけもののDMでのみ受け付けさせて頂いています。" . PHP_EOL;
+		$msg2 = "また、寄付して頂いた額に応じて全員が特典を得ることができます\n特典は寄付額に応じて以下のボタンから寄付を受け取り可能です。" . PHP_EOL;
+		$msg3 = '特典に関しては寄付していただいた際に寄付してくださった方の要望をある程度反映したいと考えております。' . PHP_EOL . PHP_EOL;
+		$msg4 = '総寄付金額 11000円' . PHP_EOL;
 		$this
 			->setTitle('Donation Form')
-			->setText("なま鯖では寄付を募っています。\n寄付方法はamazonギフトカードでDiscordにてなまけもののDMでのみ受け付けさせて頂いています。\n\n
-            また、寄付して頂いた額に応じて全員が特典を得ることができます\n特典は寄付額に応じて以下のボタンから寄付を受け取り可能です。\n\n
-            特典に関しては寄付していただいた際に寄付してくださった方の要望をある程度反映したいと考えております。\n\n
-            以下、今期が始まってからの総寄付金額\n\n
-            {$message}
-            ");
+			->setText($msg1 . $msg2 . $msg3 . $msg4 . $message);
 		foreach (self::DonationInfo as $key => $value) {
 			$key = (int) $key;
 			if ($value['enable']) {
 				if ($value['item']) {
-					$this->addElement(new DonationsButton("{$key}円寄付 | §a特典: アイテム§r\n寄付者: {$value['donor']}", $key, $value['donor']));
+					$this->addElement(new DonationsButton("{$key}円寄付 | §d特典: アイテム§r\n寄付者: {$value['donor']}", $key, $value['donor']));
 				} else {
-					$this->addElement(new DonationsButton("{$key}円寄付 | §a特典: 機能実装§r\n寄付者: {$value['donor']}", $key, $value['donor']));
+					$this->addElement(new DonationsButton("{$key}円寄付 | §d特典: 機能実装§r\n寄付者: {$value['donor']}", $key, $value['donor']));
 				}
 			} else {
 				$this->addElement(new DonationsButton("{$key}円寄付 | §c現在未開放§r\n寄付者: None", $key));
