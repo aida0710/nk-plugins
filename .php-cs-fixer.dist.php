ea3c4efb8a37cf0c8a7e8a2666cf7ada8c9004a6
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-02-11 11:52:24 +0900

    設定を更新

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types = 1);
-include_once __DIR__ . "/vendor/autoload.php";
+include_once __DIR__ . '/vendor/autoload.php';
 $finder = PhpCsFixer\Finder::create()
 	->in(__DIR__);
 return (new PhpCsFixer\Config())
--- www-scratches/実行ファイル.php
+++ www-scratches/実行ファイル.php
@@ -1,13 +1,5 @@
 <?php
 
 declare(strict_types = 1);
-$nextExp = 80;
-foreach (range(1, 811) as $lv) {
-	if ($lv === 1) {
-		$nextExp = 80;
-		echo "[LV.$lv] " . $nextExp . PHP_EOL;
-		continue;
-	}
-	$nextExp += $lv - 1;
-	echo "[LV.$lv] " . $nextExp . PHP_EOL;
-}
+
+var_dump(737942943942974294737 - 873247538427826482462);
