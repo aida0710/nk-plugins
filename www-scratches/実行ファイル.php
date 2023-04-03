1839525414cb4be7ca0b3dc5a6b95051f3230b93
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-04 00:52:54 +0900

    設定ファイルを追加

--- www-scratches/実行ファイル.php
+++ www-scratches/実行ファイル.php
@@ -1,23 +1,23 @@
 <?php
 
-declare(strict_types = 1);
+class Human {
 
-class Main {
+	private int $年齢;
+	private string $名前;
 
-	private int $member;
-
-	public function __construct(?int $member = null){
-		if($member === null) return;
-		self::$member = $member;
+	public function __construct(int $年齢, string $名前) {
+		$this->年齢 = $年齢;
+		$this->名前 = $名前;
 	}
 
-	public function getMember() : int{
-		return self::$member;
+	public function get年齢() : int {
+		return $this->年齢;
+	}
+
+	public function get名前() : string {
+		return $this->名前;
 	}
 }
 
-$main = new Main(10);
-var_dump($main->getMember()); //10
-$main = new Main(null);
+var_dump(new Human(18, 'lazyperson0710'));
 
-var_dump($main->getMember()); //10
