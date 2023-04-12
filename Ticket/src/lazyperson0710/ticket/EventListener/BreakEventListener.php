46730789f820cf930ff2d11ff531ce968e70e0e8
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-13 05:29:00 +0900

    Ticketをまとめて配布するように

--- Ticket/src/lazyperson0710/ticket/EventListener/BreakEventListener.php
+++ Ticket/src/lazyperson0710/ticket/EventListener/BreakEventListener.php
@@ -6,22 +6,23 @@ namespace lazyperson0710\ticket\EventListener;
 
 use lazyperson0710\miningtools\event\MiningToolsBreakEvent;
 use lazyperson0710\ticket\TicketAPI;
-use lazyperson710\core\packet\SendMessage\SendBroadcastTip;
+use lazyperson710\core\packet\SendNoSoundMessage\SendNoSoundTip;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
+use pocketmine\Server;
 use function mt_rand;
 
 class BreakEventListener implements Listener {
+
+	private array $ticket_temp = [];
 
 	/**
 	 * @priority MONITOR
 	 */
 	public function onBlockBreak(BlockBreakEvent $event) : void {
 		if ($event->isCancelled()) return;
-		$probability = '0.125';
-		$random = mt_rand(1, 800);
-		$this->giveTicket($event->getPlayer(), $random, $probability);
+		$this->setCache($event->getPlayer(), 'normal');
 	}
 
 	/**
@@ -29,15 +30,36 @@ class BreakEventListener implements Listener {
 	 */
 	public function onMiningToolsBlockBreak(MiningToolsBreakEvent $event) : void {
 		if ($event->isCancelled()) return;
-		$probability = '0.0769';
-		$random = mt_rand(1, 1300);
-		$this->giveTicket($event->getPlayer(), $random, $probability);
+		$this->setCache($event->getPlayer(), 'miningTools');
 	}
 
-	private function giveTicket(Player $player, int $random, string $probability) : void {
-		if ($random === 500) {
-			TicketAPI::getInstance()->addTicket($player, 1);
-			SendBroadcastTip::Send("Ticketを{$probability}％の確率で{$player->getName()}がゲットしました", 'Ticket');
-		}
+	private function setCache(Player $player, string $type) : void {
+		$this->ticket_temp[$player->getName()][$type]['count']++;
 	}
+
+	public function giveTicket() : void {
+		$temp = $this->ticket_temp;
+		$this->ticket_temp = [];
+		$rand_normal = mt_rand(1, 800);
+		$rand_miningTools = mt_rand(1, 1300);
+		foreach ($temp as $player_name) {
+			$count = 0;
+			$normal = 0;
+			$miningTools = 0;
+			if (isset($player_name['normal'])) $normal = $player_name['normal']['count'];
+			if (isset($player_name['miningTools'])) $miningTools = $player_name['miningTools']['count'];
+			for ($k = 0; $k < $normal; $k++) {
+				if ($rand_normal === 500) $count++;
+			}
+			for ($k = 0; $k < $miningTools; $k++) {
+				if ($rand_miningTools === 500) $count++;
+			}
+			if (!is_null($player = Server::getInstance()->getPlayerExact($player_name))) {
+				SendNoSoundTip::Send($player, '3分間でチケットを' . $count . '枚獲得しました', 'Ticket', true);
+			} elseif (is_null($player = Server::getInstance()->getOfflinePlayer($player_name))) throw new \RuntimeException('オフラインプレイヤーのデータが見つかりませんでした');
+			TicketAPI::getInstance()->addTicket($player, $count);
+		}
+		var_dump($temp);
+	}
+
 }
--- Ticket/src/lazyperson0710/ticket/TicketTask.php
+++ Ticket/src/lazyperson0710/ticket/TicketTask.php
@@ -4,11 +4,13 @@ declare(strict_types = 0);
 
 namespace lazyperson0710\ticket;
 
+use lazyperson0710\ticket\EventListener\BreakEventListener;
 use pocketmine\scheduler\Task;
 
-class SaveTask extends Task {
+class TicketTask extends Task {
 
 	public function onRun() : void {
+		(new BreakEventListener())->giveTicket();
 		TicketAPI::getInstance()->dataSave();
 	}
 }
