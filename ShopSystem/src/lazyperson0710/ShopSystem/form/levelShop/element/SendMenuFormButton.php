a171fdc78c729ad91b77d135f130e01e1865ab97
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-13 13:46:38 +0900

    通常画面の送信用ボタンを追加

--- ShopSystem/src/lazyperson0710/ShopSystem/form/levelShop/element/SendMenuFormButton.php
+++ ShopSystem/src/lazyperson0710/ShopSystem/form/levelShop/element/SendMenuFormButton.php
@@ -7,8 +7,7 @@ namespace lazyperson0710\ShopSystem\form\levelShop\element;
 use bbo51dog\bboform\element\Button;
 use bbo51dog\bboform\element\ButtonImage;
 use bbo51dog\bboform\form\FormBase;
-use bbo51dog\bboform\form\SimpleForm;
-use lazyperson0710\ShopSystem\form\levelShop\future\LevelConfirmation;
+use lazyperson0710\ShopSystem\form\levelShop\future\LevelCheck;
 use pocketmine\player\Player;
 
 class SendMenuFormButton extends Button {
@@ -16,13 +15,13 @@ class SendMenuFormButton extends Button {
 	private FormBase $form;
 	private int $restrictionLevel;
 
-	public function __construct(string $text, SimpleForm $form, int $restrictionLevel, ?ButtonImage $image = null) {
+	public function __construct(string $text, FormBase $form, int $restrictionLevel, ?ButtonImage $image = null) {
 		parent::__construct($text, $image);
 		$this->form = $form;
 		$this->restrictionLevel = $restrictionLevel;
 	}
 
 	public function handleSubmit(Player $player) : void {
-		LevelConfirmation::getInstance()->levelConfirmation($player, $this->form, $this->restrictionLevel);
+		LevelCheck::getInstance()->check($player, $this->form, $this->restrictionLevel);
 	}
 }
