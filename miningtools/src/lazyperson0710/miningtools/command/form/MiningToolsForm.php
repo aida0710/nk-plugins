b6b23fa887d6a07dab08e390e8d2f31e228560c8
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-13 13:41:36 +0900

    コマンド実行式ではなくformをそのまま送信するように

--- miningtools/src/lazyperson0710/miningtools/command/form/MiningToolsForm.php
+++ miningtools/src/lazyperson0710/miningtools/command/form/MiningToolsForm.php
@@ -6,9 +6,11 @@ namespace lazyperson0710\miningtools\command\form;
 
 use bbo51dog\bboform\form\SimpleForm;
 use Deceitya\MiningLevel\MiningLevelAPI;
-use lazyperson0710\miningtools\element\CommandDispatchButton;
 use lazyperson0710\miningtools\element\SendFormButton;
+use lazyperson0710\miningtools\element\SendFormLevelCheckButton;
 use lazyperson0710\miningtools\EnablingSetting\SelectEnablingSettings;
+use lazyperson0710\miningtools\extensions\ExtensionsMainForm;
+use lazyperson0710\miningtools\normal\ConfirmForm;
 use pocketmine\player\Player;
 
 class MiningToolsForm extends SimpleForm {
@@ -37,9 +39,9 @@ class MiningToolsForm extends SimpleForm {
 			->setTitle('Mining Tools')
 			->setText('見たいコンテンツを選択してください')
 			->addElements(
-				new CommandDispatchButton("Diamond Tools - 範囲採掘ツール\n{$diamondLimit}", 'dmt', null),
-				new CommandDispatchButton("Netherite Tools - 範囲採掘ツール\n{$netheriteLimit}", 'nmt', null),
-				new CommandDispatchButton("アップグレード - Ex範囲採掘ツール\n{$expansionLimit}", 'emt', null),
+				new SendFormLevelCheckButton(new ConfirmForm($player, 'diamond'), "Diamond Tools - 範囲採掘ツール\n{$diamondLimit}", self::DiamondMiningToolsLevelLimit),
+				new SendFormLevelCheckButton(new ConfirmForm($player, 'netherite'), "Netherite Tools - 範囲採掘ツール\n{$netheriteLimit}", self::NetheriteMiningToolsLevelLimit),
+				new SendFormLevelCheckButton(new ExtensionsMainForm($player), "アップグレード - Ex範囲採掘ツール\n{$expansionLimit}", self::ExpansionMiningToolsLevelLimit, true),
 				new SendFormButton(new SelectEnablingSettings($player), '設定機能を有効化'),
 			);
 	}
