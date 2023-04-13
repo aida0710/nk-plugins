8297221ea0c0da26d0c1461f9600209631bf372e
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-13 13:44:00 +0900

    アイテムの選択画面を作成

--- ShopSystem/src/lazyperson0710/ShopSystem/form/levelShop/ItemSelectForm.php
+++ ShopSystem/src/lazyperson0710/ShopSystem/form/levelShop/ItemSelectForm.php
@@ -6,21 +6,28 @@ namespace lazyperson0710\ShopSystem\form\levelShop;
 
 use bbo51dog\bboform\form\SimpleForm;
 use lazyperson0710\ShopSystem\database\ItemShopAPI;
-use lazyperson0710\ShopSystem\form\levelShop\element\FirstBackFormButton;
+use lazyperson0710\ShopSystem\form\levelShop\element\SendMenuFormButton;
+use lazyperson0710\ShopSystem\form\levelShop\future\RestrictionShop;
 use lazyperson0710\ShopSystem\form\levelShop\future\ShopText;
+use pocketmine\player\Player;
 
 class ItemSelectForm extends SimpleForm implements ShopText {
 
-	public function __construct(int $shopNumber, string $category) {
+	public function __construct(Player $player, int $shopNumber, string $category) {
 		$shopItems = ItemShopAPI::getInstance()->getCategoryItems($shopNumber, $category);
+		$restriction = RestrictionShop::getInstance()->getRestrictionByShopNumber($shopNumber);
 		$this
 			->setTitle(self::TITLE)
 			->setText(self::CONTENT);
 		foreach ($shopItems as $item) {
-			//todo 実際にアイテムの確認画面に移行するように修正
-			$this->addElement(new ShopItemButton($item->getDisplayName(), $shopNumber, $category));
+			if ($item->getSell() === 0) {
+				$this->addElement(new SendMenuFormButton($item->getDisplayName() . PHP_EOL . '購入: ' . number_format($item->getBuy()) . '円 / 売却: 不可', new ItemBuyForm($player, $item), $restriction));
+			} else {
+				$this->addElement(new SendMenuFormButton($item->getDisplayName() . PHP_EOL . '購入: ' . number_format($item->getBuy()) . '円 / 売却: ' . number_format($item->getSell()) . '円', new SelectTypeForm($player, $item), $restriction));
+			}
 		}
-		$this->addElement(new FirstBackFormButton('ショップ選択メニューに戻る'));
+		$this->addElement(new SendMenuFormButton('このカテゴリのアイテムを一括売却する', new BulkSaleForm($player, $shopNumber, $category), $restriction));
+		$this->addElement(new SendMenuFormButton('カテゴリ選択メニューに戻る', new CategorySelectForm($player, $shopNumber), $restriction));
 	}
 
 }
