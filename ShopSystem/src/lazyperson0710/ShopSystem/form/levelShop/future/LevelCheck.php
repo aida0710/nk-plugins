90c868a412f8b9a455aee796db7c0b7d4a93a7ca
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-04-13 13:48:47 +0900

    レベルチェックを容易に行えるように関数を作成

--- ShopSystem/src/lazyperson0710/ShopSystem/form/levelShop/future/LevelCheck.php
+++ ShopSystem/src/lazyperson0710/ShopSystem/form/levelShop/future/LevelCheck.php
@@ -9,16 +9,17 @@ use lazyperson710\core\packet\SendForm;
 use lazyperson710\core\packet\SoundPacket;
 use pocketmine\player\Player;
 use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\TextFormat;
 
-class LevelConfirmation {
+class LevelCheck {
 
 	use SingletonTrait;
 
-	public function levelConfirmation(Player $player, FormBase $formBase, int $restrictionLevel) : void {
+	public function check(Player $player, FormBase $formBase, int $restrictionLevel) : void {
 		if (MiningLevelAPI::getInstance()->getLevel($player) >= $restrictionLevel) {
 			SendForm::Send($player, $formBase);
 		} else {
-			$error = "§c要求されたレベルに達していない為処理が中断されました\n要求レベル -> lv.{$restrictionLevel}";
+			$error = PHP_EOL . TextFormat::RED . '要求されたレベルに達していない為処理が中断されました' . PHP_EOL . '要求レベル -> lv.' . $restrictionLevel;
 			SendForm::Send($player, (new ShopSelectForm($player, $error)));
 			SoundPacket::Send($player, 'dig.chain');
 		}
