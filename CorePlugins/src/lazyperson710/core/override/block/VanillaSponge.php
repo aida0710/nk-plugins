a9ad3510bb72647fa44fae124864ed24e640a7fa
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-08 07:16:26 +0900

    スポンジの有効範囲を上昇

--- CorePlugins/src/lazyperson710/core/override/block/VanillaSponge.php
+++ CorePlugins/src/lazyperson710/core/override/block/VanillaSponge.php
@@ -2,8 +2,10 @@
 
 declare(strict_types = 0);
 
-namespace bbo51dog\vanillasponge\block;
+namespace lazyperson710\core\override\block;
 
+use lazyperson0710\WorldManagement\database\WorldCategory;
+use lazyperson710\core\packet\SendMessage\SendTip;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier;
@@ -24,23 +26,27 @@ class VanillaSponge extends Sponge {
 
 	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, Player $player = null) : bool {
 		if (!$this->isWet()) {
+			if (!in_array($player->getWorld()->getFolderName(), WorldCategory::NatureAll, true)) {
+				SendTip::Send($player, 'このワールドではスポンジは機能しません。資源系ワールドでの使用可能です。', 'Sponge', false);
+				return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
+			}
 			$this->absorb();
 		}
 		return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
 	}
 
-	private function absorb() {
+	private function absorb() : void {
 		$pos = $this->getPosition();
-		$startX = $pos->getFloorX() - 2;
-		$startY = $pos->getFloorY() - 2;
-		$startZ = $pos->getFloorZ() - 2;
+		$startX = $pos->getFloorX() - 8;
+		$startY = $pos->getFloorY() - 8;
+		$startZ = $pos->getFloorZ() - 8;
 		$world = $pos->getWorld();
 		$absorbed = false;
-		for ($x = 0; $x <= 5; $x++) {
-			for ($y = 0; $y <= 5; $y++) {
-				for ($z = 0; $z <= 5; $z++) {
+		for ($x = 0; $x <= 16; $x++) {
+			for ($y = 0; $y <= 16; $y++) {
+				for ($z = 0; $z <= 16; $z++) {
 					$id = $world->getBlockAt($startX + $x, $startY + $y, $startZ + $z)->getId();
-					if ($id === BlockLegacyIds::WATER || $id === BlockLegacyIds::FLOWING_WATER) {
+					if ($id === BlockLegacyIds::WATER || $id === BlockLegacyIds::FLOWING_WATER || $id === BlockLegacyIds::LAVA || $id === BlockLegacyIds::FLOWING_LAVA) {
 						$world->setBlockAt($startX + $x, $startY + $y, $startZ + $z, VanillaBlocks::AIR(), true);
 						$absorbed = true;
 					}
