9be836f91386e7eed985920d44182962d673bb91
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-08 07:17:50 +0900

    コメントを追加

--- CorePlugins/src/lazyperson710/core/packet/SendForm.php
+++ CorePlugins/src/lazyperson710/core/packet/SendForm.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 0);
+declare(strict_types = 1);
 
 namespace lazyperson710\core\packet;
 
@@ -11,6 +11,13 @@ use pocketmine\player\Player;
 
 class SendForm {
 
+	/**
+	 * 基本的にformをプレイヤーに送信するときは使ってください
+	 *
+	 * @param Player $player
+	 * @param Form   $form
+	 * @return void
+	 */
 	public static function Send(Player $player, Form $form) : void {
 		if (IntervalTask::check($player, 'SendForm')) {
 			SendNoSoundTip::Send($player, '0.3秒以内連続でFormを送信することは出来ません', 'SendForm', true);
