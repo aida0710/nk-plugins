00d742c89e92734a78470088712cbd5ca33d8f09
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-05-30 15:58:15 +0900

    PluginBaseの継承、とクラス名変更

--- ShopSystem/src/lazyperson0710/shop/ShopPlugin.php
+++ ShopSystem/src/lazyperson0710/shop/ShopPlugin.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace lazyperson0710\folivora\features\shop;
+namespace lazyperson0710\shop;
 
 use lazyperson0710\shop\command\EffectShopCommand;
 use lazyperson0710\shop\command\EnchantShopCommand;
@@ -12,16 +12,19 @@ use lazyperson0710\shop\database\EnchantShopAPI;
 use lazyperson0710\shop\database\ItemShopAPI;
 use pocketmine\plugin\PluginBase;
 
-class Shop {
+class ShopPlugin extends PluginBase {
 
-	public function init(PluginBase $base) : void {
+	protected function onEnable() : void {
 		ItemShopAPI::getInstance()->init();
 		EffectShopAPI::getInstance()->init();
 		EnchantShopAPI::getInstance()->init();
-		$base->getServer()->getCommandMap()->registerAll('shopSystem', [
+		$this->getServer()->getCommandMap()->registerAll('shopSystem', [
 			new ItemShopCommand(),
 			new EnchantShopCommand(),
 			new EffectShopCommand(),
 		]);
+	}
+
+	protected function onDisable() : void {
 	}
 }
