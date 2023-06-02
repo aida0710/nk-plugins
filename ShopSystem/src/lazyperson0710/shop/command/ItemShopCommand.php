0ee3386c8d7759ff57c13d5176853ab6db14fa23
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-02 09:36:18 +0900

    sendForm関数を使用

--- ShopSystem/src/lazyperson0710/shop/command/ItemShopCommand.php
+++ ShopSystem/src/lazyperson0710/shop/command/ItemShopCommand.php
@@ -18,37 +18,37 @@ use pocketmine\player\Player;
 
 class ItemShopCommand extends Command {
 
-	public function __construct() {
-		parent::__construct('shop', 'ItemShopを開きます - /shop [shopID]');
-	}
+    public function __construct() {
+        parent::__construct('shop', 'ItemShopを開きます - /shop [shopID]');
+    }
 
-	/**
-	 * @param CommandSender $sender
-	 * @param string        $commandLabel
-	 * @param array         $args
-	 * @return void
-	 */
-	public function execute(CommandSender $sender, string $commandLabel, array $args) : void {
-		if (!($sender instanceof Player)) {
-			$sender->sendMessage('サーバー内で実行してください');
-			return;
-		}
-		if (!isset($args[0])) {
-			$sender->sendForm(new ShopSelectForm($sender));
-			return;
-		}
-		match ($args[0]) {
-			'other' => LevelCheck::getInstance()->check($sender, new OtherShopSelectForm(), RestrictionShop::RESTRICTION_LEVEL_OTHER_SHOP),
-			'search' => LevelCheck::getInstance()->check($sender, new SearchItemForm(), RestrictionShop::RESTRICTION_LEVEL_OTHER_SHOP),
-			'invsell' => LevelCheck::getInstance()->check($sender, new InvSellConfirmationForm(), RestrictionShop::RESTRICTION_LEVEL_OTHER_SHOP),
-			'1' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 1), RestrictionShop::RESTRICTION_LEVEL_SHOP_1),
-			'2' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 2), RestrictionShop::RESTRICTION_LEVEL_SHOP_2),
-			'3' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 3), RestrictionShop::RESTRICTION_LEVEL_SHOP_3),
-			'4' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 4), RestrictionShop::RESTRICTION_LEVEL_SHOP_4),
-			'5' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 5), RestrictionShop::RESTRICTION_LEVEL_SHOP_5),
-			'6' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 6), RestrictionShop::RESTRICTION_LEVEL_SHOP_6),
-			'7' => LevelCheck::getInstance()->check($sender, new CategorySelectForm($sender, 7), RestrictionShop::RESTRICTION_LEVEL_SHOP_7),
-			default => SendForm::Send($sender, new ShopSelectForm($sender)),
-		};
-	}
+    /**
+     * @param CommandSender $sender
+     * @param string        $commandLabel
+     * @param array         $args
+     * @return void
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : void {
+        if (!($sender instanceof Player)) {
+            $sender->sendMessage('サーバー内で実行してください');
+            return;
+        }
+        if (!isset($args[0])) {
+            $sender->sendForm(new ShopSelectForm($sender));
+            return;
+        }
+        match ($args[0]) {
+            'other' => LevelCheck::sendForm($sender, new OtherShopSelectForm(), RestrictionShop::RESTRICTION_LEVEL_OTHER_SHOP),
+            'search' => LevelCheck::sendForm($sender, new SearchItemForm(), RestrictionShop::RESTRICTION_LEVEL_OTHER_SHOP),
+            'invsell' => LevelCheck::sendForm($sender, new InvSellConfirmationForm(), RestrictionShop::RESTRICTION_LEVEL_OTHER_SHOP),
+            '1' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 1), RestrictionShop::RESTRICTION_LEVEL_SHOP_1),
+            '2' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 2), RestrictionShop::RESTRICTION_LEVEL_SHOP_2),
+            '3' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 3), RestrictionShop::RESTRICTION_LEVEL_SHOP_3),
+            '4' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 4), RestrictionShop::RESTRICTION_LEVEL_SHOP_4),
+            '5' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 5), RestrictionShop::RESTRICTION_LEVEL_SHOP_5),
+            '6' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 6), RestrictionShop::RESTRICTION_LEVEL_SHOP_6),
+            '7' => LevelCheck::sendForm($sender, new CategorySelectForm($sender, 7), RestrictionShop::RESTRICTION_LEVEL_SHOP_7),
+            default => SendForm::Send($sender, new ShopSelectForm($sender)),
+        };
+    }
 }
