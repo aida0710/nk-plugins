1282d766eddd0e82fdaaefbdc58835b281318636
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-06-23 12:22:21 +0900

    取得できる経験値量を定数化

--- CorePlugins/src/lazyperson710/core/listener/ExpAssignment.php
+++ CorePlugins/src/lazyperson710/core/listener/ExpAssignment.php
@@ -10,16 +10,18 @@ use pocketmine\event\Listener;
 
 class ExpAssignment implements Listener {
 
+    public const MONSTER_SPAWNER_EXP = 5000;
+
     /**
      * @priority Monitor
      * @param BlockBreakEvent $event
      * @return void
      */
-    public function onBreak(BlockBreakEvent $event) : void {
+    public function onBreak(BlockBreakEvent $event): void {
         if ($event->isCancelled()) return;
         if ($event->getBlock()->getName() === VanillaBlocks::MONSTER_SPAWNER()->getName()) {
             $exp = $event->getXpDropAmount();
-            $exp += 5000;
+            $exp += self::MONSTER_SPAWNER_EXP;
             $player = $event->getPlayer();
             $player->getXpManager()->addXp($exp);
             $event->setXpDropAmount(0);
