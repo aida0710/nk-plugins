b107d6127a787a69313fb2c7952e90d9a3d78bca
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-07-14 11:30:14 +0900

    bboFormを更新

---
+++ BboForm-master/src/bbo51dog/bboform/form/FormBase.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace bbo51dog\bboform\form;
+
+use pocketmine\form\Form;
+use pocketmine\player\Player;
+
+abstract class FormBase implements Form {
+
+    public const TYPE_SIMPLE = "form";
+    public const TYPE_MODAL = "modal";
+    public const TYPE_CUSTOM = "custom_form";
+
+    /** @var string */
+    private $title = "";
+
+    abstract public function getType(): string;
+
+    /**
+     * @return string|array
+     */
+    abstract public function serializeContent();
+
+    /**
+     * @inheritDoc
+     */
+    public function jsonSerialize(): array {
+        return [
+            "type" => $this->getType(),
+            "title" => $this->getTitle(),
+            "content" => $this->serializeContent(),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return FormBase
+     */
+    public function setTitle(string $title): self {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function handleResponse(Player $player, $data): void {
+        if ($data === null) {
+            $this->handleClosed($player);
+        }
+    }
+
+    /**
+     * Called when form is submitted
+     *
+     * @param Player $player
+     */
+    public function handleSubmit(Player $player): void {
+    }
+
+    /**
+     * Called when form is closed without submit
+     *
+     * @param Player $player
+     */
+    public function handleClosed(Player $player): void {
+    }
+}
